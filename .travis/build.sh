fractal-l/AyuGramDesktop

commit 0d00f48ba4df5a5a24fe470694492ccf50fc722d

Fixed build for QtCreator and Ubuntu.

.travis/build.sh
@@ -141,6 +141,8 @@ build() {
 
     # Patch tdesktop
     sed -i 's/CUSTOM_API_ID//g' "$UPSTREAM/Telegram/Telegram.pro"
+    sed -i 's,LIBS += /usr/local/lib/libxkbcommon.a,,g" "$UPSTREAM/Telegram/Telegram.pro"
+    sed -i 's,#xkbcommon,xkbcommon,g" "$UPSTREAM/Telegram/Telegram.pro"
     sed -i "s,\..*/Libraries/breakpad/,$BREAKPAD_PATH/,g" "$UPSTREAM/Telegram/Telegram.pro"
 
 	local options=""

Telegram/SourceFiles/platform/linux/linux_libs.h
@@ -227,7 +227,7 @@ inline GtkWindow *gtk_window_cast(Object *obj) {
 	return g_type_cic_helper<GtkWindow, Object>(obj, gtk_window_get_type());
 }
 
-typedef gboolean (*f_g_type_check_instance_is_a)(GTypeInstance *instance, GType iface_type) G_GNUC_PURE;
+typedef gboolean (*f_g_type_check_instance_is_a)(GTypeInstance *instance, GType iface_type);
 extern f_g_type_check_instance_is_a g_type_check_instance_is_a;
 
 template <typename Object>

Telegram/SourceFiles/platform/linux/main_window_linux.cpp
@@ -360,8 +360,8 @@ void MainWindow::psUpdateCounter() {
 			bool muted = App::histories().unreadOnlyMuted();
 
 			style::color bg = muted ? st::counterMuteBG : st::counterBG;
-			icon.addPixmap(App::pixmapFromImageInPlace(iconWithCounter(16, counter, bg, true), Qt::ColorOnly));
-			icon.addPixmap(App::pixmapFromImageInPlace(iconWithCounter(32, counter, bg, true), Qt::ColorOnly));
+			icon.addPixmap(App::pixmapFromImageInPlace(iconWithCounter(16, counter, bg, true)));
+			icon.addPixmap(App::pixmapFromImageInPlace(iconWithCounter(32, counter, bg, true)));
 		}
 		trayIcon->setIcon(icon);
 	}

Telegram/Telegram.pro
@@ -111,7 +111,6 @@ SOURCES += \
 	./SourceFiles/apiwrap.cpp \
 	./SourceFiles/app.cpp \
 	./SourceFiles/application.cpp \
-	./SourceFiles/audio.cpp \
 	./SourceFiles/autoupdater.cpp \
 	./SourceFiles/dialogswidget.cpp \
 	./SourceFiles/dropdown.cpp \
@@ -280,7 +279,6 @@ HEADERS += \
 	./SourceFiles/apiwrap.h \
 	./SourceFiles/app.h \
 	./SourceFiles/application.h \
-	./SourceFiles/audio.h \
 	./SourceFiles/autoupdater.h \
 	./SourceFiles/config.h \
 	./SourceFiles/countries.h \
@@ -534,7 +532,11 @@ PKGCONFIG += \
 	x11\
 	xi\
 	xext\
-	xkbcommon\
+# In order to work libxkbcommon must be linked statically,
+# PKGCONFIG links it like "-L/usr/local/lib -lxkbcommon"
+# which makes a dynamic link which leads to segfault in
+# QApplication() -> createPlatformIntegration -> QXcbIntegrationPlugin::create
+	#xkbcommon\
 	openal\
 	libavformat\
 	libavcodec\
@@ -555,6 +557,12 @@ LIBS += $${QT_TDESKTOP_PATH}/plugins/platforminputcontexts/libcomposeplatforminp
         $${QT_TDESKTOP_PATH}/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.a
 
 LIBS += ./../../../Libraries/breakpad/src/client/linux/libbreakpad_client.a
+
+# In order to work libxkbcommon must be linked statically,
+# PKGCONFIG links it like "-L/usr/local/lib -lxkbcommon"
+# which makes a dynamic link which leads to segfault in
+# QApplication() -> createPlatformIntegration -> QXcbIntegrationPlugin::create
+LIBS += /usr/local/lib/libxkbcommon.a
 
 RESOURCES += \
 	./Resources/telegram.qrc \

Telegram/build/makefile_static.sh
@@ -58,7 +58,6 @@ Replace '\-lICE' "$ArchDirPath\/libICE\.a"
 Replace '\-lfontconfig' "$ArchDirPath\/libfontconfig\.a $ArchDirPath\/libexpat\.a"
 Replace '\-lfreetype' "$ArchDirPath\/libfreetype\.a"
 Replace '\-lXext' "$ArchDirPath\/libXext\.a"
-Replace '\-lxkbcommon' "$LocalDirPath\/libxkbcommon\.a"
 Replace '\-lopus' "$LocalDirPath\/libopus\.a"
 Replace '\-lopenal' "$LocalDirPath\/libopenal\.a"
 Replace '\-lavformat' "$LocalDirPath\/libavformat\.a"
@@ -66,4 +65,5 @@ Replace '\-lavcodec' "$LocalDirPath\/libavcodec\.a"
 Replace '\-lswresample' "$LocalDirPath\/libswresample\.a"
 Replace '\-lswscale' "$LocalDirPath\/libswscale\.a"
 Replace '\-lavutil' "$LocalDirPath\/libavutil\.a"
+Replace '\-lva-x11' "$LocalDirPath\/libva-x11\.a"
 Replace '\-lva' "$LocalDirPath\/libva\.a"

commit 0ff3e53a27dc0a90dbc8aa3fe2e85ea6f220098d

Fix syntax error
How did this even build?

Errors introduced by https://github.com/telegramdesktop/tdesktop/commit/0d00f48ba4df5a5a24fe470694492ccf50fc722d

Signed-off-by: Christoph <auer.chrisi@gmx.net>

.travis/build.sh
@@ -141,8 +141,8 @@ build() {
 
     # Patch tdesktop
     sed -i 's/CUSTOM_API_ID//g' "$UPSTREAM/Telegram/Telegram.pro"
-    sed -i 's,LIBS += /usr/local/lib/libxkbcommon.a,,g" "$UPSTREAM/Telegram/Telegram.pro"
-    sed -i 's,#xkbcommon,xkbcommon,g" "$UPSTREAM/Telegram/Telegram.pro"
+    sed -i 's,LIBS += /usr/local/lib/libxkbcommon.a,,g' "$UPSTREAM/Telegram/Telegram.pro"
+    sed -i 's,#xkbcommon,xkbcommon,g' "$UPSTREAM/Telegram/Telegram.pro"
     sed -i "s,\..*/Libraries/breakpad/,$BREAKPAD_PATH/,g" "$UPSTREAM/Telegram/Telegram.pro"
 
 	local options=""
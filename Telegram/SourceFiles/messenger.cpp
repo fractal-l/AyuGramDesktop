fractal-l/AyuGramDesktop

commit 5c8bcf68e43d99c96e90ab890ad7fb8f50173487

Return FlatLabel.margins in getMargins().

Telegram/SourceFiles/settings/settings_cover.cpp
@@ -153,24 +153,23 @@ void CoverWidget::refreshButtonsGeometry(int newWidth) {
 void CoverWidget::refreshNameGeometry(int newWidth) {
 	auto margins = getMargins();
 	auto infoLeft = _userpicButton->x() + _userpicButton->width();
-	auto nameLeft = infoLeft + st::settingsNameLeft - st::settingsNameLabel.margin.left();
-	auto nameTop = _userpicButton->y() + st::settingsNameTop - st::settingsNameLabel.margin.top();
+	auto nameLeft = infoLeft + st::settingsNameLeft;
+	auto nameTop = _userpicButton->y() + st::settingsNameTop;
 	auto nameWidth = newWidth - infoLeft - st::settingsNameLeft;
 	auto editNameInlineVisible = !_editNameVisible;
 	if (editNameInlineVisible) {
 		nameWidth -= _editNameInline->width();
 	}
-	int marginsAdd = st::settingsNameLabel.margin.left() + st::settingsNameLabel.margin.right();
 
-	_name->resizeToWidth(qMin(nameWidth - marginsAdd, _name->naturalWidth()) + marginsAdd);
+	_name->resizeToNaturalWidth(nameWidth);
 	_name->moveToLeft(
 		margins.left() + nameLeft,
 		margins.top() + nameTop,
 		newWidth);
 
 	_editNameInline->moveToLeft(
-		margins.left() + nameLeft + _name->width(),
-		margins.top() + nameTop,
+		margins.left() + nameLeft + _name->widthNoMargins(),
+		margins.top() + nameTop + st::settingsNameLabel.margin.top(),
 		newWidth);
 	_editNameInline->setVisible(editNameInlineVisible);
 }

Telegram/SourceFiles/auth_session.cpp
@@ -162,6 +162,10 @@ void AuthSessionData::constructFromSerialized(const QByteArray &serialized) {
 	_variables.groupStickersSectionHidden = std::move(groupStickersSectionHidden);
 	_variables.thirdSectionInfoEnabled = thirdSectionInfoEnabled;
 	_variables.smallDialogsList = smallDialogsList;
+
+	if (_variables.thirdSectionInfoEnabled) {
+		_variables.tabbedSelectorSectionEnabled = false;
+	}
 }
 
 void AuthSessionData::setTabbedSelectorSectionEnabled(bool enabled) {

Telegram/SourceFiles/boxes/boxes.style
@@ -582,13 +582,13 @@ editPrivacyTitle: FlatLabel(defaultFlatLabel) {
 	width: 320px;
 	textFg: boxTitleFg;
 	maxHeight: 56px;
-	margin: margins(0px, 20px, 0px, 13px);
 	style: TextStyle(defaultTextStyle) {
 		font: boxTitleFont;
 		linkFont: boxTitleFont;
 		linkFontOver: boxTitleFont;
 	}
 }
+editPrivacyTitlePadding: margins(23px, 20px, 21px, 13px);
 editPrivacyLabel: FlatLabel(defaultFlatLabel) {
 	width: 320px;
 	textFg: membersAboutLimitFg;
@@ -674,11 +674,11 @@ mutePhotoButton: PeerAvatarButton {
 muteChatTitle: FlatLabel(boxLabel) {
 	width: 235px;
 	maxHeight: 20px;  // block word wrap
-	margin: margins(50px, 0px, 0px, 0px);  // left offset for place peer avatar
 	style: TextStyle(boxTextStyle) {
 		font: font(boxFontSize semibold);
 	}
 }
+muteChatTitleLeft: 50px;
 
 groupStickersRemove: contactsSearchCancel;
 groupStickersRemovePosition: point(6px, 6px);

Telegram/SourceFiles/boxes/edit_privacy_box.cpp
@@ -119,7 +119,7 @@ int EditPrivacyBox::resizeGetHeight(int newWidth) {
 	layoutRow(_contacts, st::editPrivacyOptionMargin);
 	layoutRow(_nobody, st::editPrivacyOptionMargin);
 	layoutRow(_warning, st::editPrivacyWarningPadding);
-	layoutRow(_exceptionsTitle, st::editPrivacyPadding);
+	layoutRow(_exceptionsTitle, st::editPrivacyTitlePadding);
 	auto linksTop = top;
 	layoutRow(_alwaysLink, st::editPrivacyPadding);
 	layoutRow(_neverLink, st::editPrivacyPadding);
@@ -167,7 +167,7 @@ int EditPrivacyBox::countDefaultHeight(int newWidth) {
 	height += optionHeight(Option::Contacts);
 	height += optionHeight(Option::Nobody);
 	height += labelHeight(_controller->warning(), st::editPrivacyLabel, st::editPrivacyWarningPadding);
-	height += labelHeight(lang(lng_edit_privacy_exceptions), st::editPrivacyTitle, st::editPrivacyPadding);
+	height += labelHeight(lang(lng_edit_privacy_exceptions), st::editPrivacyTitle, st::editPrivacyTitlePadding);
 	height += linkHeight();
 	height += linkHeight();
 	height += labelHeight(_controller->exceptionsDescription(), st::editPrivacyLabel, st::editPrivacyPadding);

Telegram/SourceFiles/boxes/mute_settings_box.cpp
@@ -29,8 +29,9 @@ void MuteSettingsBox::prepare() {
 
 	object_ptr<Ui::FlatLabel> title(this, st::muteChatTitle);
 	title->setText(App::peerName(_peer, true));
-	title->moveToLeft(st::boxPadding.left(),
-		y + icon->height() / 2 - title->height() / 2);
+	title->moveToLeft(
+		st::boxPadding.left() + st::muteChatTitleLeft,
+		y + (icon->height() / 2) - (title->height() / 2));
 	// the icon is always higher than this chat title
 	y += icon->height() + st::boxMediumSkip;
 

Telegram/SourceFiles/calls/calls.style
@@ -197,7 +197,8 @@ callRatingComment: InputField(defaultInputField) {
 }
 callRatingCommentTop: 8px;
 
+callDebugPadding: margins(24px, 0px, 24px, 0px);
 callDebugLabel: FlatLabel(defaultFlatLabel) {
-	margin: margins(24px, 0px, 24px, 0px);
+	margin: callDebugPadding;
 }
 callPanelDuration: 150;

Telegram/SourceFiles/calls/calls_top_bar.cpp
@@ -23,6 +23,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "styles/style_calls.h"
 #include "ui/widgets/buttons.h"
 #include "ui/widgets/labels.h"
+#include "ui/wrap/padding_wrap.h"
 #include "lang/lang_keys.h"
 #include "calls/calls_call.h"
 #include "calls/calls_instance.h"
@@ -59,7 +60,11 @@ void DebugInfoBox::prepare() {
 	setTitle([] { return QString("Call Debug"); });
 
 	addButton(langFactory(lng_close), [this] { closeBox(); });
-	_text = setInnerWidget(object_ptr<Ui::FlatLabel>(this, st::callDebugLabel));
+	_text = setInnerWidget(
+		object_ptr<Ui::PaddingWrap<Ui::FlatLabel>>(
+			this,
+			object_ptr<Ui::FlatLabel>(this, st::callDebugLabel),
+			st::callDebugPadding))->entity();
 	_text->setSelectable(true);
 	updateText();
 	_updateTextTimer.setCallback([this] { updateText(); });

Telegram/SourceFiles/info/info.style
@@ -138,8 +138,15 @@ infoIconNotifications: icon {{ "info_notifications", infoIconFg }};
 infoIconActions: icon {{ "info_actions", infoIconFg }};
 
 infoLabel: FlatLabel(defaultFlatLabel) {
+	textFg: windowSubTextFg;
 }
 infoLabeled: FlatLabel(defaultFlatLabel) {
+	width: 180px;
+	margin: margins(5px, 5px, 5px, 5px);
+}
+infoLabeledOneLine: FlatLabel(infoLabeled) {
+	width: 0px; // No need to set minWidth in one-line text.
+	maxHeight: 20px;
 }
 
 infoProfileButton: InfoProfileButton {

commit 683a52a42e6bc09be13ee296b9a6aaa5b0c797a1

Fix edit name button in Settings.
Regression was introduced in 5c8bcf68e4.

Telegram/SourceFiles/messenger.cpp
@@ -547,7 +547,7 @@ void Messenger::chatPhotoDone(PeerId peer, const MTPUpdates &updates) {
 	emit peerPhotoDone(peer);
 }
 
-bool Messenger::peerPhotoFail(PeerId peer, const RPCError &error) {
+bool Messenger::peerPhotoFailed(PeerId peer, const RPCError &error) {
 	if (MTP::isDefaultHandledError(error)) return false;
 
 	LOG(("Application Error: update photo failed %1: %2").arg(error.type()).arg(error.description()));
@@ -560,12 +560,12 @@ void Messenger::peerClearPhoto(PeerId id) {
 	if (!AuthSession::Exists()) return;
 
 	if (id == Auth().userPeerId()) {
-		MTP::send(MTPphotos_UpdateProfilePhoto(MTP_inputPhotoEmpty()), rpcDone(&Messenger::selfPhotoCleared), rpcFail(&Messenger::peerPhotoFail, id));
+		MTP::send(MTPphotos_UpdateProfilePhoto(MTP_inputPhotoEmpty()), rpcDone(&Messenger::selfPhotoCleared), rpcFail(&Messenger::peerPhotoFailed, id));
 	} else if (peerIsChat(id)) {
-		MTP::send(MTPmessages_EditChatPhoto(peerToBareMTPInt(id), MTP_inputChatPhotoEmpty()), rpcDone(&Messenger::chatPhotoCleared, id), rpcFail(&Messenger::peerPhotoFail, id));
+		MTP::send(MTPmessages_EditChatPhoto(peerToBareMTPInt(id), MTP_inputChatPhotoEmpty()), rpcDone(&Messenger::chatPhotoCleared, id), rpcFail(&Messenger::peerPhotoFailed, id));
 	} else if (peerIsChannel(id)) {
 		if (auto channel = App::channelLoaded(id)) {
-			MTP::send(MTPchannels_EditPhoto(channel->inputChannel, MTP_inputChatPhotoEmpty()), rpcDone(&Messenger::chatPhotoCleared, id), rpcFail(&Messenger::peerPhotoFail, id));
+			MTP::send(MTPchannels_EditPhoto(channel->inputChannel, MTP_inputChatPhotoEmpty()), rpcDone(&Messenger::chatPhotoCleared, id), rpcFail(&Messenger::peerPhotoFailed, id));
 		}
 	}
 }
@@ -655,13 +655,13 @@ void Messenger::photoUpdated(const FullMsgId &msgId, bool silent, const MTPInput
 	if (i != photoUpdates.end()) {
 		auto id = i.value();
 		if (id == Auth().userPeerId()) {
-			MTP::send(MTPphotos_UploadProfilePhoto(file), rpcDone(&Messenger::selfPhotoDone), rpcFail(&Messenger::peerPhotoFail, id));
+			MTP::send(MTPphotos_UploadProfilePhoto(file), rpcDone(&Messenger::selfPhotoDone), rpcFail(&Messenger::peerPhotoFailed, id));
 		} else if (peerIsChat(id)) {
 			auto history = App::history(id);
-			history->sendRequestId = MTP::send(MTPmessages_EditChatPhoto(history->peer->asChat()->inputChat, MTP_inputChatUploadedPhoto(file)), rpcDone(&Messenger::chatPhotoDone, id), rpcFail(&Messenger::peerPhotoFail, id), 0, 0, history->sendRequestId);
+			history->sendRequestId = MTP::send(MTPmessages_EditChatPhoto(history->peer->asChat()->inputChat, MTP_inputChatUploadedPhoto(file)), rpcDone(&Messenger::chatPhotoDone, id), rpcFail(&Messenger::peerPhotoFailed, id), 0, 0, history->sendRequestId);
 		} else if (peerIsChannel(id)) {
 			auto history = App::history(id);
-			history->sendRequestId = MTP::send(MTPchannels_EditPhoto(history->peer->asChannel()->inputChannel, MTP_inputChatUploadedPhoto(file)), rpcDone(&Messenger::chatPhotoDone, id), rpcFail(&Messenger::peerPhotoFail, id), 0, 0, history->sendRequestId);
+			history->sendRequestId = MTP::send(MTPchannels_EditPhoto(history->peer->asChannel()->inputChannel, MTP_inputChatUploadedPhoto(file)), rpcDone(&Messenger::chatPhotoDone, id), rpcFail(&Messenger::peerPhotoFailed, id), 0, 0, history->sendRequestId);
 		}
 	}
 }

Telegram/SourceFiles/messenger.h
@@ -159,7 +159,7 @@ public:
 	void chatPhotoCleared(PeerId peer, const MTPUpdates &updates);
 	void selfPhotoDone(const MTPphotos_Photo &result);
 	void chatPhotoDone(PeerId peerId, const MTPUpdates &updates);
-	bool peerPhotoFail(PeerId peerId, const RPCError &e);
+	bool peerPhotoFailed(PeerId peerId, const RPCError &e);
 	void peerClearPhoto(PeerId peer);
 
 	void writeUserConfigIn(TimeMs ms);

Telegram/SourceFiles/settings/settings_cover.cpp
@@ -61,19 +61,30 @@ CoverWidget::CoverWidget(QWidget *parent, UserData *self)
 	_name->setSelectable(true);
 	_name->setContextCopyText(lang(lng_profile_copy_fullname));
 
-	_setPhoto->setClickedCallback(App::LambdaDelayed(st::settingsPrimaryButton.ripple.hideDuration, this, [this] { onSetPhoto(); }));
-	connect(_editName, SIGNAL(clicked()), this, SLOT(onEditName()));
-	connect(_editNameInline, SIGNAL(clicked()), this, SLOT(onEditName()));
+	_setPhoto->setClickedCallback(App::LambdaDelayed(
+		st::settingsPrimaryButton.ripple.hideDuration,
+		this,
+		[this] { chooseNewPhoto(); }));
+	_editName->addClickHandler([this] { editName(); });
+	_editNameInline->addClickHandler([this] { editName(); });
 
 	auto observeEvents = Notify::PeerUpdate::Flag::NameChanged | Notify::PeerUpdate::Flag::PhotoChanged;
 	subscribe(Notify::PeerUpdated(), Notify::PeerUpdatedHandler(observeEvents, [this](const Notify::PeerUpdate &update) {
 		notifyPeerUpdated(update);
 	}));
 
-	connect(&Messenger::Instance(), SIGNAL(peerPhotoDone(PeerId)), this, SLOT(onPhotoUploadStatusChanged(PeerId)));
-	connect(&Messenger::Instance(), SIGNAL(peerPhotoFail(PeerId)), this, SLOT(onPhotoUploadStatusChanged(PeerId)));
+	connect(
+		&Messenger::Instance(),
+		&Messenger::peerPhotoDone,
+		this,
+		&CoverWidget::onPhotoUploadStatusChanged);
+	connect(
+		&Messenger::Instance(),
+		&Messenger::peerPhotoFail,
+		this,
+		&CoverWidget::onPhotoUploadStatusChanged);
 
-	connect(_userpicButton, SIGNAL(clicked()), this, SLOT(onPhotoShow()));
+	_userpicButton->addClickHandler([this] { showPhoto(); });
 	validatePhoto();
 
 	refreshNameText();
@@ -92,13 +103,13 @@ PhotoData *CoverWidget::validatePhoto() const {
 	return photo;
 }
 
-void CoverWidget::onPhotoShow() {
+void CoverWidget::showPhoto() {
 	if (auto photo = validatePhoto()) {
 		Messenger::Instance().showPhoto(photo, _self);
 	}
 }
 
-void CoverWidget::onCancelPhotoUpload() {
+void CoverWidget::cancelPhotoUpload() {
 	Messenger::Instance().cancelPhotoUpdate(_self->id);
 	refreshStatusText();
 }
@@ -175,8 +186,8 @@ void CoverWidget::refreshNameGeometry(int newWidth) {
 		newWidth);
 
 	_editNameInline->moveToLeft(
-		margins.left() + nameLeft + _name->widthNoMargins(),
-		margins.top() + nameTop + st::settingsNameLabel.margin.top(),
+		margins.left() + nameLeft + _name->widthNoMargins() + st::settingsNameLabel.margin.right(),
+		margins.top() + nameTop - st::settingsNameLabel.margin.top(),
 		newWidth);
 	_editNameInline->setVisible(editNameInlineVisible);
 }
@@ -310,7 +321,7 @@ void CoverWidget::refreshStatusText() {
 		if (!_cancelPhotoUpload) {
 			auto margins = getMargins();
 			_cancelPhotoUpload.create(this, lang(lng_cancel), st::defaultLinkButton);
-			connect(_cancelPhotoUpload, SIGNAL(clicked()), this, SLOT(onCancelPhotoUpload()));
+			_cancelPhotoUpload->addClickHandler([this] { cancelPhotoUpload(); });
 			_cancelPhotoUpload->show();
 			_cancelPhotoUpload->moveToLeft(
 				margins.left()
@@ -335,7 +346,7 @@ void CoverWidget::refreshStatusText() {
 	update();
 }
 
-void CoverWidget::onSetPhoto() {
+void CoverWidget::chooseNewPhoto() {
 	auto imageExtensions = cImgExtensions();
 	auto filter = qsl("Image files (*") + imageExtensions.join(qsl(" *")) + qsl(");;") + FileDialog::AllFilesFilter();
 	FileDialog::GetOpenPath(lang(lng_choose_image), filter, base::lambda_guarded(this, [this](const FileDialog::OpenResult &result) {
@@ -354,7 +365,7 @@ void CoverWidget::onSetPhoto() {
 	}));
 }
 
-void CoverWidget::onEditName() {
+void CoverWidget::editName() {
 	Ui::show(Box<EditNameTitleBox>(self()));
 }
 

Telegram/SourceFiles/settings/settings_cover.h
@@ -41,18 +41,8 @@ class CoverDropArea;
 namespace Settings {
 
 class CoverWidget : public BlockWidget {
-	Q_OBJECT
-
 public:
 	CoverWidget(QWidget *parent, UserData *self);
-
-private slots:
-	void onPhotoShow();
-	void onPhotoUploadStatusChanged(PeerId peerId = 0);
-	void onCancelPhotoUpload();
-
-	void onSetPhoto();
-	void onEditName();
 
 protected:
 	void dragEnterEvent(QDragEnterEvent *e) override;
@@ -67,6 +57,13 @@ protected:
 private:
 	// Observed notifications.
 	void notifyPeerUpdated(const Notify::PeerUpdate &update);
+
+	void showPhoto();
+	void cancelPhotoUpload();
+	void chooseNewPhoto();
+	void editName();
+
+	void onPhotoUploadStatusChanged(PeerId peerId = 0);
 
 	PhotoData *validatePhoto() const;
 
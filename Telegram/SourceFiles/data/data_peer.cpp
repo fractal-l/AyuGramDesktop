fractal-l/AyuGramDesktop

commit 3ffed1ddcfce88410a5015147e876f43690c3e85

Refactor userpic storage and access in PeerData.

Telegram/SourceFiles/data/data_peer.cpp
@@ -24,6 +24,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include <rpl/map.h>
 #include "data/data_peer_values.h"
 #include "data/data_channel_admins.h"
+#include "data/data_photo.h"
 #include "lang/lang_keys.h"
 #include "observer_peer.h"
 #include "mainwidget.h"
@@ -38,6 +39,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 namespace {
 
 constexpr auto kUpdateFullPeerTimeout = TimeMs(5000); // Not more than once in 5 seconds.
+constexpr auto kUserpicSize = 160;
 
 int peerColorIndex(const PeerId &peer) {
 	auto myId = Auth().userId();
@@ -347,14 +349,30 @@ ClickHandlerPtr PeerData::createOpenLink() {
 }
 
 void PeerData::setUserpic(
-		ImagePtr userpic,
-		StorageImageLocation location) {
+		PhotoId photoId,
+		const StorageImageLocation &location,
+		ImagePtr userpic) {
+	_userpicPhotoId = photoId;
 	_userpic = userpic;
 	_userpicLocation = location;
 	if (useEmptyUserpic()) {
 		_userpicEmpty.set(id, name);
 	} else {
 		_userpicEmpty.clear();
+	}
+}
+
+void PeerData::setUserpicPhoto(const MTPPhoto &data) {
+	auto photoId = [&]() -> PhotoId {
+		if (auto photo = App::feedPhoto(data)) {
+			photo->peer = this;
+			return photo->id;
+		}
+		return 0;
+	}();
+	if (_userpicPhotoId != photoId) {
+		_userpicPhotoId = photoId;
+		Notify::peerUpdatedDelayed(this, UpdateFlag::PhotoChanged);
 	}
 }
 
@@ -449,35 +467,44 @@ bool UserData::canShareThisContact() const {
 	return canShareThisContactFast() || !App::phoneFromSharedContact(peerToUser(id)).isEmpty();
 }
 
-void UserData::setPhoto(const MTPUserProfilePhoto &p) { // see Local::readPeer as well
-	auto newPhotoId = photoId;
-	auto newPhoto = _userpic;
-	auto newPhotoLoc = _userpicLocation;
-
-	switch (p.type()) {
-	case mtpc_userProfilePhoto: {
-		const auto &d(p.c_userProfilePhoto());
-		newPhotoId = d.vphoto_id.v;
-		newPhotoLoc = App::imageLocation(160, 160, d.vphoto_small);
-		newPhoto = newPhotoLoc.isNull() ? ImagePtr() : ImagePtr(newPhotoLoc);
-		//App::feedPhoto(App::photoFromUserPhoto(peerToUser(id), MTP_int(unixtime()), p));
-	} break;
-	default: {
-		newPhotoId = 0;
-		if (id == ServiceUserId) {
-			if (!_userpic) {
-				newPhoto = ImagePtr(App::pixmapFromImageInPlace(Messenger::Instance().logoNoMargin().scaledToWidth(160, Qt::SmoothTransformation)), "PNG");
-			}
-		} else {
-			newPhoto = ImagePtr();
-		}
-		newPhotoLoc = StorageImageLocation();
-	} break;
-	}
-	if (newPhotoId != photoId || newPhoto.v() != _userpic.v() || newPhotoLoc != _userpicLocation) {
-		photoId = newPhotoId;
-		setUserpic(newPhoto, newPhotoLoc);
+void PeerData::updateUserpic(
+		PhotoId photoId,
+		const MTPFileLocation &location) {
+	const auto size = kUserpicSize;
+	const auto loc = StorageImageLocation::FromMTP(size, size, location);
+	const auto photo = loc.isNull() ? ImagePtr() : ImagePtr(loc);
+	if (_userpicPhotoId != photoId
+		|| _userpic.v() != photo.v()
+		|| _userpicLocation != loc) {
+		setUserpic(photoId, loc, photo);
 		Notify::peerUpdatedDelayed(this, UpdateFlag::PhotoChanged);
+	}
+}
+
+void PeerData::clearUserpic() {
+	const auto photoId = PhotoId(0);
+	const auto loc = StorageImageLocation();
+	const auto photo = [&] {
+		if (id == peerFromUser(ServiceUserId)) {
+			auto image = Messenger::Instance().logoNoMargin().scaledToWidth(
+				kUserpicSize,
+				Qt::SmoothTransformation);
+			auto pixmap = App::pixmapFromImageInPlace(std::move(image));
+			return _userpic
+				? _userpic
+				: ImagePtr(std::move(pixmap), "PNG");
+		}
+		return ImagePtr();
+	}();
+}
+
+// see Local::readPeer as well
+void UserData::setPhoto(const MTPUserProfilePhoto &photo) {
+	if (photo.type() == mtpc_userProfilePhoto) {
+		const auto &data = photo.c_userProfilePhoto();
+		updateUserpic(data.vphoto_id.v, data.vphoto_small);
+	} else {
+		clearUserpic();
 	}
 }
 
@@ -668,32 +695,16 @@ bool UserData::hasCalls() const {
 	return (callsStatus() != CallsStatus::Disabled) && (callsStatus() != CallsStatus::Unknown);
 }
 
-void ChatData::setPhoto(const MTPChatPhoto &p, const PhotoId &phId) { // see Local::readPeer as well
-	auto newPhotoId = photoId;
-	auto newPhoto = _userpic;
-	auto newPhotoLoc = _userpicLocation;
+void ChatData::setPhoto(const MTPChatPhoto &photo) {
+	setPhoto(userpicPhotoId(), photo);
+}
 
-	switch (p.type()) {
-	case mtpc_chatPhoto: {
-		auto &d = p.c_chatPhoto();
-		if (phId != UnknownPeerPhotoId) {
-			newPhotoId = phId;
-		}
-		newPhotoLoc = App::imageLocation(160, 160, d.vphoto_small);
-		newPhoto = newPhotoLoc.isNull() ? ImagePtr() : ImagePtr(newPhotoLoc);
-//		photoFull = newPhoto ? ImagePtr(640, 640, d.vphoto_big, ImagePtr()) : ImagePtr();
-	} break;
-	default: {
-		newPhotoId = 0;
-		newPhotoLoc = StorageImageLocation();
-		newPhoto = ImagePtr();
-//		photoFull = ImagePtr();
-	} break;
-	}
-	if (newPhotoId != photoId || newPhoto.v() != _userpic.v() || newPhotoLoc != _userpicLocation) {
-		photoId = newPhotoId;
-		setUserpic(newPhoto, newPhotoLoc);
-		Notify::peerUpdatedDelayed(this, UpdateFlag::PhotoChanged);
+void ChatData::setPhoto(PhotoId photoId, const MTPChatPhoto &photo) {
+	if (photo.type() == mtpc_chatPhoto) {
+		const auto &data = photo.c_chatPhoto();
+		updateUserpic(photoId, data.vphoto_small);
+	} else {
+		clearUserpic();
 	}
 }
 
@@ -736,32 +747,16 @@ ChannelData::ChannelData(const PeerId &id)
 		}, _lifetime);
 }
 
-void ChannelData::setPhoto(const MTPChatPhoto &p, const PhotoId &phId) { // see Local::readPeer as well
-	auto newPhotoId = photoId;
-	auto newPhoto = _userpic;
-	auto newPhotoLoc = _userpicLocation;
+void ChannelData::setPhoto(const MTPChatPhoto &photo) {
+	setPhoto(userpicPhotoId(), photo);
+}
 
-	switch (p.type()) {
-	case mtpc_chatPhoto: {
-		auto &d = p.c_chatPhoto();
-		if (phId != UnknownPeerPhotoId) {
-			newPhotoId = phId;
-		}
-		newPhotoLoc = App::imageLocation(160, 160, d.vphoto_small);
-		newPhoto = newPhotoLoc.isNull() ? ImagePtr() : ImagePtr(newPhotoLoc);
-//		photoFull = newPhoto ? ImagePtr(640, 640, d.vphoto_big, newPhoto) : ImagePtr();
-	} break;
-	default: {
-		newPhotoId = 0;
-		newPhotoLoc = StorageImageLocation();
-		newPhoto = ImagePtr();
-//		photoFull = ImagePtr();
-	} break;
-	}
-	if (newPhotoId != photoId || newPhoto.v() != _userpic.v() || newPhotoLoc != _userpicLocation) {
-		photoId = newPhotoId;
-		setUserpic(newPhoto, newPhotoLoc);
-		Notify::peerUpdatedDelayed(this, UpdateFlag::PhotoChanged);
+void ChannelData::setPhoto(PhotoId photoId, const MTPChatPhoto &photo) {
+	if (photo.type() == mtpc_chatPhoto) {
+		const auto &data = photo.c_chatPhoto();
+		updateUserpic(photoId, data.vphoto_small);
+	} else {
+		clearUserpic();
 	}
 }
 

Telegram/SourceFiles/data/data_peer.h
@@ -70,8 +70,6 @@ private:
 
 };
 
-static const PhotoId UnknownPeerPhotoId = 0xFFFFFFFFFFFFFFFFULL;
-
 class PeerData;
 
 class PeerClickHandler : public ClickHandler {
@@ -190,7 +188,11 @@ public:
 	LoadedStatus loadedStatus = NotLoaded;
 	MTPinputPeer input;
 
-	void setUserpic(ImagePtr userpic, StorageImageLocation location);
+	void setUserpic(
+		PhotoId photoId,
+		const StorageImageLocation &location,
+		ImagePtr userpic);
+	void setUserpicPhoto(const MTPPhoto &data);
 	void paintUserpic(
 		Painter &p,
 		int x,
@@ -233,8 +235,12 @@ public:
 	StorageImageLocation userpicLocation() const {
 		return _userpicLocation;
 	}
-
-	PhotoId photoId = UnknownPeerPhotoId;
+	bool userpicPhotoUnknown() const {
+		return (_userpicPhotoId == kUnknownPhotoId);
+	}
+	PhotoId userpicPhotoId() const {
+		return userpicPhotoUnknown() ? 0 : _userpicPhotoId;
+	}
 
 	int nameVersion = 1;
 
@@ -259,13 +265,18 @@ protected:
 		const QString &newName,
 		const QString &newNameOrPhone,
 		const QString &newUsername);
-
-	ImagePtr _userpic;
-	mutable EmptyUserpic _userpicEmpty;
-	StorageImageLocation _userpicLocation;
+	void updateUserpic(PhotoId photoId, const MTPFileLocation &location);
+	void clearUserpic();
 
 private:
 	void fillNames();
+
+	static constexpr auto kUnknownPhotoId = PhotoId(0xFFFFFFFFFFFFFFFFULL);
+
+	ImagePtr _userpic;
+	PhotoId _userpicPhotoId = kUnknownPhotoId;
+	mutable EmptyUserpic _userpicEmpty;
+	StorageImageLocation _userpicLocation;
 
 	Data::NotifySettings _notify;
 
@@ -519,9 +530,8 @@ public:
 	: PeerData(id)
 	, inputChat(MTP_int(bareId())) {
 	}
-	void setPhoto(
-		const MTPChatPhoto &photo,
-		const PhotoId &phId = UnknownPeerPhotoId);
+	void setPhoto(const MTPChatPhoto &photo);
+	void setPhoto(PhotoId photoId, const MTPChatPhoto &photo);
 
 	void setName(const QString &newName);
 
@@ -768,9 +778,8 @@ public:
 
 	ChannelData(const PeerId &id);
 
-	void setPhoto(
-		const MTPChatPhoto &photo,
-		const PhotoId &phId = UnknownPeerPhotoId);
+	void setPhoto(const MTPChatPhoto &photo);
+	void setPhoto(PhotoId photoId, const MTPChatPhoto &photo);
 
 	void setName(const QString &name, const QString &username);
 

Telegram/SourceFiles/apiwrap.cpp
@@ -320,15 +320,7 @@ void ApiWrap::gotChatFull(PeerData *peer, const MTPmessages_ChatFull &result, mt
 			} break;
 			}
 		}
-		auto newPhotoId = PhotoId(0);
-		if (auto photo = App::feedPhoto(f.vchat_photo)) {
-			newPhotoId = photo->id;
-			photo->peer = chat;
-		}
-		if (chat->photoId != newPhotoId) {
-			chat->photoId = newPhotoId;
-			Notify::peerUpdatedDelayed(chat, UpdateFlag::PhotoChanged);
-		}
+		chat->setUserpicPhoto(f.vchat_photo);
 		chat->setInviteLink((f.vexported_invite.type() == mtpc_chatInviteExported) ? qs(f.vexported_invite.c_chatInviteExported().vlink) : QString());
 		chat->fullUpdated();
 
@@ -345,15 +337,7 @@ void ApiWrap::gotChatFull(PeerData *peer, const MTPmessages_ChatFull &result, mt
 		auto canEditStickers = channel->canEditStickers();
 
 		channel->setFullFlags(f.vflags.v);
-		auto newPhotoId = PhotoId(0);
-		if (auto photo = App::feedPhoto(f.vchat_photo)) {
-			newPhotoId = photo->id;
-			photo->peer = channel;
-		}
-		if (channel->photoId != newPhotoId) {
-			channel->photoId = newPhotoId;
-			Notify::peerUpdatedDelayed(channel, UpdateFlag::PhotoChanged);
-		}
+		channel->setUserpicPhoto(f.vchat_photo);
 		if (f.has_migrated_from_chat_id()) {
 			channel->addFlags(MTPDchannel::Flag::f_megagroup);
 			auto cfrom = App::chat(peerFromChat(f.vmigrated_from_chat_id));

Telegram/SourceFiles/app.cpp
@@ -1159,28 +1159,6 @@ namespace {
 		return ImagePtr();
 	}
 
-	StorageImageLocation imageLocation(int32 w, int32 h, const MTPFileLocation &loc) {
-		if (loc.type() == mtpc_fileLocation) {
-			const auto &l(loc.c_fileLocation());
-			return StorageImageLocation(w, h, l.vdc_id.v, l.vvolume_id.v, l.vlocal_id.v, l.vsecret.v);
-		}
-		return StorageImageLocation(w, h, 0, 0, 0, 0);
-	}
-
-	StorageImageLocation imageLocation(const MTPPhotoSize &size) {
-		switch (size.type()) {
-		case mtpc_photoSize: {
-			const auto &d(size.c_photoSize());
-			return imageLocation(d.vw.v, d.vh.v, d.vlocation);
-		} break;
-		case mtpc_photoCachedSize: {
-			const auto &d(size.c_photoCachedSize());
-			return imageLocation(d.vw.v, d.vh.v, d.vlocation);
-		} break;
-		}
-		return StorageImageLocation();
-	}
-
 	void feedInboxRead(const PeerId &peer, MsgId upTo) {
 		if (auto history = App::historyLoaded(peer)) {
 			history->inboxRead(upTo);
@@ -1418,7 +1396,18 @@ namespace {
 	}
 
 	DocumentData *feedDocument(const MTPDdocument &document, DocumentData *convert) {
-		return App::documentSet(document.vid.v, convert, document.vaccess_hash.v, document.vversion.v, document.vdate.v, document.vattributes.v, qs(document.vmime_type), App::image(document.vthumb), document.vdc_id.v, document.vsize.v, App::imageLocation(document.vthumb));
+		return App::documentSet(
+			document.vid.v,
+			convert,
+			document.vaccess_hash.v,
+			document.vversion.v,
+			document.vdate.v,
+			document.vattributes.v,
+			qs(document.vmime_type),
+			App::image(document.vthumb),
+			document.vdc_id.v,
+			document.vsize.v,
+			StorageImageLocation::FromMTP(document.vthumb));
 	}
 
 	WebPageData *feedWebPage(const MTPDwebPage &webpage, WebPageData *convert) {

Telegram/SourceFiles/app.h
@@ -82,8 +82,6 @@ namespace App {
 	void feedUserLink(MTPint userId, const MTPContactLink &myLink, const MTPContactLink &foreignLink);
 
 	ImagePtr image(const MTPPhotoSize &size);
-	StorageImageLocation imageLocation(int32 w, int32 h, const MTPFileLocation &loc);
-	StorageImageLocation imageLocation(const MTPPhotoSize &size);
 
 	PhotoData *feedPhoto(const MTPPhoto &photo, const PreparedPhotoThumbs &thumbs);
 	PhotoData *feedPhoto(const MTPPhoto &photo, PhotoData *convert = nullptr);

Telegram/SourceFiles/boxes/confirm_box.cpp
@@ -628,8 +628,12 @@ ConfirmInviteBox::ConfirmInviteBox(QWidget*, const QString &title, bool isChanne
 	}
 	_status->setText(status);
 	if (photo.type() == mtpc_chatPhoto) {
-		auto &d = photo.c_chatPhoto();
-		auto location = App::imageLocation(160, 160, d.vphoto_small);
+		const auto &data = photo.c_chatPhoto();
+		const auto size = 160;
+		const auto location = StorageImageLocation::FromMTP(
+			size,
+			size,
+			data.vphoto_small);
 		if (!location.isNull()) {
 			_photo = ImagePtr(location);
 			if (!_photo->loaded()) {

Telegram/SourceFiles/calls/calls_panel.cpp
@@ -436,20 +436,26 @@ void Panel::processUserPhoto() {
 	if (!_user->userpicLoaded()) {
 		_user->loadUserpic(true);
 	}
-	auto photo = (_user->photoId && _user->photoId != UnknownPeerPhotoId) ? App::photo(_user->photoId) : nullptr;
+	const auto photo = _user->userpicPhotoId()
+		? App::photo(_user->userpicPhotoId())
+		: nullptr;
 	if (isGoodUserPhoto(photo)) {
 		photo->full->load(true);
-	} else {
-		if ((_user->photoId == UnknownPeerPhotoId) || (_user->photoId && (!photo || !photo->date))) {
-			Auth().api().requestFullPeer(_user);
-		}
+	} else if (_user->userpicPhotoUnknown() || (photo && !photo->date)) {
+		Auth().api().requestFullPeer(_user);
 	}
 	refreshUserPhoto();
 }
 
 void Panel::refreshUserPhoto() {
-	auto photo = (_user->photoId && _user->photoId != UnknownPeerPhotoId) ? App::photo(_user->photoId) : nullptr;
-	if (isGoodUserPhoto(photo) && photo->full->loaded() && (photo->id != _userPhotoId || !_userPhotoFull)) {
+	const auto photo = _user->userpicPhotoId()
+		? App::photo(_user->userpicPhotoId())
+		: nullptr;
+	const auto isNewPhoto = [&](not_null<PhotoData*> photo) {
+		return photo->full->loaded()
+			&& (photo->id != _userPhotoId || !_userPhotoFull);
+	};
+	if (isGoodUserPhoto(photo) && isNewPhoto(photo)) {
 		_userPhotoId = photo->id;
 		_userPhotoFull = true;
 		createUserpicCache(photo->full);

Telegram/SourceFiles/data/data_shared_media.cpp
@@ -231,7 +231,8 @@ base::optional<int> SharedMediaWithLastSlice::indexOfImpl(Value value) const {
 	return base::get_if<FullMsgId>(&value)
 		? _slice.indexOf(*base::get_if<FullMsgId>(&value))
 		: (isolatedInSlice()
-			|| (*base::get_if<not_null<PhotoData*>>(&value))->id != _lastPhotoId)
+			|| !_lastPhotoId
+			|| (*base::get_if<not_null<PhotoData*>>(&value))->id != *_lastPhotoId)
 			? base::none
 			: Add(_slice.size() - 1, lastPhotoSkip());
 }
@@ -256,7 +257,7 @@ SharedMediaWithLastSlice::Value SharedMediaWithLastSlice::operator[](int index) 
 	}
 	return (index < _slice.size())
 		? Value(_slice[index])
-		: Value(App::photo(_lastPhotoId));
+		: Value(App::photo(*_lastPhotoId));
 }
 
 base::optional<int> SharedMediaWithLastSlice::distance(
@@ -274,20 +275,23 @@ void SharedMediaWithLastSlice::reverse() {
 	_reversed = !_reversed;
 }
 
-PhotoId SharedMediaWithLastSlice::LastPeerPhotoId(PeerId peerId) {
+base::optional<PhotoId> SharedMediaWithLastSlice::LastPeerPhotoId(
+		PeerId peerId) {
 	if (auto peer = App::peerLoaded(peerId)) {
-		return peer->photoId;
+		return peer->userpicPhotoUnknown()
+			? base::none
+			: base::make_optional(peer->userpicPhotoId());
 	}
-	return UnknownPeerPhotoId;
+	return base::none;
 }
 
 base::optional<bool> SharedMediaWithLastSlice::IsLastIsolated(
 		const SparseIdsMergedSlice &slice,
 		const base::optional<SparseIdsMergedSlice> &ending,
-		PhotoId lastPeerPhotoId) {
-	if (lastPeerPhotoId == UnknownPeerPhotoId) {
+		base::optional<PhotoId> lastPeerPhotoId) {
+	if (!lastPeerPhotoId) {
 		return base::none;
-	} else if (!lastPeerPhotoId) {
+	} else if (!*lastPeerPhotoId) {
 		return false;
 	}
 	return LastFullMsgId(ending ? *ending : slice)
@@ -299,7 +303,7 @@ base::optional<bool> SharedMediaWithLastSlice::IsLastIsolated(
 				: nullptr;
 		}
 		| [](PhotoData *photo) { return photo ? photo->id : 0; }
-		| [&](PhotoId photoId) { return lastPeerPhotoId != photoId; };
+		| [&](PhotoId photoId) { return *lastPeerPhotoId != photoId; };
 }
 
 base::optional<FullMsgId> SharedMediaWithLastSlice::LastFullMsgId(

commit 095b613000983d34ae348ade510e2a6d3c15b13d

Fix userpic removing.
Regression was introduced in 3ffed1ddcf.

Fixes #4152.

Telegram/SourceFiles/data/data_peer.cpp
@@ -277,12 +277,7 @@ void PeerData::updateUserpic(
 	const auto size = kUserpicSize;
 	const auto loc = StorageImageLocation::FromMTP(size, size, location);
 	const auto photo = loc.isNull() ? ImagePtr() : ImagePtr(loc);
-	if (_userpicPhotoId != photoId
-		|| _userpic.v() != photo.v()
-		|| _userpicLocation != loc) {
-		setUserpic(photoId, loc, photo);
-		Notify::peerUpdatedDelayed(this, UpdateFlag::PhotoChanged);
-	}
+	setUserpicChecked(photoId, loc, photo);
 }
 
 void PeerData::clearUserpic() {
@@ -300,6 +295,19 @@ void PeerData::clearUserpic() {
 		}
 		return ImagePtr();
 	}();
+	setUserpicChecked(photoId, loc, photo);
+}
+
+void PeerData::setUserpicChecked(
+		PhotoId photoId,
+		const StorageImageLocation &location,
+		ImagePtr userpic) {
+	if (_userpicPhotoId != photoId
+		|| _userpic.v() != userpic.v()
+		|| _userpicLocation != location) {
+		setUserpic(photoId, location, userpic);
+		Notify::peerUpdatedDelayed(this, UpdateFlag::PhotoChanged);
+	}
 }
 
 void PeerData::fillNames() {

Telegram/SourceFiles/data/data_peer.h
@@ -238,6 +238,11 @@ private:
 	std::unique_ptr<Ui::EmptyUserpic> createEmptyUserpic() const;
 	void refreshEmptyUserpic() const;
 
+	void setUserpicChecked(
+		PhotoId photoId,
+		const StorageImageLocation &location,
+		ImagePtr userpic);
+
 	static constexpr auto kUnknownPhotoId = PhotoId(0xFFFFFFFFFFFFFFFFULL);
 
 	ImagePtr _userpic;
fractal-l/AyuGramDesktop

commit 32a8d7f33420d96ffd93ff29eaa60d9cf8a43930

Send album after cancel of some media uploads.
Also display checks when part of the album medias are uploaded.

Telegram/SourceFiles/storage/file_upload.cpp
@@ -137,7 +137,6 @@ void Uploader::uploadMedia(const FullMsgId &msgId, const SendMediaReady &media) 
 		} else {
 			document = App::feedDocument(media.document, media.photoThumbs.begin().value());
 		}
-		document->status = FileUploading;
 		if (!media.data.isEmpty()) {
 			document->setData(media.data);
 		}
@@ -154,10 +153,10 @@ void Uploader::upload(
 		const std::shared_ptr<FileLoadResult> &file) {
 	if (file->type == SendMediaType::Photo) {
 		auto photo = App::feedPhoto(file->photo, file->photoThumbs);
-		photo->uploadingData = std::make_unique<PhotoData::UploadingData>(file->partssize);
+		photo->uploadingData = std::make_unique<Data::UploadState>(file->partssize);
 	} else if (file->type == SendMediaType::File || file->type == SendMediaType::Audio) {
 		auto document = file->thumb.isNull() ? App::feedDocument(file->document) : App::feedDocument(file->document, file->thumb);
-		document->status = FileUploading;
+		document->uploadingData = std::make_unique<Data::UploadState>(document->size);
 		if (!file->content.isEmpty()) {
 			document->setData(file->content);
 		}
@@ -176,7 +175,7 @@ void Uploader::currentFailed() {
 			emit photoFailed(j->first);
 		} else if (j->second.type() == SendMediaType::File) {
 			const auto document = App::document(j->second.id());
-			if (document->status == FileUploading) {
+			if (document->uploading()) {
 				document->status = FileUploadFailed;
 			}
 			emit documentFailed(j->first);
@@ -477,10 +476,9 @@ void Uploader::partLoaded(const MTPBool &result, mtpRequestId requestId) {
 				if (document->uploading()) {
 					const auto doneParts = file.docSentParts
 						- int(docRequestsSent.size());
-					document->uploadOffset = doneParts * file.docPartSize;
-					if (document->uploadOffset > document->size) {
-						document->uploadOffset = document->size;
-					}
+					document->uploadingData->offset = std::max(
+						document->uploadingData->size,
+						doneParts * file.docPartSize);
 				}
 				emit documentProgress(fullId);
 			}

Telegram/Resources/langs/lang.strings
@@ -1098,10 +1098,10 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 "lng_send_images_selected#other" = "{count} images selected";
 "lng_send_photos#one" = "Send {count} photo";
 "lng_send_photos#other" = "Send {count} photos";
-"lng_send_photos_videos#one" = "Send {count} photo and video file";
-"lng_send_photos_videos#other" = "Send {count} photos and video files";
+"lng_send_photos_videos#one" = "Send {count} photo and video";
+"lng_send_photos_videos#other" = "Send {count} photos and videos";
 "lng_send_separate_photos" = "Send separate photos";
-"lng_send_separate_photos_videos" = "Send separate photos and video files";
+"lng_send_separate_photos_videos" = "Send separate photos and videos";
 "lng_send_files_selected#one" = "{count} file selected";
 "lng_send_files_selected#other" = "{count} files selected";
 "lng_send_files#one" = "Send {count} file";

Telegram/SourceFiles/apiwrap.cpp
@@ -2743,6 +2743,9 @@ void ApiWrap::sendFiles(
 	}
 
 	const auto to = FileLoadTaskOptions(options);
+	if (album) {
+		album->silent = to.silent;
+	}
 	auto tasks = std::vector<std::unique_ptr<Task>>();
 	tasks.reserve(list.files.size());
 	for (auto &file : list.files) {
@@ -2806,7 +2809,7 @@ void ApiWrap::sendUploadedPhoto(
 			MTPVector<MTPInputDocument>(),
 			MTP_int(0));
 		if (const auto groupId = item->groupId()) {
-			uploadAlbumMedia(item, groupId, media, silent);
+			uploadAlbumMedia(item, groupId, media);
 		} else {
 			sendMedia(item, media, silent);
 		}
@@ -2840,7 +2843,7 @@ void ApiWrap::sendUploadedDocument(
 				MTPVector<MTPInputDocument>(),
 				MTP_int(0));
 			if (groupId) {
-				uploadAlbumMedia(item, groupId, media, silent);
+				uploadAlbumMedia(item, groupId, media);
 			} else {
 				sendMedia(item, media, silent);
 			}
@@ -2848,11 +2851,18 @@ void ApiWrap::sendUploadedDocument(
 	}
 }
 
+void ApiWrap::cancelLocalItem(not_null<HistoryItem*> item) {
+	Expects(!IsServerMsgId(item->id));
+
+	if (const auto groupId = item->groupId()) {
+		sendAlbumWithCancelled(item, groupId);
+	}
+}
+
 void ApiWrap::uploadAlbumMedia(
 		not_null<HistoryItem*> item,
 		const MessageGroupId &groupId,
-		const MTPInputMedia &media,
-		bool silent) {
+		const MTPInputMedia &media) {
 	const auto localId = item->fullId();
 	const auto failed = [this] {
 
@@ -2864,6 +2874,13 @@ void ApiWrap::uploadAlbumMedia(
 		const auto item = App::histItemById(localId);
 		if (!item) {
 			failed();
+		}
+		if (const auto media = item->getMedia()) {
+			if (const auto photo = media->getPhoto()) {
+				photo->setWaitingForAlbum();
+			} else if (const auto document = media->getDocument()) {
+				document->setWaitingForAlbum();
+			}
 		}
 
 		switch (result.type()) {
@@ -2883,7 +2900,7 @@ void ApiWrap::uploadAlbumMedia(
 				MTP_inputPhoto(photo.vid, photo.vaccess_hash),
 				data.has_caption() ? data.vcaption : MTP_string(QString()),
 				data.has_ttl_seconds() ? data.vttl_seconds : MTPint());
-			trySendAlbum(item, groupId, media, silent);
+			sendAlbumWithUploaded(item, groupId, media);
 		} break;
 
 		case mtpc_messageMediaDocument: {
@@ -2902,46 +2919,12 @@ void ApiWrap::uploadAlbumMedia(
 				MTP_inputDocument(document.vid, document.vaccess_hash),
 				data.has_caption() ? data.vcaption : MTP_string(QString()),
 				data.has_ttl_seconds() ? data.vttl_seconds : MTPint());
-			trySendAlbum(item, groupId, media, silent);
+			sendAlbumWithUploaded(item, groupId, media);
 		} break;
 		}
 	}).fail([=](const RPCError &error) {
 		failed();
 	}).send();
-}
-
-void ApiWrap::trySendAlbum(
-		not_null<HistoryItem*> item,
-		const MessageGroupId &groupId,
-		const MTPInputMedia &media,
-		bool silent) {
-	const auto localId = item->fullId();
-	const auto randomId = rand_value<uint64>();
-	App::historyRegRandom(randomId, localId);
-
-	const auto medias = completeAlbum(localId, groupId, media, randomId);
-	if (medias.empty()) {
-		return;
-	}
-
-	const auto history = item->history();
-	const auto replyTo = item->replyToId();
-	const auto flags = MTPmessages_SendMultiMedia::Flags(0)
-		| (replyTo
-			? MTPmessages_SendMultiMedia::Flag::f_reply_to_msg_id
-			: MTPmessages_SendMultiMedia::Flag(0))
-		| (IsSilentPost(item, silent)
-			? MTPmessages_SendMultiMedia::Flag::f_silent
-			: MTPmessages_SendMultiMedia::Flag(0));
-	history->sendRequestId = request(MTPmessages_SendMultiMedia(
-		MTP_flags(flags),
-		history->peer->input,
-		MTP_int(replyTo),
-		MTP_vector<MTPInputSingleMedia>(medias)
-	)).done([=](const MTPUpdates &result) { applyUpdates(result);
-	}).fail([=](const RPCError &error) { sendMessageFail(error);
-	}).afterRequest(history->sendRequestId
-	).send();
 }
 
 void ApiWrap::sendMedia(
@@ -2951,6 +2934,14 @@ void ApiWrap::sendMedia(
 	const auto randomId = rand_value<uint64>();
 	App::historyRegRandom(randomId, item->fullId());
 
+	sendMediaWithRandomId(item, media, silent, randomId);
+}
+
+void ApiWrap::sendMediaWithRandomId(
+		not_null<HistoryItem*> item,
+		const MTPInputMedia &media,
+		bool silent,
+		uint64 randomId) {
 	const auto history = item->history();
 	const auto replyTo = item->replyToId();
 	const auto flags = MTPmessages_SendMedia::Flags(0)
@@ -2973,11 +2964,14 @@ void ApiWrap::sendMedia(
 	).send();
 }
 
-QVector<MTPInputSingleMedia> ApiWrap::completeAlbum(
-		FullMsgId localId,
+void ApiWrap::sendAlbumWithUploaded(
+		not_null<HistoryItem*> item,
 		const MessageGroupId &groupId,
-		const MTPInputMedia &media,
-		uint64 randomId) {
+		const MTPInputMedia &media) {
+	const auto localId = item->fullId();
+	const auto randomId = rand_value<uint64>();
+	App::historyRegRandom(randomId, localId);
+
 	const auto albumIt = _sendingAlbums.find(groupId.raw());
 	Assert(albumIt != _sendingAlbums.end());
 	const auto &album = albumIt->second;
@@ -2990,15 +2984,79 @@ QVector<MTPInputSingleMedia> ApiWrap::completeAlbum(
 	Assert(!itemIt->media);
 	itemIt->media = MTP_inputSingleMedia(media, MTP_long(randomId));
 
-	auto result = QVector<MTPInputSingleMedia>();
-	result.reserve(album->items.size());
+	sendAlbumIfReady(album.get());
+}
+
+void ApiWrap::sendAlbumWithCancelled(
+		not_null<HistoryItem*> item,
+		const MessageGroupId &groupId) {
+	const auto localId = item->fullId();
+	const auto albumIt = _sendingAlbums.find(groupId.raw());
+	Assert(albumIt != _sendingAlbums.end());
+	const auto &album = albumIt->second;
+
+	const auto proj = [](const SendingAlbum::Item &item) {
+		return item.msgId;
+	};
+	const auto itemIt = ranges::find(album->items, localId, proj);
+	Assert(itemIt != album->items.end());
+	album->items.erase(itemIt);
+
+	sendAlbumIfReady(album.get());
+}
+
+void ApiWrap::sendAlbumIfReady(not_null<SendingAlbum*> album) {
+	const auto groupId = album->groupId;
+	if (album->items.empty()) {
+		_sendingAlbums.remove(groupId);
+		return;
+	}
+	auto sample = (HistoryItem*)nullptr;
+	auto medias = QVector<MTPInputSingleMedia>();
+	medias.reserve(album->items.size());
 	for (const auto &item : album->items) {
 		if (!item.media) {
-			return {};
+			return;
+		} else if (!sample) {
+			sample = App::histItemById(item.msgId);
 		}
-		result.push_back(*item.media);
+		medias.push_back(*item.media);
 	}
-	return result;
+	if (!sample) {
+		_sendingAlbums.remove(groupId);
+		return;
+	} else if (medias.size() < 2) {
+		const auto &single = medias.front().c_inputSingleMedia();
+		sendMediaWithRandomId(
+			sample,
+			single.vmedia,
+			album->silent,
+			single.vrandom_id.v);
+		_sendingAlbums.remove(groupId);
+		return;
+	}
+	const auto history = sample->history();
+	const auto replyTo = sample->replyToId();
+	const auto flags = MTPmessages_SendMultiMedia::Flags(0)
+		| (replyTo
+			? MTPmessages_SendMultiMedia::Flag::f_reply_to_msg_id
+			: MTPmessages_SendMultiMedia::Flag(0))
+		| (IsSilentPost(sample, album->silent)
+			? MTPmessages_SendMultiMedia::Flag::f_silent
+			: MTPmessages_SendMultiMedia::Flag(0));
+	history->sendRequestId = request(MTPmessages_SendMultiMedia(
+		MTP_flags(flags),
+		history->peer->input,
+		MTP_int(replyTo),
+		MTP_vector<MTPInputSingleMedia>(medias)
+	)).done([=](const MTPUpdates &result) {
+		_sendingAlbums.remove(groupId);
+		applyUpdates(result);
+	}).fail([=](const RPCError &error) {
+		_sendingAlbums.remove(groupId);
+		sendMessageFail(error);
+	}).afterRequest(history->sendRequestId
+	).send();
 }
 
 void ApiWrap::readServerHistory(not_null<History*> history) {

Telegram/SourceFiles/apiwrap.h
@@ -215,6 +215,7 @@ public:
 		const MTPInputFile &file,
 		const base::optional<MTPInputFile> &thumb,
 		bool silent);
+	void cancelLocalItem(not_null<HistoryItem*> item);
 
 	~ApiWrap();
 
@@ -317,22 +318,24 @@ private:
 	void uploadAlbumMedia(
 		not_null<HistoryItem*> item,
 		const MessageGroupId &groupId,
-		const MTPInputMedia &media,
-		bool silent);
-	void trySendAlbum(
+		const MTPInputMedia &media);
+	void sendAlbumWithUploaded(
 		not_null<HistoryItem*> item,
 		const MessageGroupId &groupId,
-		const MTPInputMedia &media,
-		bool silent);
-	QVector<MTPInputSingleMedia> completeAlbum(
-		FullMsgId localId,
-		const MessageGroupId &groupId,
-		const MTPInputMedia &media,
-		uint64 randomId);
+		const MTPInputMedia &media);
+	void sendAlbumWithCancelled(
+		not_null<HistoryItem*> item,
+		const MessageGroupId &groupId);
+	void sendAlbumIfReady(not_null<SendingAlbum*> album);
 	void sendMedia(
 		not_null<HistoryItem*> item,
 		const MTPInputMedia &media,
 		bool silent);
+	void sendMediaWithRandomId(
+		not_null<HistoryItem*> item,
+		const MTPInputMedia &media,
+		bool silent,
+		uint64 randomId);
 
 	not_null<AuthSession*> _session;
 	mtpRequestId _changelogSubscription = 0;

Telegram/SourceFiles/app.cpp
@@ -1375,17 +1375,24 @@ namespace {
 	}
 
 	PhotoData *photo(const PhotoId &photo) {
-		PhotosData::const_iterator i = ::photosData.constFind(photo);
+		auto i = ::photosData.constFind(photo);
 		if (i == ::photosData.cend()) {
 			i = ::photosData.insert(photo, new PhotoData(photo));
 		}
 		return i.value();
 	}
 
-	PhotoData *photoSet(const PhotoId &photo, PhotoData *convert, const uint64 &access, int32 date, const ImagePtr &thumb, const ImagePtr &medium, const ImagePtr &full) {
+	PhotoData *photoSet(
+			const PhotoId &photo,
+			PhotoData *convert,
+			const uint64 &access,
+			int32 date,
+			const ImagePtr &thumb,
+			const ImagePtr &medium,
+			const ImagePtr &full) {
 		if (convert) {
 			if (convert->id != photo) {
-				PhotosData::iterator i = ::photosData.find(convert->id);
+				const auto i = ::photosData.find(convert->id);
 				if (i != ::photosData.cend() && i.value() == convert) {
 					::photosData.erase(i);
 				}
@@ -1400,9 +1407,9 @@ namespace {
 				updateImage(convert->full, full);
 			}
 		}
-		PhotosData::const_iterator i = ::photosData.constFind(photo);
+		const auto i = ::photosData.constFind(photo);
 		PhotoData *result;
-		LastPhotosMap::iterator inLastIter = lastPhotosMap.end();
+		auto inLastIter = lastPhotosMap.end();
 		if (i == ::photosData.cend()) {
 			if (convert) {
 				result = convert;
@@ -1436,27 +1443,39 @@ namespace {
 	}
 
 	DocumentData *document(const DocumentId &document) {
-		DocumentsData::const_iterator i = ::documentsData.constFind(document);
+		auto i = ::documentsData.constFind(document);
 		if (i == ::documentsData.cend()) {
 			i = ::documentsData.insert(document, DocumentData::create(document));
 		}
 		return i.value();
 	}
 
-	DocumentData *documentSet(const DocumentId &document, DocumentData *convert, const uint64 &access, int32 version, int32 date, const QVector<MTPDocumentAttribute> &attributes, const QString &mime, const ImagePtr &thumb, int32 dc, int32 size, const StorageImageLocation &thumbLocation) {
+	DocumentData *documentSet(
+			const DocumentId &document,
+			DocumentData *convert,
+			const uint64 &access,
+			int32 version,
+			int32 date,
+			const QVector<MTPDocumentAttribute> &attributes,
+			const QString &mime,
+			const ImagePtr &thumb,
+			int32 dc,
+			int32 size,
+			const StorageImageLocation &thumbLocation) {
 		bool versionChanged = false;
 		bool sentSticker = false;
 		if (convert) {
-			MediaKey oldKey = convert->mediaKey();
-			bool idChanged = (convert->id != document);
+			const auto oldKey = convert->mediaKey();
+			const auto idChanged = (convert->id != document);
 			if (idChanged) {
-				DocumentsData::iterator i = ::documentsData.find(convert->id);
+				const auto i = ::documentsData.find(convert->id);
 				if (i != ::documentsData.cend() && i.value() == convert) {
 					::documentsData.erase(i);
 				}
 
 				convert->id = document;
 				convert->status = FileReady;
+				convert->uploadingData = nullptr;
 				sentSticker = (convert->sticker() != 0);
 			}
 			if (date) {
@@ -1474,7 +1493,7 @@ namespace {
 					convert->sticker()->loc = thumbLocation;
 				}
 
-				MediaKey newKey = convert->mediaKey();
+				const auto newKey = convert->mediaKey();
 				if (idChanged) {
 					if (convert->isVoiceMessage()) {
 						Local::copyAudio(oldKey, newKey);
@@ -1488,7 +1507,7 @@ namespace {
 				Local::writeSavedGifs();
 			}
 		}
-		DocumentsData::const_iterator i = ::documentsData.constFind(document);
+		const auto i = ::documentsData.constFind(document);
 		DocumentData *result;
 		if (i == ::documentsData.cend()) {
 			if (convert) {
@@ -1565,10 +1584,23 @@ namespace {
 		return i.value();
 	}
 
-	WebPageData *webPageSet(const WebPageId &webPage, WebPageData *convert, const QString &type, const QString &url, const QString &displayUrl, const QString &siteName, const QString &title, const TextWithEntities &description, PhotoData *photo, DocumentData *document, int32 duration, const QString &author, int32 pendingTill) {
+	WebPageData *webPageSet(
+			const WebPageId &webPage,
+			WebPageData *convert,
+			const QString &type,
+			const QString &url,
+			const QString &displayUrl,
+			const QString &siteName,
+			const QString &title,
+			const TextWithEntities &description,
+			PhotoData *photo,
+			DocumentData *document,
+			int32 duration,
+			const QString &author,
+			int32 pendingTill) {
 		if (convert) {
 			if (convert->id != webPage) {
-				auto i = webPagesData.find(convert->id);
+				const auto i = webPagesData.find(convert->id);
 				if (i != webPagesData.cend() && i.value() == convert) {
 					webPagesData.erase(i);
 				}
@@ -1592,7 +1624,7 @@ namespace {
 				if (App::main()) App::main()->webPageUpdated(convert);
 			}
 		}
-		auto i = webPagesData.constFind(webPage);
+		const auto i = webPagesData.constFind(webPage);
 		WebPageData *result;
 		if (i == webPagesData.cend()) {
 			if (convert) {

Telegram/SourceFiles/data/data_document.cpp
@@ -332,28 +332,15 @@ void DocumentOpenClickHandler::doOpen(
 }
 
 void DocumentOpenClickHandler::onClickImpl() const {
-	const auto item = context()
-		? App::histItemById(context())
-		: App::hoveredLinkItem()
-		? App::hoveredLinkItem()
-		: App::contextItem()
-		? App::contextItem()
-		: nullptr;
-	const auto action = document()->isVoiceMessage()
+	const auto data = document();
+	const auto action = data->isVoiceMessage()
 		? ActionOnLoadNone
 		: ActionOnLoadOpen;
-	doOpen(document(), item, action);
+	doOpen(data, getActionItem(), action);
 }
 
 void GifOpenClickHandler::onClickImpl() const {
-	const auto item = context()
-		? App::histItemById(context())
-		: App::hoveredLinkItem()
-		? App::hoveredLinkItem()
-		: App::contextItem()
-		? App::contextItem()
-		: nullptr;
-	doOpen(document(), item, ActionOnLoadPlayInline);
+	doOpen(document(), getActionItem(), ActionOnLoadPlayInline);
 }
 
 void DocumentSaveClickHandler::doSave(
@@ -382,17 +369,13 @@ void DocumentSaveClickHandler::onClickImpl() const {
 }
 
 void DocumentCancelClickHandler::onClickImpl() const {
-	auto data = document();
+	const auto data = document();
 	if (!data->date) return;
 
 	if (data->uploading()) {
-		if (auto item = App::hoveredLinkItem() ? App::hoveredLinkItem() : (App::contextItem() ? App::contextItem() : nullptr)) {
-			if (auto media = item->getMedia()) {
-				if (media->getDocument() == data) {
-					App::contextItem(item);
-					App::main()->cancelUploadLayer();
-				}
-			}
+		if (const auto item = App::histItemById(context())) {
+			App::contextItem(item);
+			App::main()->cancelUploadLayer();
 		}
 	} else {
 		data->cancel();
@@ -685,12 +668,19 @@ QString DocumentData::loadingFilePath() const {
 }
 
 bool DocumentData::displayLoading() const {
-	return loading() ? (!_loader->loadingLocal() || !_loader->autoLoading()) : uploading();
+	return loading()
+		? (!_loader->loadingLocal() || !_loader->autoLoading())
+		: (uploading() && !waitingForAlbum());
 }
 
 float64 DocumentData::progress() const {
 	if (uploading()) {
-		return snap((size > 0) ? float64(uploadOffset) / size : 0., 0., 1.);
+		if (uploadingData->size > 0) {
+			const auto result = float64(uploadingData->offset)
+				/ uploadingData->size;
+			return snap(result, 0., 1.);
+		}
+		return 0.;
 	}
 	return loading() ? _loader->currentProgress() : (loaded() ? 1. : 0.);
 }
@@ -700,7 +690,17 @@ int32 DocumentData::loadOffset() const {
 }
 
 bool DocumentData::uploading() const {
-	return status == FileUploading;
+	return (uploadingData != nullptr);
+}
+
+void DocumentData::setWaitingForAlbum() {
+	if (uploading()) {
+		uploadingData->waitingForAlbum = true;
+	}
+}
+
+bool DocumentData::waitingForAlbum() const {
+	return uploading() && uploadingData->waitingForAlbum;
 }
 
 void DocumentData::save(const QString &toFile, ActionOnLoad action, const FullMsgId &actionMsgId, LoadFromCloudSetting fromCloud, bool autoLoading) {

Telegram/SourceFiles/data/data_document.h
@@ -125,6 +125,9 @@ public:
 	int32 loadOffset() const;
 	bool uploading() const;
 
+	void setWaitingForAlbum();
+	bool waitingForAlbum() const;
+
 	QByteArray data() const;
 	const FileLocation &location(bool check = false) const;
 	void setLocation(const FileLocation &loc);
@@ -241,7 +244,8 @@ public:
 	int32 size = 0;
 
 	FileStatus status = FileReady;
-	int32 uploadOffset = 0;
+
+	std::unique_ptr<Data::UploadState> uploadingData;
 
 	int32 md5[8];
 

Telegram/SourceFiles/data/data_photo.cpp
@@ -63,7 +63,9 @@ bool PhotoData::loading() const {
 }
 
 bool PhotoData::displayLoading() const {
-	return full->loading() ? full->displayLoading() : uploading();
+	return full->loading()
+		? full->displayLoading()
+		: (uploading() && !waitingForAlbum());
 }
 
 void PhotoData::cancel() {
@@ -91,12 +93,22 @@ float64 PhotoData::progress() const {
 	return full->progress();
 }
 
+void PhotoData::setWaitingForAlbum() {
+	if (uploading()) {
+		uploadingData->waitingForAlbum = true;
+	}
+}
+
+bool PhotoData::waitingForAlbum() const {
+	return uploading() && uploadingData->waitingForAlbum;
+}
+
 int32 PhotoData::loadOffset() const {
 	return full->loadOffset();
 }
 
 bool PhotoData::uploading() const {
-	return !!uploadingData;
+	return (uploadingData != nullptr);
 }
 
 void PhotoData::forget() {

commit 021511141e5f781a9efb03e6e391778c8e7f4333

Fix file upload progress display.
Regression was introduced in 32a8d7f334.

Telegram/SourceFiles/storage/file_upload.cpp
@@ -476,7 +476,7 @@ void Uploader::partLoaded(const MTPBool &result, mtpRequestId requestId) {
 				if (document->uploading()) {
 					const auto doneParts = file.docSentParts
 						- int(docRequestsSent.size());
-					document->uploadingData->offset = std::max(
+					document->uploadingData->offset = std::min(
 						document->uploadingData->size,
 						doneParts * file.docPartSize);
 				}
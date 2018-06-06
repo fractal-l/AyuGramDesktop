fractal-l/AyuGramDesktop

commit 860063e5a47aee0c4e0fa1c1a5eb65afd5e4282d

Pass modifiers inside InputField submitted signal.
Also use non-MOC connections for all InputFields.
Also use Ctrl/Cmd + Enter to submit fast share box.
Fixes #4769.

Telegram/SourceFiles/boxes/edit_color_box.cpp
@@ -630,34 +630,36 @@ EditColorBox::EditColorBox(QWidget*, const QString &title, QColor current) : Box
 }
 
 void EditColorBox::prepare() {
-	setTitle([this] { return _title; });
+	setTitle([=] { return _title; });
 
-	connect(_hueField, SIGNAL(changed()), this, SLOT(onFieldChanged()));
-	connect(_saturationField, SIGNAL(changed()), this, SLOT(onFieldChanged()));
-	connect(_brightnessField, SIGNAL(changed()), this, SLOT(onFieldChanged()));
-	connect(_redField, SIGNAL(changed()), this, SLOT(onFieldChanged()));
-	connect(_greenField, SIGNAL(changed()), this, SLOT(onFieldChanged()));
-	connect(_blueField, SIGNAL(changed()), this, SLOT(onFieldChanged()));
-	connect(_result, SIGNAL(changed()), this, SLOT(onFieldChanged()));
+	const auto changed = [=] { fieldChanged(); };
+	connect(_hueField, &Ui::MaskedInputField::changed, changed);
+	connect(_saturationField, &Ui::MaskedInputField::changed, changed);
+	connect(_brightnessField, &Ui::MaskedInputField::changed, changed);
+	connect(_redField, &Ui::MaskedInputField::changed, changed);
+	connect(_greenField, &Ui::MaskedInputField::changed, changed);
+	connect(_blueField, &Ui::MaskedInputField::changed, changed);
+	connect(_result, &Ui::MaskedInputField::changed, changed);
 
-	connect(_hueField, SIGNAL(submitted(bool)), this, SLOT(onFieldSubmitted()));
-	connect(_saturationField, SIGNAL(submitted(bool)), this, SLOT(onFieldSubmitted()));
-	connect(_brightnessField, SIGNAL(submitted(bool)), this, SLOT(onFieldSubmitted()));
-	connect(_redField, SIGNAL(submitted(bool)), this, SLOT(onFieldSubmitted()));
-	connect(_greenField, SIGNAL(submitted(bool)), this, SLOT(onFieldSubmitted()));
-	connect(_blueField, SIGNAL(submitted(bool)), this, SLOT(onFieldSubmitted()));
-	connect(_result, SIGNAL(submitted(bool)), this, SLOT(onFieldSubmitted()));
+	const auto submitted = [=] { fieldSubmitted(); };
+	connect(_hueField, &Ui::MaskedInputField::submitted, submitted);
+	connect(_saturationField, &Ui::MaskedInputField::submitted, submitted);
+	connect(_brightnessField, &Ui::MaskedInputField::submitted, submitted);
+	connect(_redField, &Ui::MaskedInputField::submitted, submitted);
+	connect(_greenField, &Ui::MaskedInputField::submitted, submitted);
+	connect(_blueField, &Ui::MaskedInputField::submitted, submitted);
+	connect(_result, &Ui::MaskedInputField::submitted, submitted);
 
-	addButton(langFactory(lng_settings_save), [this] { saveColor(); });
-	addButton(langFactory(lng_cancel), [this] { closeBox(); });
+	addButton(langFactory(lng_settings_save), [=] { saveColor(); });
+	addButton(langFactory(lng_cancel), [=] { closeBox(); });
 
 	auto height = st::colorEditSkip + st::colorPickerSize + st::colorEditSkip + st::colorSliderWidth + st::colorEditSkip;
 	setDimensions(st::colorEditWidth, height);
 
-	subscribe(_picker->changed(), [this] { updateFromControls(); });
-	subscribe(_hueSlider->changed(), [this] { updateFromControls(); });
-	subscribe(_opacitySlider->changed(), [this] { updateFromControls(); });
-	subscribe(boxClosing, [this] {
+	subscribe(_picker->changed(), [=] { updateFromControls(); });
+	subscribe(_hueSlider->changed(), [=] { updateFromControls(); });
+	subscribe(_opacitySlider->changed(), [=] { updateFromControls(); });
+	subscribe(boxClosing, [=] {
 		if (_cancelCallback) {
 			_cancelCallback();
 		}
@@ -670,7 +672,7 @@ void EditColorBox::setInnerFocus() {
 	_result->selectAll();
 }
 
-void EditColorBox::onFieldChanged() {
+void EditColorBox::fieldChanged() {
 	auto emitter = sender();
 	auto checkHSVSender = [this, emitter](QObject *field) {
 		if (emitter == field) {
@@ -693,7 +695,7 @@ void EditColorBox::onFieldChanged() {
 	}
 }
 
-void EditColorBox::onFieldSubmitted() {
+void EditColorBox::fieldSubmitted() {
 	Ui::MaskedInputField *fields[] = {
 		_hueField,
 		_saturationField,

Telegram/SourceFiles/boxes/edit_color_box.h
@@ -10,8 +10,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "boxes/abstract_box.h"
 
 class EditColorBox : public BoxContent {
-	Q_OBJECT
-
 public:
 	EditColorBox(QWidget*, const QString &title, QColor current = QColor(255, 255, 255));
 
@@ -37,12 +35,10 @@ protected:
 
 	void setInnerFocus() override;
 
-private slots:
-	void onFieldChanged();
-	void onFieldSubmitted();
-
 private:
 	void saveColor();
+	void fieldChanged();
+	void fieldSubmitted();
 
 	void updateFromColor(QColor color);
 	void updateControlsFromColor();

Telegram/SourceFiles/boxes/add_contact_box.cpp
@@ -122,9 +122,9 @@ void AddContactBox::prepare() {
 	}
 	updateButtons();
 
-	connect(_first, SIGNAL(submitted(bool)), this, SLOT(onSubmit()));
-	connect(_last, SIGNAL(submitted(bool)), this, SLOT(onSubmit()));
-	connect(_phone, SIGNAL(submitted(bool)), this, SLOT(onSubmit()));
+	connect(_first, &Ui::InputField::submitted, [=] { submit(); });
+	connect(_last, &Ui::InputField::submitted, [=] { submit(); });
+	connect(_phone, &Ui::PhoneInput::submitted, [=] { submit(); });
 
 	setDimensions(st::boxWideWidth, st::contactPadding.top() + _first->height() + st::contactSkip + _last->height() + st::contactPhoneSkip + _phone->height() + st::contactPadding.bottom() + st::boxPadding.bottom());
 }
@@ -170,21 +170,21 @@ void AddContactBox::resizeEvent(QResizeEvent *e) {
 	}
 }
 
-void AddContactBox::onSubmit() {
+void AddContactBox::submit() {
 	if (_first->hasFocus()) {
 		_last->setFocus();
 	} else if (_last->hasFocus()) {
 		if (_phone->isEnabled()) {
 			_phone->setFocus();
 		} else {
-			onSave();
+			save();
 		}
 	} else if (_phone->hasFocus()) {
-		onSave();
+		save();
 	}
 }
 
-void AddContactBox::onSave() {
+void AddContactBox::save() {
 	if (_addRequest) return;
 
 	auto firstName = TextUtilities::PrepareForSending(_first->getLastText());
@@ -274,7 +274,7 @@ void AddContactBox::onSaveUserDone(const MTPcontacts_ImportedContacts &res) {
 	closeBox();
 }
 
-void AddContactBox::onRetry() {
+void AddContactBox::retry() {
 	_addRequest = 0;
 	_contactId = 0;
 	showChildren();
@@ -291,9 +291,9 @@ void AddContactBox::onRetry() {
 void AddContactBox::updateButtons() {
 	clearButtons();
 	if (_retrying) {
-		addButton(langFactory(lng_try_other_contact), [this] { onRetry(); });
+		addButton(langFactory(lng_try_other_contact), [this] { retry(); });
 	} else {
-		addButton(langFactory(_user ? lng_settings_save : lng_add_contact), [this] { onSave(); });
+		addButton(langFactory(_user ? lng_settings_save : lng_add_contact), [this] { save(); });
 		addButton(langFactory(lng_cancel), [this] { closeBox(); });
 	}
 }
@@ -335,14 +335,14 @@ void GroupInfoBox::prepare() {
 		_description->setInstantReplacesEnabled(
 			Global::ReplaceEmojiValue());
 
-		connect(_description, SIGNAL(resized()), this, SLOT(onDescriptionResized()));
-		connect(_description, SIGNAL(submitted(bool)), this, SLOT(onNext()));
-		connect(_description, SIGNAL(cancelled()), this, SLOT(onClose()));
+		connect(_description, &Ui::InputField::resized, [=] { descriptionResized(); });
+		connect(_description, &Ui::InputField::submitted, [=] { submit(); });
+		connect(_description, &Ui::InputField::cancelled, [=] { closeBox(); });
 	}
 
-	connect(_title, SIGNAL(submitted(bool)), this, SLOT(onNameSubmit()));
+	connect(_title, &Ui::InputField::submitted, [=] { submitName(); });
 
-	addButton(langFactory(_creating == CreatingGroupChannel ? lng_create_group_create : lng_create_group_next), [this] { onNext(); });
+	addButton(langFactory(_creating == CreatingGroupChannel ? lng_create_group_create : lng_create_group_next), [this] { submit(); });
 	addButton(langFactory(_fromTypeChoose ? lng_create_group_back : lng_cancel), [this] { closeBox(); });
 
 	updateMaxHeight();
@@ -373,14 +373,14 @@ void GroupInfoBox::resizeEvent(QResizeEvent *e) {
 	}
 }
 
-void GroupInfoBox::onNameSubmit() {
+void GroupInfoBox::submitName() {
 	if (_title->getLastText().trimmed().isEmpty()) {
 		_title->setFocus();
 		_title->showError();
 	} else if (_description) {
 		_description->setFocus();
 	} else {
-		onNext();
+		submit();
 	}
 }
 
@@ -460,7 +460,7 @@ void GroupInfoBox::createGroup(not_null<PeerListBox*> selectUsersBox, const QStr
 	}).send();
 }
 
-void GroupInfoBox::onNext() {
+void GroupInfoBox::submit() {
 	if (_creationRequestId) return;
 
 	auto title = TextUtilities::PrepareForSending(_title->getLastText());
@@ -560,7 +560,7 @@ void GroupInfoBox::createChannel(const QString &title, const QString &descriptio
 	}).send();
 }
 
-void GroupInfoBox::onDescriptionResized() {
+void GroupInfoBox::descriptionResized() {
 	updateMaxHeight();
 	update();
 }
@@ -598,14 +598,14 @@ void SetupChannelBox::prepare() {
 
 	_checkRequestId = MTP::send(MTPchannels_CheckUsername(_channel->inputChannel, MTP_string("preston")), RPCDoneHandlerPtr(), rpcFail(&SetupChannelBox::onFirstCheckFail));
 
-	addButton(langFactory(lng_settings_save), [this] { onSave(); });
-	addButton(langFactory(_existing ? lng_cancel : lng_create_group_skip), [this] { closeBox(); });
+	addButton(langFactory(lng_settings_save), [=] { save(); });
+	addButton(langFactory(_existing ? lng_cancel : lng_create_group_skip), [=] { closeBox(); });
 
-	connect(_link, SIGNAL(changed()), this, SLOT(onChange()));
+	connect(_link, &Ui::MaskedInputField::changed, [=] { handleChange(); });
 	_link->setVisible(_privacyGroup->value() == Privacy::Public);
 
 	_checkTimer.setSingleShot(true);
-	connect(&_checkTimer, SIGNAL(timeout()), this, SLOT(onCheck()));
+	connect(&_checkTimer, &QTimer::timeout, [=] { check(); });
 
 	_privacyGroup->setChangedCallback([this](Privacy value) { privacyChanged(value); });
 	subscribe(Notify::PeerUpdated(), Notify::PeerUpdatedHandler(Notify::PeerUpdate::Flag::InviteLinkChanged, [this](const Notify::PeerUpdate &update) {
@@ -644,7 +644,7 @@ void SetupChannelBox::keyPressEvent(QKeyEvent *e) {
 				_link->setFocus();
 				_link->showError();
 			} else {
-				onSave();
+				save();
 			}
 		}
 	} else {
@@ -733,7 +733,7 @@ void SetupChannelBox::updateSelected(const QPoint &cursorGlobalPosition) {
 	}
 }
 
-void SetupChannelBox::onSave() {
+void SetupChannelBox::save() {
 	if (_privacyGroup->value() == Privacy::Private) {
 		if (_existing) {
 			_sentUsername = QString();
@@ -756,7 +756,7 @@ void SetupChannelBox::onSave() {
 	_saveRequestId = MTP::send(MTPchannels_UpdateUsername(_channel->inputChannel, MTP_string(_sentUsername)), rpcDone(&SetupChannelBox::onUpdateDone), rpcFail(&SetupChannelBox::onUpdateFail));
 }
 
-void SetupChannelBox::onChange() {
+void SetupChannelBox::handleChange() {
 	QString name = _link->text().trimmed();
 	if (name.isEmpty()) {
 		if (!_errorText.isEmpty() || !_goodText.isEmpty()) {
@@ -793,7 +793,7 @@ void SetupChannelBox::onChange() {
 	}
 }
 
-void SetupChannelBox::onCheck() {
+void SetupChannelBox::check() {
 	if (_checkRequestId) {
 		MTP::cancel(_checkRequestId);
 	}
@@ -816,7 +816,7 @@ void SetupChannelBox::privacyChanged(Privacy value) {
 			Ui::show(Box<RevokePublicLinkBox>(base::lambda_guarded(this, [this] {
 				_tooMuchUsernames = false;
 				_privacyGroup->setValue(Privacy::Public);
-				onCheck();
+				check();
 			})), LayerOption::KeepOther);
 			return;
 		}
@@ -952,16 +952,16 @@ void EditNameBox::prepare() {
 	newHeight += st::boxPadding.bottom() + st::contactPadding.bottom();
 	setDimensions(st::boxWideWidth, newHeight);
 
-	addButton(langFactory(lng_settings_save), [this] { save(); });
-	addButton(langFactory(lng_cancel), [this] { closeBox(); });
+	addButton(langFactory(lng_settings_save), [=] { save(); });
+	addButton(langFactory(lng_cancel), [=] { closeBox(); });
 	if (_invertOrder) {
 		setTabOrder(_last, _first);
 	}
 	_first->setMaxLength(kMaxGroupChannelTitle);
 	_last->setMaxLength(kMaxGroupChannelTitle);
 
-	connect(_first, &Ui::InputField::submitted, this, [this] { submit(); });
-	connect(_last, &Ui::InputField::submitted, this, [this] { submit(); });
+	connect(_first, &Ui::InputField::submitted, [=] { submit(); });
+	connect(_last, &Ui::InputField::submitted, [=] { submit(); });
 }
 
 void EditNameBox::setInnerFocus() {
@@ -1081,9 +1081,9 @@ void EditBioBox::prepare() {
 	auto cursor = _bio->textCursor();
 	cursor.setPosition(_bio->getLastText().size());
 	_bio->setTextCursor(cursor);
-	connect(_bio, &Ui::InputField::submitted, this, [this](bool ctrlShiftEnter) { save(); });
-	connect(_bio, &Ui::InputField::resized, this, [this] { updateMaxHeight(); });
-	connect(_bio, &Ui::InputField::changed, this, [this] { handleBioUpdated(); });
+	connect(_bio, &Ui::InputField::submitted, [=] { save(); });
+	connect(_bio, &Ui::InputField::resized, [=] { updateMaxHeight(); });
+	connect(_bio, &Ui::InputField::changed, [=] { handleBioUpdated(); });
 	_bio->setInstantReplaces(Ui::InstantReplaces::Default());
 	_bio->setInstantReplacesEnabled(Global::ReplaceEmojiValue());
 	handleBioUpdated();
@@ -1154,7 +1154,7 @@ EditChannelBox::EditChannelBox(QWidget*, not_null<ChannelData*> channel)
 void EditChannelBox::prepare() {
 	setTitle(langFactory(_channel->isMegagroup() ? lng_edit_group : lng_edit_channel_title));
 
-	addButton(langFactory(lng_settings_save), [this] { onSave(); });
+	addButton(langFactory(lng_settings_save), [this] { save(); });
 	addButton(langFactory(lng_cancel), [this] { closeBox(); });
 
 	subscribe(Notify::PeerUpdated(), Notify::PeerUpdatedHandler(Notify::PeerUpdate::Flag::NameChanged, [this](const Notify::PeerUpdate &update) {
@@ -1172,11 +1172,11 @@ void EditChannelBox::prepare() {
 	_description->setInstantReplaces(Ui::InstantReplaces::Default());
 	_description->setInstantReplacesEnabled(Global::ReplaceEmojiValue());
 
-	connect(_description, SIGNAL(resized()), this, SLOT(onDescriptionResized()));
-	connect(_description, SIGNAL(submitted(bool)), this, SLOT(onSave()));
-	connect(_description, SIGNAL(cancelled()), this, SLOT(onClose()));
+	connect(_description, &Ui::InputField::resized, [=] { descriptionResized(); });
+	connect(_description, &Ui::InputField::submitted, [=] { save(); });
+	connect(_description, &Ui::InputField::cancelled, [=] { closeBox(); });
 
-	connect(_publicLink, SIGNAL(clicked()), this, SLOT(onPublicLink()));
+	_publicLink->addClickHandler([=] { setupPublicLink(); });
 	_publicLink->setVisible(_channel->canEditUsername());
 	_sign->setVisible(canEditSignatures());
 	_inviteEverybody->setVisible(canEditInvites());
@@ -1192,7 +1192,7 @@ void EditChannelBox::setInnerFocus() {
 void EditChannelBox::keyPressEvent(QKeyEvent *e) {
 	if (e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) {
 		if (_title->hasFocus()) {
-			onSave();
+			save();
 		}
 	} else {
 		BoxContent::keyPressEvent(e);
@@ -1204,7 +1204,7 @@ void EditChannelBox::handleChannelNameChange() {
 	_sign->setChecked(_channel->addsSignature());
 }
 
-void EditChannelBox::onDescriptionResized() {
+void EditChannelBox::descriptionResized() {
 	updateMaxHeight();
 	update();
 }
@@ -1268,7 +1268,7 @@ void EditChannelBox::paintEvent(QPaintEvent *e) {
 	}
 }
 
-void EditChannelBox::onSave() {
+void EditChannelBox::save() {
 	if (_saveTitleRequestId || _saveDescriptionRequestId || _saveSignRequestId || _saveInvitesRequestId) return;
 
 	auto title = TextUtilities::PrepareForSending(_title->getLastText());
@@ -1287,7 +1287,7 @@ void EditChannelBox::onSave() {
 	}
 }
 
-void EditChannelBox::onPublicLink() {
+void EditChannelBox::setupPublicLink() {
 	Ui::show(
 		Box<SetupChannelBox>(_channel, true),
 		LayerOption::KeepOther);

Telegram/SourceFiles/boxes/add_contact_box.h
@@ -36,8 +36,6 @@ enum class PeerFloodType {
 QString PeerFloodErrorText(PeerFloodType type);
 
 class AddContactBox : public BoxContent, public RPCSender {
-	Q_OBJECT
-
 public:
 	AddContactBox(QWidget*, QString fname = QString(), QString lname = QString(), QString phone = QString());
 	AddContactBox(QWidget*, UserData *user);
@@ -50,12 +48,10 @@ protected:
 
 	void setInnerFocus() override;
 
-private slots:
-	void onSubmit();
-	void onSave();
-	void onRetry();
-
 private:
+	void submit();
+	void retry();
+	void save();
 	void updateButtons();
 	void onImportDone(const MTPcontacts_ImportedContacts &res);
 
@@ -79,8 +75,6 @@ private:
 };
 
 class GroupInfoBox : public BoxContent, private MTP::Sender {
-	Q_OBJECT
-
 public:
 	GroupInfoBox(QWidget*, CreatingGroupType creating, bool fromTypeChoose);
 
@@ -90,18 +84,13 @@ protected:
 
 	void resizeEvent(QResizeEvent *e) override;
 
-private slots:
-	void onNext();
-	void onNameSubmit();
-	void onDescriptionResized();
-	void onClose() {
-		closeBox();
-	}
-
 private:
 	void createChannel(const QString &title, const QString &description);
 	void createGroup(not_null<PeerListBox*> selectUsersBox, const QString &title, const std::vector<not_null<PeerData*>> &users);
+	void submitName();
+	void submit();
 
+	void descriptionResized();
 	void updateMaxHeight();
 	void updateSelected(const QPoint &cursorGlobalPosition);
 
@@ -119,8 +108,6 @@ private:
 };
 
 class SetupChannelBox : public BoxContent, public RPCSender {
-	Q_OBJECT
-
 public:
 	SetupChannelBox(QWidget*, ChannelData *channel, bool existing = false);
 
@@ -136,11 +123,6 @@ protected:
 	void mousePressEvent(QMouseEvent *e) override;
 	void leaveEventHook(QEvent *e) override;
 
-private slots:
-	void onSave();
-	void onChange();
-	void onCheck();
-
 private:
 	enum class Privacy {
 		Public,
@@ -149,6 +131,9 @@ private:
 	void privacyChanged(Privacy value);
 	void updateSelected(const QPoint &cursorGlobalPosition);
 	void showAddContactsToChannelBox() const;
+	void handleChange();
+	void check();
+	void save();
 
 	void onUpdateDone(const MTPBool &result);
 	bool onUpdateFail(const RPCError &error);
@@ -239,8 +224,6 @@ private:
 };
 
 class EditChannelBox : public BoxContent, public RPCSender {
-	Q_OBJECT
-
 public:
 	EditChannelBox(QWidget*, not_null<ChannelData*> channel);
 
@@ -252,19 +235,14 @@ protected:
 	void resizeEvent(QResizeEvent *e) override;
 	void paintEvent(QPaintEvent *e) override;
 
-private slots:
-	void onSave();
-	void onDescriptionResized();
-	void onPublicLink();
-	void onClose() {
-		closeBox();
-	}
-
 private:
 	void updateMaxHeight();
 	bool canEditSignatures() const;
 	bool canEditInvites() const;
 	void handleChannelNameChange();
+	void descriptionResized();
+	void setupPublicLink();
+	void save();
 
 	void onSaveTitleDone(const MTPUpdates &result);
 	void onSaveDescriptionDone(const MTPBool &result);

Telegram/SourceFiles/boxes/change_phone_box.cpp
@@ -125,7 +125,7 @@ void ChangePhoneBox::EnterPhone::prepare() {
 
 	_phone->resize(st::boxWidth - 2 * st::boxPadding.left(), _phone->height());
 	_phone->moveToLeft(st::boxPadding.left(), st::boxLittleSkip);
-	connect(_phone, &Ui::PhoneInput::submitted, this, [this] { submit(); });
+	connect(_phone, &Ui::PhoneInput::submitted, [=] { submit(); });
 
 	auto description = object_ptr<Ui::FlatLabel>(this, lang(lng_change_phone_new_description), Ui::FlatLabel::InitType::Simple, st::changePhoneLabel);
 	auto errorSkip = st::boxLittleSkip + st::changePhoneError.style.font->height;
@@ -225,12 +225,12 @@ void ChangePhoneBox::EnterCode::prepare() {
 
 	auto phoneValue = QString();
 	_code.create(this, st::defaultInputField, langFactory(lng_change_phone_code_title), phoneValue);
-	_code->setAutoSubmit(_codeLength, [this] { submit(); });
-	_code->setChangedCallback([this] { hideError(); });
+	_code->setAutoSubmit(_codeLength, [=] { submit(); });
+	_code->setChangedCallback([=] { hideError(); });
 
 	_code->resize(st::boxWidth - 2 * st::boxPadding.left(), _code->height());
 	_code->moveToLeft(st::boxPadding.left(), description->bottomNoMargins());
-	connect(_code, &Ui::InputField::submitted, this, [this] { submit(); });
+	connect(_code, &Ui::InputField::submitted, [=] { submit(); });
 
 	setDimensions(st::boxWidth, countHeight());
 
@@ -239,8 +239,8 @@ void ChangePhoneBox::EnterCode::prepare() {
 		updateCall();
 	}
 
-	addButton(langFactory(lng_change_phone_new_submit), [this] { submit(); });
-	addButton(langFactory(lng_cancel), [this] { closeBox(); });
+	addButton(langFactory(lng_change_phone_new_submit), [=] { submit(); });
+	addButton(langFactory(lng_cancel), [=] { closeBox(); });
 }
 
 int ChangePhoneBox::EnterCode::countHeight() {

Telegram/SourceFiles/boxes/confirm_phone_box.cpp
@@ -198,17 +198,17 @@ void ConfirmPhoneBox::prepare() {
 	_about->setMarkedText(aboutText);
 
 	_code.create(this, st::confirmPhoneCodeField, langFactory(lng_code_ph));
-	_code->setAutoSubmit(_sentCodeLength, [this] { onSendCode(); });
-	_code->setChangedCallback([this] { showError(QString()); });
+	_code->setAutoSubmit(_sentCodeLength, [=] { sendCode(); });
+	_code->setChangedCallback([=] { showError(QString()); });
 
 	setTitle(langFactory(lng_confirm_phone_title));
 
-	addButton(langFactory(lng_confirm_phone_send), [this] { onSendCode(); });
-	addButton(langFactory(lng_cancel), [this] { closeBox(); });
+	addButton(langFactory(lng_confirm_phone_send), [=] { sendCode(); });
+	addButton(langFactory(lng_cancel), [=] { closeBox(); });
 
 	setDimensions(st::boxWidth, st::usernamePadding.top() + _code->height() + st::usernameSkip + _about->height() + st::usernameSkip);
 
-	connect(_code, SIGNAL(submitted(bool)), this, SLOT(onSendCode()));
+	connect(_code, &Ui::InputField::submitted, [=] { sendCode(); });
 
 	showChildren();
 }
@@ -217,7 +217,7 @@ void ConfirmPhoneBox::callDone(const MTPauth_SentCode &result) {
 	_call.callDone();
 }
 
-void ConfirmPhoneBox::onSendCode() {
+void ConfirmPhoneBox::sendCode() {
 	if (_sendCodeRequestId) {
 		return;
 	}

Telegram/SourceFiles/boxes/confirm_phone_box.h
@@ -82,15 +82,10 @@ private:
 };
 
 class ConfirmPhoneBox : public BoxContent, public RPCSender {
-	Q_OBJECT
-
 public:
 	static void start(const QString &phone, const QString &hash);
 
 	~ConfirmPhoneBox();
-
-private slots:
-	void onSendCode();
 
 protected:
 	void prepare() override;
@@ -103,6 +98,7 @@ private:
 	ConfirmPhoneBox(QWidget*, const QString &phone, const QString &hash);
 	friend class object_ptr<ConfirmPhoneBox>;
 
+	void sendCode();
 	void sendCall();
 	void checkPhoneAndHash();
 

Telegram/SourceFiles/boxes/edit_caption_box.cpp
@@ -194,13 +194,9 @@ void EditCaptionBox::prepare() {
 	addButton(langFactory(lng_cancel), [this] { closeBox(); });
 
 	updateBoxSize();
-	connect(_field, &Ui::InputField::submitted, this, [this] { save(); });
-	connect(_field, &Ui::InputField::cancelled, this, [this] {
-		closeBox();
-	});
-	connect(_field, &Ui::InputField::resized, this, [this] {
-		captionResized();
-	});
+	connect(_field, &Ui::InputField::submitted, [=] { save(); });
+	connect(_field, &Ui::InputField::cancelled, [=] { closeBox(); });
+	connect(_field, &Ui::InputField::resized, [=] { captionResized(); });
 
 	auto cursor = _field->textCursor();
 	cursor.movePosition(QTextCursor::End);

commit 7d28e285deab3bfcd5310030f784e54f7bd35937

Fix EditColorBox.
Regression was introduced in 860063e5a4.

Fixes #4803.

Telegram/SourceFiles/boxes/edit_color_box.cpp
@@ -632,14 +632,17 @@ EditColorBox::EditColorBox(QWidget*, const QString &title, QColor current) : Box
 void EditColorBox::prepare() {
 	setTitle([=] { return _title; });
 
-	const auto changed = [=] { fieldChanged(); };
-	connect(_hueField, &Ui::MaskedInputField::changed, changed);
-	connect(_saturationField, &Ui::MaskedInputField::changed, changed);
-	connect(_brightnessField, &Ui::MaskedInputField::changed, changed);
-	connect(_redField, &Ui::MaskedInputField::changed, changed);
-	connect(_greenField, &Ui::MaskedInputField::changed, changed);
-	connect(_blueField, &Ui::MaskedInputField::changed, changed);
-	connect(_result, &Ui::MaskedInputField::changed, changed);
+	const auto hsvChanged = [=] { updateFromHSVFields(); };
+	const auto rgbChanged = [=] { updateFromRGBFields(); };
+	connect(_hueField, &Ui::MaskedInputField::changed, hsvChanged);
+	connect(_saturationField, &Ui::MaskedInputField::changed, hsvChanged);
+	connect(_brightnessField, &Ui::MaskedInputField::changed, hsvChanged);
+	connect(_redField, &Ui::MaskedInputField::changed, rgbChanged);
+	connect(_greenField, &Ui::MaskedInputField::changed, rgbChanged);
+	connect(_blueField, &Ui::MaskedInputField::changed, rgbChanged);
+	connect(_result, &Ui::MaskedInputField::changed, [=] {
+		updateFromResultField();
+	});
 
 	const auto submitted = [=] { fieldSubmitted(); };
 	connect(_hueField, &Ui::MaskedInputField::submitted, submitted);
@@ -672,29 +675,6 @@ void EditColorBox::prepare() {
 void EditColorBox::setInnerFocus() {
 	_result->setFocus();
 	_result->selectAll();
-}
-
-void EditColorBox::fieldChanged() {
-	auto emitter = sender();
-	auto checkHSVSender = [this, emitter](QObject *field) {
-		if (emitter == field) {
-			updateFromHSVFields();
-		}
-	};
-	auto checkRGBSender = [this, emitter](QObject *field) {
-		if (emitter == field) {
-			updateFromRGBFields();
-		}
-	};
-	checkHSVSender(_hueField);
-	checkHSVSender(_saturationField);
-	checkHSVSender(_brightnessField);
-	checkRGBSender(_redField);
-	checkRGBSender(_greenField);
-	checkRGBSender(_blueField);
-	if (emitter == _result) {
-		updateFromResultField();
-	}
 }
 
 void EditColorBox::fieldSubmitted() {

Telegram/SourceFiles/boxes/edit_color_box.h
@@ -37,7 +37,6 @@ protected:
 
 private:
 	void saveColor();
-	void fieldChanged();
 	void fieldSubmitted();
 
 	void updateFromColor(QColor color);

Telegram/SourceFiles/storage/file_download.cpp
@@ -1325,9 +1325,12 @@ void WebLoadManager::sendRequest(webFileLoaderPrivate *loader, const QString &re
 	}
 
 	QNetworkReply *r = loader->request(_manager, redirect);
+
+	// Those use QObject::sender, so don't just remove the receiver pointer!
 	connect(r, SIGNAL(downloadProgress(qint64, qint64)), this, SLOT(onProgress(qint64, qint64)));
 	connect(r, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(onFailed(QNetworkReply::NetworkError)));
 	connect(r, SIGNAL(metaDataChanged()), this, SLOT(onMeta()));
+
 	_replies.insert(r, loader);
 }
 
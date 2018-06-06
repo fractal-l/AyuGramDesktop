fractal-l/AyuGramDesktop

commit 281f8801340ec2c3ebf0ce5304224df8ce7f6ff7

Allow showing boxes in passport panel.

Telegram/SourceFiles/media/view/media_clip_controller.cpp
@@ -75,6 +75,23 @@ void Controller::handleSeekFinished(float64 progress) {
 	refreshTimeTexts();
 }
 
+template <typename Callback>
+void Controller::startFading(Callback start) {
+	if (!_fadeAnimation->animating()) {
+		showChildren();
+		_playbackSlider->hide();
+		_childrenHidden = false;
+	}
+	start();
+	if (_fadeAnimation->animating()) {
+		hideChildren();
+		_childrenHidden = true;
+	} else {
+		fadeFinished();
+	}
+	_playbackSlider->show();
+}
+
 void Controller::showAnimated() {
 	startFading([this]() {
 		_fadeAnimation->fadeIn(st::mediaviewShowDuration);
@@ -87,14 +104,8 @@ void Controller::hideAnimated() {
 	});
 }
 
-template <typename Callback>
-void Controller::startFading(Callback start) {
-	start();
-	_playbackSlider->show();
-}
-
 void Controller::fadeFinished() {
-	fadeUpdated(1.);
+	fadeUpdated(_fadeAnimation->visible() ? 1. : 0.);
 }
 
 void Controller::fadeUpdated(float64 opacity) {
@@ -160,7 +171,9 @@ void Controller::refreshTimeTexts() {
 	_toPlayLeft->setText(timeLeft, &leftChanged);
 	if (alreadyChanged || leftChanged) {
 		resizeEvent(nullptr);
-		_fadeAnimation->refreshCache();
+		startFading([this]() {
+			_fadeAnimation->refreshCache();
+		});
 	}
 }
 
@@ -171,16 +184,6 @@ void Controller::setInFullScreen(bool inFullScreen) {
 
 	auto handler = inFullScreen ? SIGNAL(fromFullScreenPressed()) : SIGNAL(toFullScreenPressed());
 	connect(_fullScreenToggle, SIGNAL(clicked()), this, handler);
-}
-
-void Controller::grabStart() {
-	showChildren();
-	_playbackSlider->hide();
-}
-
-void Controller::grabFinish() {
-	hideChildren();
-	_playbackSlider->show();
 }
 
 void Controller::resizeEvent(QResizeEvent *e) {
@@ -206,7 +209,11 @@ void Controller::paintEvent(QPaintEvent *e) {
 	if (_fadeAnimation->paint(p)) {
 		return;
 	}
-
+	if (_childrenHidden) {
+		showChildren();
+		_playbackSlider->setFadeOpacity(1.);
+		_childrenHidden = false;
+	}
 	App::roundRect(p, rect(), st::mediaviewSaveMsgBg, MediaviewSaveCorners);
 }
 

Telegram/Resources/langs/lang.strings
@@ -1525,7 +1525,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 "lng_passport_policy" = "{bot} privacy policy";
 "lng_passport_authorize" = "Authorize";
 "lng_passport_form_error" = "Could not get authorization form.";
-"lng_passport_save_value" = "Done";
+"lng_passport_save_value" = "Save";
+"lng_passport_saving" = "Saving...";
+"lng_passport_uploading" = "Uploading...";
 "lng_passport_upload_header" = "Scans";
 "lng_passport_scan_index" = "Scan {index}";
 "lng_passport_upload_scans" = "Upload scans";

Telegram/SourceFiles/boxes/abstract_box.cpp
@@ -227,8 +227,8 @@ void BoxContent::paintEvent(QPaintEvent *e) {
 	}
 }
 
-AbstractBox::AbstractBox(QWidget *parent, Window::Controller *controller, object_ptr<BoxContent> content) : LayerWidget(parent)
-, _controller(controller)
+AbstractBox::AbstractBox(QWidget *parent, object_ptr<BoxContent> content)
+: LayerWidget(parent)
 , _content(std::move(content)) {
 	subscribe(Lang::Current().updated(), [this] { refreshLang(); });
 	_content->setParent(this);

Telegram/SourceFiles/boxes/abstract_box.h
@@ -23,14 +23,8 @@ class FlatLabel;
 class FadeShadow;
 } // namespace Ui
 
-namespace Window {
-class Controller;
-} // namespace Window
-
 class BoxContentDelegate {
 public:
-	virtual Window::Controller *controller() const = 0;
-
 	virtual void setLayerType(bool layerType) = 0;
 	virtual void setTitle(base::lambda<TextWithEntities()> titleFactory) = 0;
 	virtual void setAdditionalTitle(base::lambda<QString()> additionalFactory) = 0;
@@ -106,10 +100,6 @@ public:
 		_preparing = true;
 		prepare();
 		finishPrepare();
-	}
-
-	Window::Controller *controller() {
-		return getDelegate()->controller();
 	}
 
 public slots:
@@ -215,11 +205,8 @@ class AbstractBox
 	, public BoxContentDelegate
 	, protected base::Subscriber {
 public:
-	AbstractBox(QWidget *parent, Window::Controller *controller, object_ptr<BoxContent> content);
+	AbstractBox(QWidget *parent, object_ptr<BoxContent> content);
 
-	Window::Controller *controller() const override {
-		return _controller;
-	}
 	void parentResized() override;
 
 	void setLayerType(bool layerType) override;
@@ -275,7 +262,6 @@ private:
 	int countRealHeight() const;
 	void updateSize();
 
-	Window::Controller *_controller = nullptr;
 	int _fullHeight = 0;
 
 	bool _noContentMargin = false;

Telegram/SourceFiles/boxes/edit_caption_box.cpp
@@ -241,7 +241,7 @@ void EditCaptionBox::paintEvent(QPaintEvent *e) {
 		}
 		if (_gifPreview && _gifPreview->started()) {
 			auto s = QSize(_thumbw, _thumbh);
-			auto paused = controller()->isGifPausedAtLeastFor(Window::GifPauseReason::Layer);
+			auto paused = _controller->isGifPausedAtLeastFor(Window::GifPauseReason::Layer);
 			auto frame = _gifPreview->current(s.width(), s.height(), s.width(), s.height(), ImageRoundRadius::None, RectPart::None, paused ? 0 : getms());
 			p.drawPixmap(_thumbx, st::boxPhotoPadding.top(), frame);
 		} else {

Telegram/SourceFiles/boxes/edit_caption_box.h
@@ -21,6 +21,10 @@ namespace Ui {
 class InputField;
 } // namespace Ui
 
+namespace Window {
+class Controller;
+} // namespace Window
+
 class EditCaptionBox : public BoxContent, public RPCSender {
 public:
 	EditCaptionBox(

Telegram/SourceFiles/boxes/edit_participant_box.cpp
@@ -66,7 +66,6 @@ class EditParticipantBox::Inner : public TWidget {
 public:
 	Inner(
 		QWidget *parent,
-		not_null<Window::Controller*> controller,
 		not_null<ChannelData*> channel,
 		not_null<UserData*> user,
 		bool hasAdminRights);
@@ -101,7 +100,6 @@ private:
 
 EditParticipantBox::Inner::Inner(
 	QWidget *parent,
-	not_null<Window::Controller*> controller,
 	not_null<ChannelData*> channel,
 	not_null<UserData*> user,
 	bool hasAdminRights)
@@ -110,7 +108,6 @@ EditParticipantBox::Inner::Inner(
 , _user(user)
 , _userPhoto(
 	this,
-	controller,
 	_user,
 	Ui::UserpicButton::Role::Custom,
 	st::rightsPhotoButton)
@@ -184,7 +181,6 @@ EditParticipantBox::EditParticipantBox(QWidget*, not_null<ChannelData*> channel,
 void EditParticipantBox::prepare() {
 	_inner = setInnerWidget(object_ptr<Inner>(
 		this,
-		controller(),
 		_channel,
 		_user,
 		hasAdminRights()));

Telegram/SourceFiles/boxes/mute_settings_box.cpp
@@ -32,7 +32,6 @@ void MuteSettingsBox::prepare() {
 
 	const auto icon = object_ptr<Ui::UserpicButton>(
 		this,
-		controller(),
 		_peer,
 		Ui::UserpicButton::Role::Custom,
 		st::mutePhotoButton);

commit 0b01cb9385205455afbc4c57cc907df9bf5a5860

Fix video seeking.
Regression was introduced in 281f880134.

Each time you hide() a widget its mousePress state is reset and
mouseMoveEvent() / mouseReleaseEvent() handlers are never called.

So you can't toggle (hide + show) widgets that are mouse-pressed.

Fixes #4802.

Telegram/SourceFiles/media/view/media_clip_controller.cpp
@@ -79,17 +79,21 @@ template <typename Callback>
 void Controller::startFading(Callback start) {
 	if (!_fadeAnimation->animating()) {
 		showChildren();
-		_playbackSlider->hide();
+		_playbackSlider->disablePaint(true);
 		_childrenHidden = false;
 	}
 	start();
 	if (_fadeAnimation->animating()) {
-		hideChildren();
+		for (const auto child : children()) {
+			if (child->isWidgetType() && child != _playbackSlider) {
+				static_cast<QWidget*>(child)->hide();
+			}
+		}
 		_childrenHidden = true;
 	} else {
 		fadeFinished();
 	}
-	_playbackSlider->show();
+	_playbackSlider->disablePaint(false);
 }
 
 void Controller::showAnimated() {

Telegram/SourceFiles/mediaview.cpp
@@ -2961,9 +2961,6 @@ bool MediaView::eventFilter(QObject *obj, QEvent *e) {
 				activate = true;
 			}
 			if (activate) {
-				if (_controlsState == ControlsHiding || _controlsState == ControlsHidden) {
-					int a = 0;
-				}
 				activateControls();
 			}
 		}

Telegram/SourceFiles/ui/widgets/continuous_sliders.cpp
@@ -195,7 +195,14 @@ float64 MediaSlider::getOverDuration() const {
 	return _st.duration;
 }
 
+void MediaSlider::disablePaint(bool disabled) {
+	_paintDisabled = disabled;
+}
+
 void MediaSlider::paintEvent(QPaintEvent *e) {
+	if (_paintDisabled) {
+		return;
+	}
 	Painter p(this);
 	PainterHighQualityEnabler hq(p);
 

Telegram/SourceFiles/ui/widgets/continuous_sliders.h
@@ -124,6 +124,7 @@ public:
 		_alwaysDisplayMarker = alwaysDisplayMarker;
 		update();
 	}
+	void disablePaint(bool disabled);
 
 protected:
 	void paintEvent(QPaintEvent *e) override;
@@ -134,6 +135,7 @@ private:
 
 	const style::MediaSlider &_st;
 	bool _alwaysDisplayMarker = false;
+	bool _paintDisabled = false;
 
 };
 
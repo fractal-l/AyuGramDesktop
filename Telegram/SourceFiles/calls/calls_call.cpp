fractal-l/AyuGramDesktop

commit c13625468e2de36779d8153f5883691af2939d18

Move all gsl::byte helpers to base/bytes module.

Telegram/SourceFiles/calls/calls_call.cpp
@@ -62,8 +62,9 @@ void ConvertEndpoint(
 }
 
 constexpr auto kFingerprintDataSize = 256;
-uint64 ComputeFingerprint(
-		const std::array<gsl::byte, kFingerprintDataSize> &authKey) {
+uint64 ComputeFingerprint(bytes::const_span authKey) {
+	Expects(authKey.size() == kFingerprintDataSize);
+
 	auto hash = openssl::Sha1(authKey);
 	return (gsl::to_integer<uint64>(hash[19]) << 56)
 		| (gsl::to_integer<uint64>(hash[18]) << 48)
@@ -133,7 +134,7 @@ Call::Call(
 	}
 }
 
-void Call::generateModExpFirst(base::const_byte_span randomSeed) {
+void Call::generateModExpFirst(bytes::const_span randomSeed) {
 	auto first = MTP::CreateModExp(_dhConfig.g, _dhConfig.p, randomSeed);
 	if (first.modexp.empty()) {
 		LOG(("Call Error: Could not compute mod-exp first."));
@@ -141,7 +142,7 @@ void Call::generateModExpFirst(base::const_byte_span randomSeed) {
 		return;
 	}
 
-	_randomPower = first.randomPower;
+	_randomPower = std::move(first.randomPower);
 	if (_type == Type::Incoming) {
 		_gb = std::move(first.modexp);
 	} else {
@@ -157,7 +158,7 @@ bool Call::isIncomingWaiting() const {
 	return (_state == State::Starting) || (_state == State::WaitingIncoming);
 }
 
-void Call::start(base::const_byte_span random) {
+void Call::start(bytes::const_span random) {
 	// Save config here, because it is possible that it changes between
 	// different usages inside the same call.
 	_dhConfig = _delegate->getDhConfig();
@@ -312,7 +313,7 @@ void Call::redial() {
 
 QString Call::getDebugLog() const {
 	constexpr auto kDebugLimit = 4096;
-	auto bytes = base::byte_vector(kDebugLimit, gsl::byte {});
+	auto bytes = bytes::vector(kDebugLimit, gsl::byte {});
 	_controller->GetDebugString(reinterpret_cast<char*>(bytes.data()), bytes.size());
 	auto end = std::find(bytes.begin(), bytes.end(), gsl::byte {});
 	auto size = (end - bytes.begin());
@@ -342,12 +343,13 @@ bool Call::isKeyShaForFingerprintReady() const {
 	return (_keyFingerprint != 0);
 }
 
-base::byte_array<Call::kSha256Size> Call::getKeyShaForFingerprint() const {
+bytes::vector Call::getKeyShaForFingerprint() const {
 	Expects(isKeyShaForFingerprintReady());
 	Expects(!_ga.empty());
-	auto encryptedChatAuthKey = base::byte_vector(_authKey.size() + _ga.size(), gsl::byte {});
-	base::copy_bytes(gsl::make_span(encryptedChatAuthKey).subspan(0, _authKey.size()), _authKey);
-	base::copy_bytes(gsl::make_span(encryptedChatAuthKey).subspan(_authKey.size(), _ga.size()), _ga);
+
+	auto encryptedChatAuthKey = bytes::vector(_authKey.size() + _ga.size(), gsl::byte {});
+	bytes::copy(gsl::make_span(encryptedChatAuthKey).subspan(0, _authKey.size()), _authKey);
+	bytes::copy(gsl::make_span(encryptedChatAuthKey).subspan(_authKey.size(), _ga.size()), _ga);
 	return openssl::Sha256(encryptedChatAuthKey);
 }
 
@@ -367,13 +369,13 @@ bool Call::handleUpdate(const MTPPhoneCall &call) {
 		}
 		_id = data.vid.v;
 		_accessHash = data.vaccess_hash.v;
-		auto gaHashBytes = bytesFromMTP(data.vg_a_hash);
+		auto gaHashBytes = bytes::make_span(data.vg_a_hash.v);
 		if (gaHashBytes.size() != _gaHash.size()) {
 			LOG(("Call Error: Wrong g_a_hash size %1, expected %2.").arg(gaHashBytes.size()).arg(_gaHash.size()));
 			finish(FinishType::Failed);
 			return true;
 		}
-		base::copy_bytes(gsl::make_span(_gaHash), gaHashBytes);
+		bytes::copy(_gaHash, gaHashBytes);
 	} return true;
 
 	case mtpc_phoneCallEmpty: {
@@ -453,7 +455,7 @@ bool Call::handleUpdate(const MTPPhoneCall &call) {
 void Call::confirmAcceptedCall(const MTPDphoneCallAccepted &call) {
 	Expects(_type == Type::Outgoing);
 
-	auto firstBytes = bytesFromMTP(call.vg_b);
+	auto firstBytes = bytes::make_span(call.vg_b.v);
 	auto computedAuthKey = MTP::CreateAuthKey(firstBytes, _randomPower, _dhConfig.p);
 	if (computedAuthKey.empty()) {
 		LOG(("Call Error: Could not compute mod-exp final."));
@@ -493,13 +495,13 @@ void Call::confirmAcceptedCall(const MTPDphoneCallAccepted &call) {
 void Call::startConfirmedCall(const MTPDphoneCall &call) {
 	Expects(_type == Type::Incoming);
 
-	auto firstBytes = bytesFromMTP(call.vg_a_or_b);
+	auto firstBytes = bytes::make_span(call.vg_a_or_b.v);
 	if (_gaHash != openssl::Sha256(firstBytes)) {
 		LOG(("Call Error: Wrong g_a hash received."));
 		finish(FinishType::Failed);
 		return;
 	}
-	_ga = base::byte_vector(firstBytes.begin(), firstBytes.end());
+	_ga = bytes::vector(firstBytes.begin(), firstBytes.end());
 
 	auto computedAuthKey = MTP::CreateAuthKey(firstBytes, _randomPower, _dhConfig.p);
 	if (computedAuthKey.empty()) {
@@ -535,12 +537,12 @@ void Call::createAndStartController(const MTPDphoneCall &call) {
 		auto callLogFolder = cWorkingDir() + qsl("DebugLogs");
 		auto callLogPath = callLogFolder + qsl("/last_call_log.txt");
 		auto callLogNative = QFile::encodeName(QDir::toNativeSeparators(callLogPath));
-		auto callLogBytesSrc = gsl::as_bytes(gsl::make_span(callLogNative));
-		auto callLogBytesDst = gsl::as_writeable_bytes(gsl::make_span(config.logFilePath));
+		auto callLogBytesSrc = bytes::make_span(callLogNative);
+		auto callLogBytesDst = bytes::make_span(config.logFilePath);
 		if (callLogBytesSrc.size() + 1 <= callLogBytesDst.size()) { // +1 - zero-terminator
 			QFile(callLogPath).remove();
 			QDir().mkpath(callLogFolder);
-			base::copy_bytes(callLogBytesDst, callLogBytesSrc);
+			bytes::copy(callLogBytesDst, callLogBytesSrc);
 		}
 	}
 

Telegram/SourceFiles/base/openssl_help.h
@@ -10,6 +10,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include <openssl/bn.h>
 #include <openssl/sha.h>
 #include <openssl/rand.h>
+#include <openssl/aes.h>
+#include <openssl/crypto.h>
 #include "base/bytes.h"
 
 namespace openssl {
@@ -61,7 +63,7 @@ public:
 	explicit BigNum(unsigned int word) : BigNum() {
 		setWord(word);
 	}
-	explicit BigNum(base::const_byte_span bytes) : BigNum() {
+	explicit BigNum(bytes::const_span bytes) : BigNum() {
 		setBytes(bytes);
 	}
 
@@ -70,7 +72,7 @@ public:
 			_failed = true;
 		}
 	}
-	void setBytes(base::const_byte_span bytes) {
+	void setBytes(bytes::const_span bytes) {
 		if (!BN_bin2bn(
 				reinterpret_cast<const unsigned char*>(bytes.data()),
 				bytes.size(),
@@ -162,12 +164,12 @@ public:
 		return failed() ? 0 : BN_num_bytes(raw());
 	}
 
-	base::byte_vector getBytes() const {
+	bytes::vector getBytes() const {
 		if (failed()) {
-			return base::byte_vector();
+			return {};
 		}
 		auto length = BN_num_bytes(raw());
-		auto result = base::byte_vector(length, gsl::byte());
+		auto result = bytes::vector(length);
 		auto resultSize = BN_bn2bin(
 			raw(),
 			reinterpret_cast<unsigned char*>(result.data()));
@@ -207,44 +209,35 @@ inline BigNum operator-(const BigNum &a, const BigNum &b) {
 	return result;
 }
 
-inline base::byte_array<SHA512_DIGEST_LENGTH> Sha512(
-		base::const_byte_span bytes) {
-	auto result = base::byte_array<SHA512_DIGEST_LENGTH>();
+inline bytes::vector Sha512(bytes::const_span data) {
+	auto result = bytes::vector(SHA512_DIGEST_LENGTH);
 	SHA512(
-		reinterpret_cast<const unsigned char*>(bytes.data()),
-		bytes.size(),
+		reinterpret_cast<const unsigned char*>(data.data()),
+		data.size(),
 		reinterpret_cast<unsigned char*>(result.data()));
 	return result;
 }
 
-inline base::byte_array<SHA256_DIGEST_LENGTH> Sha256(
-		base::const_byte_span bytes) {
-	auto result = base::byte_array<SHA256_DIGEST_LENGTH>();
+inline bytes::vector Sha256(bytes::const_span data) {
+	auto result = bytes::vector(SHA256_DIGEST_LENGTH);
 	SHA256(
-		reinterpret_cast<const unsigned char*>(bytes.data()),
-		bytes.size(),
+		reinterpret_cast<const unsigned char*>(data.data()),
+		data.size(),
 		reinterpret_cast<unsigned char*>(result.data()));
 	return result;
 }
 
-inline base::byte_array<SHA_DIGEST_LENGTH> Sha1(
-		base::const_byte_span bytes) {
-	auto result = base::byte_array<SHA_DIGEST_LENGTH>();
+inline bytes::vector Sha1(bytes::const_span data) {
+	auto result = bytes::vector(SHA_DIGEST_LENGTH);
 	SHA1(
-		reinterpret_cast<const unsigned char*>(bytes.data()),
-		bytes.size(),
+		reinterpret_cast<const unsigned char*>(data.data()),
+		data.size(),
 		reinterpret_cast<unsigned char*>(result.data()));
 	return result;
 }
 
-inline int FillRandom(base::byte_span bytes) {
-	return RAND_bytes(
-		reinterpret_cast<unsigned char*>(bytes.data()),
-		bytes.size());
-}
-
-inline void AddRandomSeed(base::const_byte_span bytes) {
-	RAND_seed(bytes.data(), bytes.size());
+inline void AddRandomSeed(bytes::const_span data) {
+	RAND_seed(data.data(), data.size());
 }
 
 } // namespace openssl

Telegram/SourceFiles/boxes/passcode_box.cpp
@@ -7,6 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "boxes/passcode_box.h"
 
+#include "base/bytes.h"
 #include "lang/lang_keys.h"
 #include "boxes/confirm_box.h"
 #include "mainwindow.h"
@@ -329,7 +330,7 @@ void PasscodeBox::save(bool force) {
 			if (_oldPasscode->isHidden() || _newPasscode->isHidden()) {
 				flags |= MTPDaccount_passwordInputSettings::Flag::f_email;
 			}
-			const auto newSecureSecret = base::byte_vector();
+			const auto newSecureSecret = bytes::vector();
 			_setRequest = MTP::send(
 				MTPaccount_UpdatePasswordSettings(
 					MTP_bytes(oldPasswordHash),

Telegram/SourceFiles/calls/calls_call.h
@@ -9,6 +9,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "base/weak_ptr.h"
 #include "base/timer.h"
+#include "base/bytes.h"
 #include "mtproto/sender.h"
 #include "mtproto/auth_key.h"
 
@@ -27,7 +28,7 @@ namespace Calls {
 struct DhConfig {
 	int32 version = 0;
 	int32 g = 0;
-	std::vector<gsl::byte> p;
+	bytes::vector p;
 };
 
 class Call : public base::has_weak_ptr, private MTP::Sender {
@@ -48,8 +49,6 @@ public:
 
 	};
 
-	static constexpr auto kRandomPowerSize = 256;
-	static constexpr auto kSha256Size = 32;
 	static constexpr auto kSoundSampleMs = 100;
 
 	enum class Type {
@@ -66,7 +65,7 @@ public:
 	}
 	bool isIncomingWaiting() const;
 
-	void start(base::const_byte_span random);
+	void start(bytes::const_span random);
 	bool handleUpdate(const MTPPhoneCall &call);
 
 	enum State {
@@ -116,7 +115,7 @@ public:
 	void redial();
 
 	bool isKeyShaForFingerprintReady() const;
-	std::array<gsl::byte, kSha256Size> getKeyShaForFingerprint() const;
+	bytes::vector getKeyShaForFingerprint() const;
 
 	QString getDebugLog() const;
 
@@ -152,7 +151,7 @@ private:
 	void startIncoming();
 	void startWaitingTrack();
 
-	void generateModExpFirst(base::const_byte_span randomSeed);
+	void generateModExpFirst(bytes::const_span randomSeed);
 	void handleControllerStateChange(
 		tgvoip::VoIPController *controller,
 		int state);
@@ -191,10 +190,10 @@ private:
 	base::Observable<bool> _muteChanged;
 
 	DhConfig _dhConfig;
-	std::vector<gsl::byte> _ga;
-	std::vector<gsl::byte> _gb;
-	std::array<gsl::byte, kSha256Size> _gaHash;
-	std::array<gsl::byte, kRandomPowerSize> _randomPower;
+	bytes::vector _ga;
+	bytes::vector _gb;
+	bytes::vector _gaHash;
+	bytes::vector _randomPower;
 	MTP::AuthKey::Data _authKey;
 	MTPPhoneCallProtocol _protocol;
 

Telegram/SourceFiles/calls/calls_emoji_fingerprint.cpp
@@ -99,7 +99,7 @@ const ushort Offsets[] = {
 620, 622, 624, 626, 628, 630, 632, 634, 636, 638, 640, 641,
 642, 643, 644, 646, 648, 650, 652, 654, 656, 658 };
 
-uint64 ComputeEmojiIndex(base::const_byte_span bytes) {
+uint64 ComputeEmojiIndex(bytes::const_span bytes) {
 	Expects(bytes.size() == 8);
 	return ((gsl::to_integer<uint64>(bytes[0]) & 0x7F) << 56)
 		| (gsl::to_integer<uint64>(bytes[1]) << 48)

Telegram/SourceFiles/calls/calls_instance.cpp
@@ -126,26 +126,26 @@ void Instance::refreshDhConfig() {
 	Expects(_currentCall != nullptr);
 	request(MTPmessages_GetDhConfig(
 		MTP_int(_dhConfig.version),
-		MTP_int(Call::kRandomPowerSize)
+		MTP_int(MTP::ModExpFirst::kRandomPowerSize)
 	)).done([this, call = base::make_weak(_currentCall)](
 			const MTPmessages_DhConfig &result) {
-		auto random = base::const_byte_span();
+		auto random = bytes::const_span();
 		switch (result.type()) {
 		case mtpc_messages_dhConfig: {
 			auto &config = result.c_messages_dhConfig();
-			if (!MTP::IsPrimeAndGood(bytesFromMTP(config.vp), config.vg.v)) {
+			if (!MTP::IsPrimeAndGood(bytes::make_span(config.vp.v), config.vg.v)) {
 				LOG(("API Error: bad p/g received in dhConfig."));
 				callFailed(call.get());
 				return;
 			}
 			_dhConfig.g = config.vg.v;
-			_dhConfig.p = byteVectorFromMTP(config.vp);
-			random = bytesFromMTP(config.vrandom);
+			_dhConfig.p = bytes::make_vector(config.vp.v);
+			random = bytes::make_span(config.vrandom.v);
 		} break;
 
 		case mtpc_messages_dhConfigNotModified: {
 			auto &config = result.c_messages_dhConfigNotModified();
-			random = bytesFromMTP(config.vrandom);
+			random = bytes::make_span(config.vrandom.v);
 			if (!_dhConfig.g || _dhConfig.p.empty()) {
 				LOG(("API Error: dhConfigNotModified on zero version."));
 				callFailed(call.get());
@@ -156,7 +156,7 @@ void Instance::refreshDhConfig() {
 		default: Unexpected("Type in messages.getDhConfig");
 		}
 
-		if (random.size() != Call::kRandomPowerSize) {
+		if (random.size() != MTP::ModExpFirst::kRandomPowerSize) {
 			LOG(("API Error: dhConfig random bytes wrong size: %1").arg(random.size()));
 			callFailed(call.get());
 			return;
@@ -186,7 +186,7 @@ void Instance::refreshServerConfig() {
 		_lastServerConfigUpdateTime = getms(true);
 
 		auto configUpdate = std::map<std::string, std::string>();
-		auto bytes = bytesFromMTP(result.c_dataJSON().vdata);
+		auto bytes = bytes::make_span(result.c_dataJSON().vdata.v);
 		auto error = QJsonParseError { 0, QJsonParseError::NoError };
 		auto document = QJsonDocument::fromJson(QByteArray::fromRawData(reinterpret_cast<const char*>(bytes.data()), bytes.size()), &error);
 		if (error.error != QJsonParseError::NoError) {

Telegram/SourceFiles/core/utils.h
@@ -92,31 +92,6 @@ using set_of_unique_ptr = std::set<std::unique_ptr<T>, base::pointer_comparator<
 template <typename T>
 using set_of_shared_ptr = std::set<std::shared_ptr<T>, base::pointer_comparator<T>>;
 
-using byte_span = gsl::span<gsl::byte>;
-using const_byte_span = gsl::span<const gsl::byte>;
-using byte_vector = std::vector<gsl::byte>;
-template <size_t N>
-using byte_array = std::array<gsl::byte, N>;
-
-inline void copy_bytes(byte_span destination, const_byte_span source) {
-	Expects(destination.size() >= source.size());
-	memcpy(destination.data(), source.data(), source.size());
-}
-
-inline void move_bytes(byte_span destination, const_byte_span source) {
-	Expects(destination.size() >= source.size());
-	memmove(destination.data(), source.data(), source.size());
-}
-
-inline void set_bytes(byte_span destination, gsl::byte value) {
-	memset(destination.data(), gsl::to_integer<unsigned char>(value), destination.size());
-}
-
-inline int compare_bytes(const_byte_span a, const_byte_span b) {
-	auto aSize = a.size(), bSize = b.size();
-	return (aSize > bSize) ? 1 : (aSize < bSize) ? -1 : memcmp(a.data(), b.data(), aSize);
-}
-
 // Thanks https://stackoverflow.com/a/28139075
 
 template <typename Container>

Telegram/SourceFiles/media/media_audio.cpp
@@ -1393,7 +1393,7 @@ void DetachFromDevice() {
 class FFMpegAttributesReader : public AbstractFFMpegLoader {
 public:
 	FFMpegAttributesReader(const FileLocation &file, const QByteArray &data)
-	: AbstractFFMpegLoader(file, data, base::byte_vector()) {
+	: AbstractFFMpegLoader(file, data, bytes::vector()) {
 	}
 
 	bool open(TimeMs positionMs) override {
@@ -1519,7 +1519,7 @@ FileMediaInformation::Song PrepareForSending(const QString &fname, const QByteAr
 
 class FFMpegWaveformCounter : public FFMpegLoader {
 public:
-	FFMpegWaveformCounter(const FileLocation &file, const QByteArray &data) : FFMpegLoader(file, data, base::byte_vector()) {
+	FFMpegWaveformCounter(const FileLocation &file, const QByteArray &data) : FFMpegLoader(file, data, bytes::vector()) {
 	}
 
 	bool open(TimeMs positionMs) override {
@@ -1562,7 +1562,7 @@ public:
 				continue;
 			}
 
-			auto sampleBytes = gsl::as_bytes(gsl::make_span(buffer));
+			auto sampleBytes = bytes::make_span(buffer);
 			if (fmt == AL_FORMAT_MONO8 || fmt == AL_FORMAT_STEREO8) {
 				Media::Audio::IterateSamples<uchar>(sampleBytes, callback);
 			} else if (fmt == AL_FORMAT_MONO16 || fmt == AL_FORMAT_STEREO16) {

commit 969cf6324f2eba1d275d1023ef7242c527abc574

Fix incoming calls.
Regression was introduced in c13625468e.

Fixes #4804.

Telegram/SourceFiles/calls/calls_call.cpp
@@ -37,6 +37,7 @@ namespace {
 constexpr auto kMinLayer = 65;
 constexpr auto kMaxLayer = 75;
 constexpr auto kHangupTimeoutMs = 5000;
+constexpr auto kSha256Size = 32;
 
 using tgvoip::Endpoint;
 
@@ -180,6 +181,7 @@ void Call::start(bytes::const_span random) {
 void Call::startOutgoing() {
 	Expects(_type == Type::Outgoing);
 	Expects(_state == State::Requesting);
+	Expects(_gaHash.size() == kSha256Size);
 
 	request(MTPphone_RequestCall(
 		_user->inputUser,
@@ -366,12 +368,12 @@ bool Call::handleUpdate(const MTPPhoneCall &call) {
 		_id = data.vid.v;
 		_accessHash = data.vaccess_hash.v;
 		auto gaHashBytes = bytes::make_span(data.vg_a_hash.v);
-		if (gaHashBytes.size() != _gaHash.size()) {
-			LOG(("Call Error: Wrong g_a_hash size %1, expected %2.").arg(gaHashBytes.size()).arg(_gaHash.size()));
+		if (gaHashBytes.size() != kSha256Size) {
+			LOG(("Call Error: Wrong g_a_hash size %1, expected %2.").arg(gaHashBytes.size()).arg(kSha256Size));
 			finish(FinishType::Failed);
 			return true;
 		}
-		bytes::copy(_gaHash, gaHashBytes);
+		_gaHash = bytes::make_vector(gaHashBytes);
 	} return true;
 
 	case mtpc_phoneCallEmpty: {
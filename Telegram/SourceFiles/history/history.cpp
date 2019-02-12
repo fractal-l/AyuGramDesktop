fractal-l/AyuGramDesktop

commit 487045378fd8feecef9ddcf53851110de694618b

Fix changelog dialogs list appearance.
Fixes #5678, fixes #5674.

Telegram/SourceFiles/history/history.cpp
@@ -2086,8 +2086,13 @@ void History::markFullyLoaded() {
 }
 
 void History::setLastMessage(HistoryItem *item) {
-	if (_lastMessage && *_lastMessage == item) {
-		return;
+	if (_lastMessage) {
+		if (*_lastMessage == item) {
+			return;
+		} else if (!IsServerMsgId((*_lastMessage)->id)
+				&& (*_lastMessage)->date() > item->date()) {
+			return;
+		}
 	}
 	_lastMessage = item;
 	_chatListMessage = std::nullopt;

commit 1097be141e7b6828abf0454bb318e78cb6e9fb11

Fix another crash in last message checking.
Regression was introduced in 487045378f.

Telegram/SourceFiles/history/history.cpp
@@ -2090,6 +2090,7 @@ void History::setLastMessage(HistoryItem *item) {
 		if (*_lastMessage == item) {
 			return;
 		} else if (*_lastMessage
+			&& item
 			&& !IsServerMsgId((*_lastMessage)->id)
 			&& (*_lastMessage)->date() > item->date()) {
 			return;
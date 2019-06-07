fractal-l/AyuGramDesktop

commit 0b7df6eb02438845f57f97b1ef2e9bd1682a8922

Use Window::Controller to manage MainWindow.

Telegram/SourceFiles/core/application.cpp
@@ -53,6 +53,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/effects/animations.h"
 #include "storage/serialize_common.h"
 #include "window/window_session_controller.h"
+#include "window/window_controller.h"
 #include "base/qthelp_regex.h"
 #include "base/qthelp_url.h"
 #include "boxes/connection_box.h"
@@ -171,12 +172,11 @@ void Application::run() {
 	// Create mime database, so it won't be slow later.
 	QMimeDatabase().mimeTypeForName(qsl("text/plain"));
 
-	_window = std::make_unique<MainWindow>();
-	_window->init();
+	_window = std::make_unique<Window::Controller>(&activeAccount());
 
-	auto currentGeometry = _window->geometry();
+	const auto currentGeometry = _window->widget()->geometry();
 	_mediaView = std::make_unique<Media::View::OverlayWidget>();
-	_window->setGeometry(currentGeometry);
+	_window->widget()->setGeometry(currentGeometry);
 
 	QCoreApplication::instance()->installEventFilter(this);
 	connect(
@@ -223,8 +223,8 @@ void Application::run() {
 bool Application::hideMediaView() {
 	if (_mediaView && !_mediaView->isHidden()) {
 		_mediaView->hide();
-		if (auto activeWindow = getActiveWindow()) {
-			activeWindow->reActivateWindow();
+		if (const auto window = activeWindow()) {
+			window->reActivate();
 		}
 		return true;
 	}
@@ -966,7 +966,7 @@ rpl::producer<bool> Application::lockValue() const {
 		_1 || _2);
 }
 
-MainWindow *Application::getActiveWindow() const {
+Window::Controller *Application::activeWindow() const {
 	return _window.get();
 }
 
@@ -974,10 +974,8 @@ bool Application::closeActiveWindow() {
 	if (hideMediaView()) {
 		return true;
 	}
-	if (auto activeWindow = getActiveWindow()) {
-		if (!activeWindow->hideNoQuit()) {
-			activeWindow->close();
-		}
+	if (const auto window = activeWindow()) {
+		window->close();
 		return true;
 	}
 	return false;
@@ -985,19 +983,19 @@ bool Application::closeActiveWindow() {
 
 bool Application::minimizeActiveWindow() {
 	hideMediaView();
-	if (auto activeWindow = getActiveWindow()) {
-		if (Global::WorkMode().value() == dbiwmTrayOnly) {
-			activeWindow->minimizeToTray();
-		} else {
-			activeWindow->setWindowState(Qt::WindowMinimized);
-		}
+	if (const auto window = activeWindow()) {
+		window->minimize();
 		return true;
 	}
 	return false;
 }
 
 QWidget *Application::getFileDialogParent() {
-	return (_mediaView && _mediaView->isVisible()) ? (QWidget*)_mediaView.get() : (QWidget*)getActiveWindow();
+	return (_mediaView && _mediaView->isVisible())
+		? (QWidget*)_mediaView.get()
+		: activeWindow()
+		? (QWidget*)activeWindow()->widget()
+		: nullptr;
 }
 
 void Application::checkMediaViewActivation() {
@@ -1032,7 +1030,7 @@ void Application::loggedOut() {
 	clearPasscodeLock();
 	Media::Player::mixer()->stopAndClear();
 	Global::SetVoiceMsgPlaybackDoubled(false);
-	if (const auto window = getActiveWindow()) {
+	if (const auto window = activeWindow()) {
 		window->tempDirDelete(Local::ClearManagerAll);
 		window->setupIntro();
 	}
@@ -1051,12 +1049,8 @@ void Application::loggedOut() {
 }
 
 QPoint Application::getPointForCallPanelCenter() const {
-	if (auto activeWindow = getActiveWindow()) {
-		Assert(activeWindow->windowHandle() != nullptr);
-		if (activeWindow->isActive()) {
-			return activeWindow->geometry().center();
-		}
-		return activeWindow->windowHandle()->screen()->geometry().center();
+	if (const auto window = activeWindow()) {
+		return window->getPointForCallPanelCenter();
 	}
 	return QApplication::desktop()->screenGeometry().center();
 }

Telegram/SourceFiles/platform/mac/main_window_mac.mm
@@ -381,8 +381,9 @@ MainWindow::Private::~Private() {
 	[_observer release];
 }
 
-MainWindow::MainWindow()
-: _private(std::make_unique<Private>(this)) {
+MainWindow::MainWindow(not_null<Window::Controller*> controller)
+: Window::MainWindow(controller)
+, _private(std::make_unique<Private>(this)) {
 #ifndef OS_MAC_OLD
 	auto forceOpenGL = std::make_unique<QOpenGLWidget>(this);
 #endif // !OS_MAC_OLD

Telegram/SourceFiles/platform/win/main_window_win.cpp
@@ -598,8 +598,9 @@ bool handleSessionNotification = false;
 
 UINT MainWindow::_taskbarCreatedMsgId = 0;
 
-MainWindow::MainWindow()
-: ps_tbHider_hWnd(createTaskbarHider()) {
+MainWindow::MainWindow(not_null<Window::Controller*> controller)
+: Window::MainWindow(controller)
+, ps_tbHider_hWnd(createTaskbarHider()) {
 	if (!_taskbarCreatedMsgId) {
 		_taskbarCreatedMsgId = RegisterWindowMessage(L"TaskbarButtonCreated");
 	}

Telegram/SourceFiles/app.cpp
@@ -29,6 +29,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "core/application.h"
 #include "window/themes/window_theme.h"
 #include "window/notifications_manager.h"
+#include "window/window_controller.h"
 #include "platform/platform_notifications_manager.h"
 #include "storage/file_upload.h"
 #include "storage/localstorage.h"
@@ -102,8 +103,8 @@ namespace App {
 	}
 
 	MainWindow *wnd() {
-		return Core::IsAppLaunched()
-			? Core::App().getActiveWindow()
+		return (Core::IsAppLaunched() && Core::App().activeWindow())
+			? Core::App().activeWindow()->widget().get()
 			: nullptr;
 	}
 

Telegram/SourceFiles/core/application.h
@@ -12,6 +12,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "base/timer.h"
 
 class AuthSessionSettings;
+class MainWindow;
 class MainWidget;
 class FileUploader;
 class Translator;
@@ -23,6 +24,7 @@ class Databases;
 
 namespace Window {
 struct TermsLock;
+class Controller;
 } // namespace Window
 
 namespace ChatHelpers {
@@ -89,7 +91,7 @@ public:
 	}
 
 	// Windows interface.
-	MainWindow *getActiveWindow() const;
+	Window::Controller *activeWindow() const;
 	bool closeActiveWindow();
 	bool minimizeActiveWindow();
 	QWidget *getFileDialogParent();
@@ -272,7 +274,7 @@ private:
 	const std::unique_ptr<Storage::Databases> _databases;
 	const std::unique_ptr<Ui::Animations::Manager> _animationsManager;
 	const std::unique_ptr<Main::Account> _account;
-	std::unique_ptr<MainWindow> _window;
+	std::unique_ptr<Window::Controller> _window;
 	std::unique_ptr<Media::View::OverlayWidget> _mediaView;
 	const std::unique_ptr<Lang::Instance> _langpack;
 	std::unique_ptr<Lang::CloudManager> _langCloudManager;

Telegram/SourceFiles/core/shortcuts.cpp
@@ -9,6 +9,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "mainwindow.h"
 #include "mainwidget.h"
+#include "window/window_controller.h"
 #include "core/application.h"
 #include "media/player/media_player_instance.h"
 #include "platform/platform_info.h"
@@ -375,7 +376,7 @@ void Manager::set(const QString &keys, Command command) {
 	}
 	auto shortcut = base::make_unique_q<QShortcut>(
 		result,
-		Core::App().getActiveWindow(),
+		Core::App().activeWindow()->widget().get(),
 		nullptr,
 		nullptr,
 		Qt::ApplicationShortcut);

Telegram/SourceFiles/mainwindow.cpp
@@ -66,7 +66,8 @@ void FeedLangTestingKey(int key) {
 
 } // namespace
 
-MainWindow::MainWindow() {
+MainWindow::MainWindow(not_null<Window::Controller*> controller)
+: Platform::MainWindow(controller) {
 	auto logo = Core::App().logo();
 	icon16 = logo.scaledToWidth(16, Qt::SmoothTransformation);
 	icon32 = logo.scaledToWidth(32, Qt::SmoothTransformation);

Telegram/SourceFiles/mainwindow.h
@@ -44,7 +44,7 @@ class MainWindow : public Platform::MainWindow {
 	Q_OBJECT
 
 public:
-	MainWindow();
+	explicit MainWindow(not_null<Window::Controller*> controller);
 	~MainWindow();
 
 	void firstShow();

commit a9d13203f5a4e3237be43d9d8cb6222c6d0e4007

Fix window title on Windows 7.
Regression was introduced in 0b7df6eb0.

Fixes #6106.

Telegram/SourceFiles/core/application.cpp
@@ -162,9 +162,6 @@ void Application::run() {
 
 	DEBUG_LOG(("Application Info: inited..."));
 
-	QCoreApplication::instance()->installNativeEventFilter(
-		psNativeEventFilter());
-
 	cChangeTimeFormat(QLocale::system().timeFormat(QLocale::ShortFormat));
 
 	DEBUG_LOG(("Application Info: starting app..."));

Telegram/SourceFiles/platform/linux/specific_linux.cpp
@@ -118,17 +118,6 @@ QString CurrentExecutablePath(int argc, char *argv[]) {
 
 namespace {
 
-class _PsEventFilter : public QAbstractNativeEventFilter {
-public:
-	bool nativeEventFilter(const QByteArray &eventType, void *message, long *result) {
-		//auto wnd = App::wnd();
-		//if (!wnd) return false;
-
-		return false;
-	}
-};
-_PsEventFilter *_psEventFilter = nullptr;
-
 QRect _monitorRect;
 auto _monitorLastGot = 0LL;
 
@@ -147,12 +136,6 @@ void psShowOverAll(QWidget *w, bool canFocus) {
 }
 
 void psBringToBack(QWidget *w) {
-}
-
-QAbstractNativeEventFilter *psNativeEventFilter() {
-	delete _psEventFilter;
-	_psEventFilter = new _PsEventFilter();
-	return _psEventFilter;
 }
 
 void psWriteDump() {
@@ -255,9 +238,6 @@ void start() {
 
 void finish() {
 	Notifications::Finish();
-
-	delete _psEventFilter;
-	_psEventFilter = nullptr;
 }
 
 bool TranslucentWindowsSupported(QPoint globalPosition) {

Telegram/SourceFiles/platform/linux/specific_linux.h
@@ -69,8 +69,6 @@ void psBringToBack(QWidget *w);
 int psCleanup();
 int psFixPrevious();
 
-QAbstractNativeEventFilter *psNativeEventFilter();
-
 void psNewVersion();
 
 void psUpdateOverlayed(QWidget *widget);

Telegram/SourceFiles/platform/mac/main_window_mac.mm
@@ -95,11 +95,30 @@ private:
 
 #endif // OS_MAC_OLD
 
+class EventFilter : public QAbstractNativeEventFilter {
+public:
+	EventFilter(not_null<MainWindow*> window) : _window(window) {
+	}
+
+	bool nativeEventFilter(
+			const QByteArray &eventType,
+			void *message,
+			long *result) {
+		return Core::Sandbox::Instance().customEnterFromEventLoop([&] {
+			return _window->psFilterNativeEvent(message);
+		});
+	}
+
+private:
+	not_null<MainWindow*> _window;
+
+};
+
 } // namespace
 
 class MainWindow::Private {
 public:
-	Private(MainWindow *window);
+	explicit Private(not_null<MainWindow*> window);
 
 	void setNativeWindow(NSWindow *window, NSView *view);
 	void setWindowBadge(const QString &str);
@@ -123,7 +142,7 @@ private:
 	void initCustomTitle();
 	void refreshWeakTitleReferences();
 
-	MainWindow *_public;
+	not_null<MainWindow*> _public;
 	friend class MainWindow;
 
 #ifdef OS_MAC_OLD
@@ -139,10 +158,12 @@ private:
 	bool _useNativeTitle = false;
 	bool _inFullScreen = false;
 
-	MainWindowObserver *_observer;
+	MainWindowObserver *_observer = nullptr;
 	NSPasteboard *_generalPasteboard = nullptr;
 	int _generalPasteboardChangeCount = -1;
 	bool _generalPasteboardHasText = false;
+
+	EventFilter _nativeEventFilter;
 
 };
 
@@ -187,9 +208,10 @@ private:
 
 namespace Platform {
 
-MainWindow::Private::Private(MainWindow *window)
+MainWindow::Private::Private(not_null<MainWindow*> window)
 : _public(window)
-, _observer([[MainWindowObserver alloc] init:this]) {
+, _observer([[MainWindowObserver alloc] init:this])
+, _nativeEventFilter(window) {
 	_generalPasteboard = [NSPasteboard generalPasteboard];
 
 	@autoreleasepool {
@@ -384,6 +406,9 @@ MainWindow::Private::~Private() {
 MainWindow::MainWindow(not_null<Window::Controller*> controller)
 : Window::MainWindow(controller)
 , _private(std::make_unique<Private>(this)) {
+	QCoreApplication::instance()->installNativeEventFilter(
+		&_private->_nativeEventFilter);
+
 #ifndef OS_MAC_OLD
 	auto forceOpenGL = std::make_unique<QOpenGLWidget>(this);
 #endif // !OS_MAC_OLD

Telegram/SourceFiles/platform/mac/specific_mac.h
@@ -68,8 +68,6 @@ int psFixPrevious();
 
 bool psShowOpenWithMenu(int x, int y, const QString &file);
 
-QAbstractNativeEventFilter *psNativeEventFilter();
-
 void psNewVersion();
 
 void psUpdateOverlayed(QWidget *widget);

Telegram/SourceFiles/platform/mac/specific_mac.mm
@@ -33,23 +33,6 @@ namespace {
 
 QStringList _initLogs;
 
-class _PsEventFilter : public QAbstractNativeEventFilter {
-public:
-	_PsEventFilter() {
-	}
-
-	bool nativeEventFilter(const QByteArray &eventType, void *message, long *result) {
-		return Core::Sandbox::Instance().customEnterFromEventLoop([&] {
-			auto wnd = App::wnd();
-			if (!wnd) return false;
-
-			return wnd->psFilterNativeEvent(message);
-		});
-	}
-};
-
-_PsEventFilter *_psEventFilter = nullptr;
-
 };
 
 namespace {
@@ -74,12 +57,6 @@ void psShowOverAll(QWidget *w, bool canFocus) {
 
 void psBringToBack(QWidget *w) {
 	objc_bringToBack(w->winId());
-}
-
-QAbstractNativeEventFilter *psNativeEventFilter() {
-	delete _psEventFilter;
-	_psEventFilter = new _PsEventFilter();
-	return _psEventFilter;
 }
 
 void psWriteDump() {
@@ -139,9 +116,6 @@ void start() {
 }
 
 void finish() {
-	delete _psEventFilter;
-	_psEventFilter = nullptr;
-
 	objc_finish();
 }
 

Telegram/SourceFiles/platform/win/main_window_win.cpp
@@ -10,6 +10,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "styles/style_window.h"
 #include "platform/platform_notifications_manager.h"
 #include "platform/win/windows_dlls.h"
+#include "platform/win/windows_event_filter.h"
 #include "window/notifications_manager.h"
 #include "mainwindow.h"
 #include "core/application.h"
@@ -129,7 +130,8 @@ public:
 		update(Change::Moved | Change::Resized);
 	}
 
-	bool init(QColor c) {
+	bool init(not_null<MainWindow*> window, QColor c) {
+		_window = window;
 		_fullsize = st::windowShadow.width();
 		_shift = st::windowShadowShift;
 		auto cornersImage = QImage(_fullsize, _fullsize, QImage::Format_ARGB32_Premultiplied);
@@ -186,7 +188,7 @@ public:
 		accumulate_max(max_h, st::titleHeight + st::windowMinHeight);
 
 		HINSTANCE appinst = (HINSTANCE)GetModuleHandle(0);
-		HWND hwnd = App::wnd() ? App::wnd()->psHwnd() : 0;
+		HWND hwnd = _window ? _window->psHwnd() : nullptr;
 
 		for (int i = 0; i < 4; ++i) {
 			QString cn = QString("TelegramShadow%1").arg(i);
@@ -320,7 +322,7 @@ public:
 	}
 
 	void update(Changes changes, WINDOWPOS *pos = 0) {
-		HWND hwnd = App::wnd() ? App::wnd()->psHwnd() : 0;
+		HWND hwnd = _window ? _window->psHwnd() : 0;
 		if (!hwnd || !hwnds[0]) return;
 
 		if (changes == Changes(Change::Activate)) {
@@ -339,7 +341,7 @@ public:
 			}
 			return;
 		}
-		if (!App::wnd()->positionInited()) return;
+		if (!_window->positionInited()) return;
 
 		int x = _x, y = _y, w = _w, h = _h;
 		if (pos && (!(pos->flags & SWP_NOMOVE) || !(pos->flags & SWP_NOSIZE) || !(pos->flags & SWP_NOREPOSITION))) {
@@ -497,12 +499,17 @@ public:
 		if (screenDC) ReleaseDC(0, screenDC);
 	}
 
+	MainWindow *window() const {
+		return _window;
+	}
+
 private:
 
 	int _x = 0, _y = 0, _w = 0, _h = 0;
 	int _metaSize = 0, _fullsize = 0, _size = 0, _shift = 0;
 	QVector<BYTE> _alphas, _colors;
 
+	MainWindow *_window = nullptr;
 	bool hidden = true;
 
 	HWND hwnds[4];
@@ -520,8 +527,10 @@ private:
 _PsShadowWindows _psShadowWindows;
 
 LRESULT CALLBACK _PsShadowWindows::wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-	auto wnd = App::wnd();
-	if (!wnd || !wnd->shadowsWorking()) return DefWindowProc(hwnd, msg, wParam, lParam);
+	const auto window = _psShadowWindows.window();
+	if (!window || !window->shadowsWorking()) {
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
 
 	int i;
 	for (i = 0; i < 4; ++i) {
@@ -533,7 +542,7 @@ LRESULT CALLBACK _PsShadowWindows::wndProc(HWND hwnd, UINT msg, WPARAM wParam, L
 
 	switch (msg) {
 	case WM_CLOSE:
-	App::wnd()->close();
+		window->close();
 	break;
 
 	case WM_NCHITTEST: {
@@ -566,20 +575,20 @@ LRESULT CALLBACK _PsShadowWindows::wndProc(HWND hwnd, UINT msg, WPARAM wParam, L
 	case WM_NCPOINTERUPDATE:
 	case WM_NCPOINTERDOWN:
 	case WM_NCPOINTERUP:
-	if (App::wnd() && App::wnd()->psHwnd()) {
+	if (window && window->psHwnd()) {
 		if (msg == WM_NCLBUTTONDOWN) {
-			::SetForegroundWindow(App::wnd()->psHwnd());
+			::SetForegroundWindow(window->psHwnd());
 		}
-		LRESULT res = SendMessage(App::wnd()->psHwnd(), msg, wParam, lParam);
+		LRESULT res = SendMessage(window->psHwnd(), msg, wParam, lParam);
 		return res;
 	}
 	return 0;
 	break;
 	case WM_ACTIVATE:
-	if (App::wnd() && App::wnd()->psHwnd() && wParam == WA_ACTIVE) {
-		if ((HWND)lParam != App::wnd()->psHwnd()) {
+	if (window && window->psHwnd() && wParam == WA_ACTIVE) {
+		if ((HWND)lParam != window->psHwnd()) {
 			::SetForegroundWindow(hwnd);
-			::SetWindowPos(App::wnd()->psHwnd(), hwnd, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+			::SetWindowPos(window->psHwnd(), hwnd, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 		}
 	}
 	return DefWindowProc(hwnd, msg, wParam, lParam);
@@ -601,6 +610,9 @@ UINT MainWindow::_taskbarCreatedMsgId = 0;
 MainWindow::MainWindow(not_null<Window::Controller*> controller)
 : Window::MainWindow(controller)
 , ps_tbHider_hWnd(createTaskbarHider()) {
+	QCoreApplication::instance()->installNativeEventFilter(
+		EventFilter::CreateInstance(this));
+
 	if (!_taskbarCreatedMsgId) {
 		_taskbarCreatedMsgId = RegisterWindowMessage(L"TaskbarButtonCreated");
 	}
@@ -787,7 +799,7 @@ void MainWindow::initHook() {
 
 Q_DECLARE_METATYPE(QMargins);
 void MainWindow::psFirstShow() {
-	_psShadowWindows.init(st::windowShadowFg->c);
+	_psShadowWindows.init(this, st::windowShadowFg->c);
 	_shadowsWorking = true;
 
 	psUpdateMargins();
@@ -973,6 +985,8 @@ MainWindow::~MainWindow() {
 	psDestroyIcons();
 	_psShadowWindows.destroy();
 	if (ps_tbHider_hWnd) DestroyWindow(ps_tbHider_hWnd);
+
+	EventFilter::Destroy();
 }
 
 } // namespace Platform

Telegram/SourceFiles/platform/win/notifications_manager_win.cpp
@@ -665,14 +665,15 @@ void querySystemNotificationSettings() {
 bool SkipAudio() {
 	querySystemNotificationSettings();
 
-	if (UserNotificationState == QUNS_NOT_PRESENT || UserNotificationState == QUNS_PRESENTATION_MODE) {
+	if (UserNotificationState == QUNS_NOT_PRESENT
+		|| UserNotificationState == QUNS_PRESENTATION_MODE
+		|| QuietHoursEnabled) {
 		return true;
 	}
-	if (QuietHoursEnabled) {
-		return true;
-	}
-	if (EventFilter::getInstance()->sessionLoggedOff()) {
-		return true;
+	if (const auto filter = EventFilter::GetInstance()) {
+		if (filter->sessionLoggedOff()) {
+			return true;
+		}
 	}
 	return false;
 }
@@ -680,10 +681,10 @@ bool SkipAudio() {
 bool SkipToast() {
 	querySystemNotificationSettings();
 
-	if (UserNotificationState == QUNS_PRESENTATION_MODE || UserNotificationState == QUNS_RUNNING_D3D_FULL_SCREEN/* || UserNotificationState == QUNS_BUSY*/) {
-		return true;
-	}
-	if (QuietHoursEnabled) {
+	if (UserNotificationState == QUNS_PRESENTATION_MODE
+		|| UserNotificationState == QUNS_RUNNING_D3D_FULL_SCREEN
+		//|| UserNotificationState == QUNS_BUSY
+		|| QuietHoursEnabled) {
 		return true;
 	}
 	return false;

Telegram/SourceFiles/platform/win/specific_win.cpp
@@ -11,7 +11,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "platform/win/notifications_manager_win.h"
 #include "platform/win/windows_app_user_model_id.h"
 #include "platform/win/windows_dlls.h"
-#include "platform/win/windows_event_filter.h"
 #include "lang/lang_keys.h"
 #include "mainwindow.h"
 #include "mainwidget.h"
@@ -80,10 +79,6 @@ namespace {
 	_PsInitializer _psInitializer;
 
 };
-
-QAbstractNativeEventFilter *psNativeEventFilter() {
-	return EventFilter::createInstance();
-}
 
 void psDeleteDir(const QString &dir) {
 	std::wstring wDir = QDir::toNativeSeparators(dir).toStdWString();
@@ -286,7 +281,6 @@ void start() {
 }
 
 void finish() {
-	EventFilter::destroy();
 }
 
 bool IsApplicationActive() {

Telegram/SourceFiles/platform/win/specific_win.h
@@ -74,8 +74,6 @@ void psBringToBack(QWidget *w);
 int psCleanup();
 int psFixPrevious();
 
-QAbstractNativeEventFilter *psNativeEventFilter();
-
 void psNewVersion();
 
 void psUpdateOverlayed(TWidget *widget);

Telegram/SourceFiles/platform/win/windows_event_filter.cpp
@@ -29,40 +29,56 @@ bool IsCompositionEnabled() {
 
 } // namespace
 
-EventFilter *EventFilter::createInstance() {
-	destroy();
-	instance = new EventFilter();
-	return getInstance();
+EventFilter *EventFilter::CreateInstance(not_null<MainWindow*> window) {
+	Expects(instance == nullptr);
+
+	return (instance = new EventFilter(window));
 }
 
-EventFilter *EventFilter::getInstance() {
+EventFilter *EventFilter::GetInstance() {
 	return instance;
 }
 
-void EventFilter::destroy() {
+void EventFilter::Destroy() {
+	Expects(instance != nullptr);
+
 	delete instance;
 	instance = nullptr;
 }
 
-bool EventFilter::nativeEventFilter(const QByteArray &eventType, void *message, long *result) {
-	auto wnd = App::wnd();
-	if (!wnd) return false;
+EventFilter::EventFilter(not_null<MainWindow*> window) : _window(window) {
+}
 
-	MSG *msg = (MSG*)message;
+bool EventFilter::nativeEventFilter(
+		const QByteArray &eventType,
+		void *message,
+		long *result) {
+	const auto msg = static_cast<MSG*>(message);
 	if (msg->message == WM_ENDSESSION) {
 		App::quit();
 		return false;
 	}
-	if (msg->hwnd == wnd->psHwnd() || msg->hwnd && !wnd->psHwnd()) {
-		return mainWindowEvent(msg->hwnd, msg->message, msg->wParam, msg->lParam, (LRESULT*)result);
+	if (msg->hwnd == _window->psHwnd()
+		|| msg->hwnd && !_window->psHwnd()) {
+		return mainWindowEvent(
+			msg->hwnd,
+			msg->message,
+			msg->wParam,
+			msg->lParam,
+			(LRESULT*)result);
 	}
 	return false;
 }
 
-bool EventFilter::mainWindowEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam, LRESULT *result) {
+bool EventFilter::mainWindowEvent(
+		HWND hWnd,
+		UINT msg,
+		WPARAM wParam,
+		LPARAM lParam,
+		LRESULT *result) {
 	using ShadowsChange = MainWindow::ShadowsChange;
 
-	if (auto tbCreatedMsgId = Platform::MainWindow::TaskbarCreatedMsgId()) {
+	if (const auto tbCreatedMsgId = Platform::MainWindow::TaskbarCreatedMsgId()) {
 		if (msg == tbCreatedMsgId) {
 			Platform::MainWindow::TaskbarCreated();
 		}
@@ -90,15 +106,15 @@ bool EventFilter::mainWindowEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPa
 
 	case WM_ACTIVATE: {
 		if (LOWORD(wParam) == WA_CLICKACTIVE) {
-			App::wnd()->setInactivePress(true);
+			_window->setInactivePress(true);
 		}
 		if (LOWORD(wParam) != WA_INACTIVE) {
-			App::wnd()->shadowsActivate();
+			_window->shadowsActivate();
 		} else {
-			App::wnd()->shadowsDeactivate();
+			_window->shadowsDeactivate();
 		}
 		if (Global::started()) {
-			App::wnd()->update();
+			_window->update();
 		}
 	} return false;
 
@@ -141,42 +157,40 @@ bool EventFilter::mainWindowEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPa
 		WINDOWPLACEMENT wp;
 		wp.length = sizeof(WINDOWPLACEMENT);
 		if (GetWindowPlacement(hWnd, &wp) && (wp.showCmd == SW_SHOWMAXIMIZED || wp.showCmd == SW_SHOWMINIMIZED)) {
-			App::wnd()->shadowsUpdate(ShadowsChange::Hidden);
+			_window->shadowsUpdate(ShadowsChange::Hidden);
 		} else {
-			App::wnd()->shadowsUpdate(ShadowsChange::Moved | ShadowsChange::Resized, (WINDOWPOS*)lParam);
+			_window->shadowsUpdate(ShadowsChange::Moved | ShadowsChange::Resized, (WINDOWPOS*)lParam);
 		}
 	} return false;
 
 	case WM_SIZE: {
-		if (App::wnd()) {
-			if (wParam == SIZE_MAXIMIZED || wParam == SIZE_RESTORED || wParam == SIZE_MINIMIZED) {
-				if (wParam != SIZE_RESTORED || App::wnd()->windowState() != Qt::WindowNoState) {
-					Qt::WindowState state = Qt::WindowNoState;
-					if (wParam == SIZE_MAXIMIZED) {
-						state = Qt::WindowMaximized;
-					} else if (wParam == SIZE_MINIMIZED) {
-						state = Qt::WindowMinimized;
-					}
-					emit App::wnd()->windowHandle()->windowStateChanged(state);
-				} else {
-					App::wnd()->positionUpdated();
+		if (wParam == SIZE_MAXIMIZED || wParam == SIZE_RESTORED || wParam == SIZE_MINIMIZED) {
+			if (wParam != SIZE_RESTORED || _window->windowState() != Qt::WindowNoState) {
+				Qt::WindowState state = Qt::WindowNoState;
+				if (wParam == SIZE_MAXIMIZED) {
+					state = Qt::WindowMaximized;
+				} else if (wParam == SIZE_MINIMIZED) {
+					state = Qt::WindowMinimized;
 				}
-				App::wnd()->psUpdateMargins();
-				MainWindow::ShadowsChanges changes = (wParam == SIZE_MINIMIZED || wParam == SIZE_MAXIMIZED) ? ShadowsChange::Hidden : (ShadowsChange::Resized | ShadowsChange::Shown);
-				App::wnd()->shadowsUpdate(changes);
+				emit _window->windowHandle()->windowStateChanged(state);
+			} else {
+				_window->positionUpdated();
 			}
+			_window->psUpdateMargins();
+			MainWindow::ShadowsChanges changes = (wParam == SIZE_MINIMIZED || wParam == SIZE_MAXIMIZED) ? ShadowsChange::Hidden : (ShadowsChange::Resized | ShadowsChange::Shown);
+			_window->shadowsUpdate(changes);
 		}
 	} return false;
 
 	case WM_SHOWWINDOW: {
 		LONG style = GetWindowLong(hWnd, GWL_STYLE);
 		auto changes = ShadowsChange::Resized | ((wParam && !(style & (WS_MAXIMIZE | WS_MINIMIZE))) ? ShadowsChange::Shown : ShadowsChange::Hidden);
-		App::wnd()->shadowsUpdate(changes);
+		_window->shadowsUpdate(changes);
 	} return false;
 
 	case WM_MOVE: {
-		App::wnd()->shadowsUpdate(ShadowsChange::Moved);
-		App::wnd()->positionUpdated();
+		_window->shadowsUpdate(ShadowsChange::Moved);
+		_window->positionUpdated();
 	} return false;
 
 	case WM_NCHITTEST: {
@@ -185,7 +199,7 @@ bool EventFilter::mainWindowEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPa
 		POINTS p = MAKEPOINTS(lParam);
 		RECT r;
 		GetWindowRect(hWnd, &r);
-		auto res = App::wnd()->hitTest(QPoint(p.x - r.left + App::wnd()->deltaLeft(), p.y - r.top + App::wnd()->deltaTop()));
+		auto res = _window->hitTest(QPoint(p.x - r.left + _window->deltaLeft(), p.y - r.top + _window->deltaTop()));
 		switch (res) {
 		case Window::HitTestResult::Client:
 		case Window::HitTestResult::SysButton:   *result = HTCLIENT; break;
@@ -210,8 +224,8 @@ bool EventFilter::mainWindowEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPa
 	case WM_SYSCOMMAND: {
 		if (wParam == SC_MOUSEMENU) {
 			POINTS p = MAKEPOINTS(lParam);
-			App::wnd()->updateSystemMenu(App::wnd()->windowHandle()->windowState());
-			TrackPopupMenu(App::wnd()->psMenu(), TPM_LEFTALIGN | TPM_TOPALIGN | TPM_LEFTBUTTON, p.x, p.y, 0, hWnd, 0);
+			_window->updateSystemMenu(_window->windowHandle()->windowState());
+			TrackPopupMenu(_window->psMenu(), TPM_LEFTALIGN | TPM_TOPALIGN | TPM_LEFTBUTTON, p.x, p.y, 0, hWnd, 0);
 		}
 	} return false;
 
@@ -219,10 +233,10 @@ bool EventFilter::mainWindowEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPa
 		if (HIWORD(wParam)) return false;
 		int cmd = LOWORD(wParam);
 		switch (cmd) {
-		case SC_CLOSE: App::wnd()->close(); return true;
-		case SC_MINIMIZE: App::wnd()->setWindowState(Qt::WindowMinimized); return true;
-		case SC_MAXIMIZE: App::wnd()->setWindowState(Qt::WindowMaximized); return true;
-		case SC_RESTORE: App::wnd()->setWindowState(Qt::WindowNoState); return true;
+		case SC_CLOSE: _window->close(); return true;
+		case SC_MINIMIZE: _window->setWindowState(Qt::WindowMinimized); return true;
+		case SC_MAXIMIZE: _window->setWindowState(Qt::WindowMaximized); return true;
+		case SC_RESTORE: _window->setWindowState(Qt::WindowNoState); return true;
 		}
 	} return true;
 

Telegram/SourceFiles/platform/win/windows_event_filter.h
@@ -11,6 +11,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 namespace Platform {
 
+class MainWindow;
+
 class EventFilter : public QAbstractNativeEventFilter {
 public:
 	bool nativeEventFilter(const QByteArray &eventType, void *message, long *result);
@@ -23,14 +25,14 @@ public:
 		_sessionLoggedOff = loggedOff;
 	}
 
-	static EventFilter *createInstance();
-	static EventFilter *getInstance();
-	static void destroy();
+	static EventFilter *CreateInstance(not_null<MainWindow*> window);
+	static EventFilter *GetInstance();
+	static void Destroy();
 
 private:
-	EventFilter() {
-	}
+	explicit EventFilter(not_null<MainWindow*> window);
 
+	not_null<MainWindow*> _window;
 	bool _sessionLoggedOff = false;
 
 };
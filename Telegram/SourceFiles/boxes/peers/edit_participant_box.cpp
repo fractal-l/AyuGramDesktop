fractal-l/AyuGramDesktop

commit 1b87a5a5794b040c881c1eb4e0c0b6771a904b3b

Move transfer ownership button.

Telegram/SourceFiles/boxes/peers/edit_participant_box.cpp
@@ -354,16 +354,6 @@ void EditAdminBox::prepare() {
 	) | rpl::then(std::move(
 		changes
 	));
-	if (canTransferOwnership()) {
-		const auto allFlags = FullAdminRights(isGroup);
-		setupTransferButton(
-			isGroup
-		)->toggleOn(rpl::duplicate(
-			selectedFlags
-		) | rpl::map(
-			((_1 & allFlags) == allFlags)
-		))->setDuration(0);
-	}
 	_aboutAddAdmins = addControl(
 		object_ptr<Ui::FlatLabel>(this, st::boxDividerLabel),
 		st::rightsAboutMargin);
@@ -375,6 +365,17 @@ void EditAdminBox::prepare() {
 	) | rpl::start_with_next([=](bool checked) {
 		refreshAboutAddAdminsText(checked);
 	}, lifetime());
+
+	if (canTransferOwnership()) {
+		const auto allFlags = FullAdminRights(isGroup);
+		setupTransferButton(
+			isGroup
+		)->toggleOn(rpl::duplicate(
+			selectedFlags
+		) | rpl::map(
+			((_1 & allFlags) == allFlags)
+		))->setDuration(0);
+	}
 
 	if (canSave()) {
 		const auto rank = (chat || channel->isMegagroup())
@@ -470,13 +471,10 @@ not_null<Ui::SlideWrap<Ui::RpWidget>*> EditAdminBox::setupTransferButton(
 			object_ptr<Ui::VerticalLayout>(this)));
 
 	const auto container = wrap->entity();
-	const auto addDivider = [&] {
-		container->add(
-			object_ptr<BoxContentDivider>(container),
-			{ 0, st::infoProfileSkip, 0, st::infoProfileSkip });
-	};
 
-	addDivider();
+	container->add(
+		object_ptr<BoxContentDivider>(container),
+		{ 0, st::infoProfileSkip, 0, st::infoProfileSkip });
 	container->add(EditPeerInfoBox::CreateButton(
 		this,
 		(isGroup
@@ -485,7 +483,6 @@ not_null<Ui::SlideWrap<Ui::RpWidget>*> EditAdminBox::setupTransferButton(
 		rpl::single(QString()),
 		[=] { transferOwnership(); },
 		st::peerPermissionsButton));
-	addDivider();
 
 	return wrap;
 }

commit 3b4c32cd3d2c0e7c5d728a2ff94bfb8d935d205a

Fix transfer ownership button visibility.
Regression was introduced in 1b87a5a579.

Telegram/SourceFiles/boxes/peers/edit_participant_box.cpp
@@ -357,7 +357,7 @@ void EditAdminBox::prepare() {
 	_aboutAddAdmins = addControl(
 		object_ptr<Ui::FlatLabel>(this, st::boxDividerLabel),
 		st::rightsAboutMargin);
-	std::move(
+	rpl::duplicate(
 		selectedFlags
 	) | rpl::map(
 		(_1 & Flag::f_add_admins) != 0
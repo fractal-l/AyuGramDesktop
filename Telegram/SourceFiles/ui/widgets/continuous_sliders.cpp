fractal-l/AyuGramDesktop

commit 39e37ca880948e098312d2e471d2713eb4f8e1fb

Fix volume slider in voice chats.

Telegram/SourceFiles/ui/widgets/continuous_sliders.cpp
@@ -260,7 +260,14 @@ void MediaSlider::paintEvent(QPaintEvent *e) {
 		: seekRect.height();
 	const auto from = 0;
 	const auto length = (horizontal ? width() : height());
-	const auto mid = qRound(from + value * length);
+	const auto alwaysSeekSize = horizontal
+		? _st.seekSize.width()
+		: _st.seekSize.height();
+	const auto mid = _alwaysDisplayMarker
+		? qRound(from
+			+ (alwaysSeekSize / 2.)
+			+ value * (length - alwaysSeekSize))
+		: qRound(from + value * length);
 	const auto till = std::max(mid, qRound(from + receivedTill * length));
 	const auto end = from + length;
 	const auto activeFg = disabled

commit d99014fa0dfe2467659179c9247497d5cfeeb5b8

Fix crash in vertical sliders.
Regression was introduced in 39e37ca880.

Telegram/SourceFiles/ui/widgets/continuous_sliders.cpp
@@ -268,7 +268,9 @@ void MediaSlider::paintEvent(QPaintEvent *e) {
 			+ (alwaysSeekSize / 2.)
 			+ value * (length - alwaysSeekSize))
 		: qRound(from + value * length);
-	const auto till = std::max(mid, qRound(from + receivedTill * length));
+	const auto till = horizontal
+		? mid
+		: std::max(mid, qRound(from + receivedTill * length));
 	const auto end = from + length;
 	const auto activeFg = disabled
 		? _st.activeFgDisabled
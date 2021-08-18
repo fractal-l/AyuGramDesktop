fractal-l/AyuGramDesktop

commit 83e15387cb887f3bc1d59ae9433e41b1d2e20f32

Fix -Wunused-variable warnings

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,16 +164,13 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
-		auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";
 		}
 		++data;
 		skipWhitespacesAndComments(data, end);
-		auto valueStart = data;
 		auto value = readValue(data, end);
-		auto valueEnd = data;
 		if (value.size() == 0) {
 			return "error";
 		}

commit 6707337de15b05a8a128e7257d69d860baa5e694

Fix creating a theme from a default one.
Regression was introduced in 83e15387cb.

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,6 +164,7 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
+		[[maybe_unused]] auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";
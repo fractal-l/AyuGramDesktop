fractal-l/AyuGramDesktop

commit 650f6835309e01ab668b7718067d8d8ed4493cf6

Destroy comments button when switched off.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -2785,6 +2785,8 @@ int Message::resizeContentGetHeight(int newWidth) {
 
 		if (item->repliesAreComments() || item->externalReply()) {
 			newHeight += st::historyCommentsButtonHeight;
+		} else {
+			_comments = nullptr;
 		}
 		newHeight += viewButtonHeight();
 	} else if (mediaDisplayed) {

commit 639190c0546b83cecedc494b61e2b7d2263e99df

Fix crash after comments button destruction.
Regression was introduced in 650f683530.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -2790,8 +2790,9 @@ int Message::resizeContentGetHeight(int newWidth) {
 
 		if (item->repliesAreComments() || item->externalReply()) {
 			newHeight += st::historyCommentsButtonHeight;
-		} else {
+		} else if (_comments) {
 			_comments = nullptr;
+			checkHeavyPart();
 		}
 		newHeight += viewButtonHeight();
 	} else if (mediaDisplayed) {
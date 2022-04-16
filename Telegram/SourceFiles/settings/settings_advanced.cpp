fractal-l/AyuGramDesktop

commit 73b553fa43bed5369bd6d198cc129c97f25c2786

Replaced Ui::InformBox and Ui::ConfirmBox with GenericBox.

Telegram/SourceFiles/settings/settings_advanced.cpp
@@ -521,8 +521,8 @@ void SetupSystemIntegrationContent(
 		}) | rpl::start_with_next([=](bool checked) {
 			if (controller->session().domain().local().hasLocalPasscode()) {
 				minimized->entity()->setChecked(false);
-				controller->show(Box<Ui::InformBox>(
-					tr::lng_error_start_minimized_passcoded(tr::now)));
+				controller->show(Ui::MakeInformBox(
+					tr::lng_error_start_minimized_passcoded()));
 			} else {
 				cSetStartMinimized(checked);
 				Local::writeSettings();
@@ -638,10 +638,11 @@ void SetupANGLE(
 					}
 					Core::Restart();
 				});
-				controller->show(Box<Ui::ConfirmBox>(
-					tr::lng_settings_need_restart(tr::now),
-					tr::lng_settings_restart_now(tr::now),
-					confirmed));
+				controller->show(Ui::MakeConfirmBox({
+					.text = tr::lng_settings_need_restart(),
+					.confirmed = confirmed,
+					.confirmText = tr::lng_settings_restart_now(),
+				}));
 			};
 			SingleChoiceBox(box, {
 				.title = tr::lng_settings_angle_backend(),
@@ -680,11 +681,12 @@ void SetupOpenGL(
 		const auto cancelled = crl::guard(button, [=] {
 			toggles->fire(!enabled);
 		});
-		controller->show(Box<Ui::ConfirmBox>(
-			tr::lng_settings_need_restart(tr::now),
-			tr::lng_settings_restart_now(tr::now),
-			confirmed,
-			cancelled));
+		controller->show(Ui::MakeConfirmBox({
+			.text = tr::lng_settings_need_restart(),
+			.confirmed = confirmed,
+			.cancelled = cancelled,
+			.confirmText = tr::lng_settings_restart_now(),
+		}));
 	}, container->lifetime());
 }
 

commit 058181e74dbb65c22c1b9860fc35aa34fe3185e6

Close restarting confirm box when cancelled
The regression seems introduced in commit 73b553fa43bed5369bd6d198cc129c97f25c2786.

Telegram/SourceFiles/settings/settings_advanced.cpp
@@ -697,8 +697,9 @@ void SetupOpenGL(
 			Local::writeSettings();
 			Core::Restart();
 		});
-		const auto cancelled = crl::guard(button, [=] {
+		const auto cancelled = crl::guard(button, [=](Fn<void()> close) {
 			toggles->fire(!enabled);
+			close();
 		});
 		controller->show(Ui::MakeConfirmBox({
 			.text = tr::lng_settings_need_restart(),
fractal-l/AyuGramDesktop

commit bafee0680611ecc408af81fc76d86a770e71c781

Show emoji statuses in channels.

Telegram/SourceFiles/info/profile/info_profile_badge.cpp
@@ -9,6 +9,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "data/data_peer.h"
 #include "data/data_session.h"
+#include "data/data_user.h"
 #include "data/stickers/data_custom_emoji.h"
 #include "info/profile/info_profile_values.h"
 #include "info/profile/info_profile_emoji_status_panel.h"
@@ -24,10 +25,16 @@ namespace {
 
 [[nodiscard]] rpl::producer<Badge::Content> ContentForPeer(
 		not_null<PeerData*> peer) {
+	const auto statusOnlyForPremium = peer->isUser();
 	return rpl::combine(
 		BadgeValue(peer),
 		EmojiStatusIdValue(peer)
 	) | rpl::map([=](BadgeType badge, DocumentId emojiStatusId) {
+		if (statusOnlyForPremium) {
+			emojiStatusId = 0;
+		} else if (emojiStatusId && badge == BadgeType::None) {
+			badge = BadgeType::Premium;
+		}
 		return Badge::Content{ badge, emojiStatusId };
 	});
 }
@@ -90,9 +97,6 @@ void Badge::setContent(Content content) {
 	}
 	if (!(_allowed & content.badge)) {
 		content.badge = BadgeType::None;
-	}
-	if (content.badge != BadgeType::Premium) {
-		content.emojiStatusId = 0;
 	}
 	if (_content == content) {
 		return;

Telegram/SourceFiles/data/data_emoji_statuses.cpp
@@ -104,19 +104,19 @@ rpl::producer<> EmojiStatuses::defaultUpdates() const {
 }
 
 void EmojiStatuses::registerAutomaticClear(
-		not_null<UserData*> user,
+		not_null<PeerData*> peer,
 		TimeId until) {
 	if (!until) {
-		_clearing.remove(user);
+		_clearing.remove(peer);
 		if (_clearing.empty()) {
 			_clearingTimer.cancel();
 		}
-	} else if (auto &already = _clearing[user]; already != until) {
+	} else if (auto &already = _clearing[peer]; already != until) {
 		already = until;
 		const auto i = ranges::min_element(_clearing, {}, [](auto &&pair) {
 			return pair.second;
 		});
-		if (i->first == user) {
+		if (i->first == peer) {
 			const auto now = base::unixtime::now();
 			if (now < until) {
 				processClearingIn(until - now);

Telegram/SourceFiles/data/data_emoji_statuses.h
@@ -51,7 +51,7 @@ public:
 	void set(DocumentId id, TimeId until = 0);
 	[[nodiscard]] bool setting() const;
 
-	void registerAutomaticClear(not_null<UserData*> user, TimeId until);
+	void registerAutomaticClear(not_null<PeerData*> peer, TimeId until);
 
 	using Groups = std::vector<Ui::EmojiGroup>;
 	[[nodiscard]] rpl::producer<Groups> emojiGroupsValue() const;
@@ -102,7 +102,7 @@ private:
 
 	mtpRequestId _sentRequestId = 0;
 
-	base::flat_map<not_null<UserData*>, TimeId> _clearing;
+	base::flat_map<not_null<PeerData*>, TimeId> _clearing;
 	base::Timer _clearingTimer;
 
 	GroupsType _emojiGroups;

Telegram/SourceFiles/data/data_peer.cpp
@@ -12,6 +12,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_chat_participant_status.h"
 #include "data/data_channel.h"
 #include "data/data_changes.h"
+#include "data/data_emoji_statuses.h"
 #include "data/data_message_reaction_id.h"
 #include "data/data_photo.h"
 #include "data/data_folder.h"
@@ -926,6 +927,24 @@ bool PeerData::changeBackgroundEmojiId(DocumentId id) {
 	_backgroundEmojiId = id;
 	return true;
 }
+
+void PeerData::setEmojiStatus(const MTPEmojiStatus &status) {
+	const auto parsed = Data::ParseEmojiStatus(status);
+	setEmojiStatus(parsed.id, parsed.until);
+}
+
+void PeerData::setEmojiStatus(DocumentId emojiStatusId, TimeId until) {
+	if (_emojiStatusId != emojiStatusId) {
+		_emojiStatusId = emojiStatusId;
+		session().changes().peerUpdated(this, UpdateFlag::EmojiStatus);
+	}
+	owner().emojiStatuses().registerAutomaticClear(this, until);
+}
+
+DocumentId PeerData::emojiStatusId() const {
+	return _emojiStatusId;
+}
+
 bool PeerData::isSelf() const {
 	if (const auto user = asUser()) {
 		return (user->flags() & UserDataFlag::Self);

Telegram/SourceFiles/data/data_peer.h
@@ -178,6 +178,10 @@ public:
 	[[nodiscard]] DocumentId backgroundEmojiId() const;
 	bool changeBackgroundEmojiId(DocumentId id);
 
+	void setEmojiStatus(const MTPEmojiStatus &status);
+	void setEmojiStatus(DocumentId emojiStatusId, TimeId until = 0);
+	[[nodiscard]] DocumentId emojiStatusId() const;
+
 	[[nodiscard]] bool isUser() const {
 		return peerIsUser(id);
 	}
@@ -466,6 +470,7 @@ private:
 	base::flat_set<QString> _nameWords; // for filtering
 	base::flat_set<QChar> _nameFirstLetters;
 
+	DocumentId _emojiStatusId = 0;
 	uint64 _backgroundEmojiId = 0;
 	crl::time _lastFullUpdate = 0;
 

Telegram/SourceFiles/data/data_session.cpp
@@ -854,6 +854,11 @@ not_null<PeerData*> Session::processChat(const MTPChat &data) {
 			channel->setDefaultRestrictions(ChatRestrictions());
 		}
 
+		if (const auto &status = data.vemoji_status()) {
+			channel->setEmojiStatus(*status);
+		} else {
+			channel->setEmojiStatus(0);
+		}
 		if (minimal) {
 			if (channel->input.type() == mtpc_inputPeerEmpty
 				|| channel->inputChannel.type() == mtpc_inputChannelEmpty) {

Telegram/SourceFiles/data/data_user.cpp
@@ -15,7 +15,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_peer_bot_command.h"
 #include "data/data_photo.h"
 #include "data/data_stories.h"
-#include "data/data_emoji_statuses.h"
 #include "data/data_wall_paper.h"
 #include "data/notify/data_notify_settings.h"
 #include "history/history.h"
@@ -71,23 +70,6 @@ void UserData::setPhoto(const MTPUserProfilePhoto &photo) {
 		removeFlags(UserDataFlag::PersonalPhoto);
 		clearUserpic();
 	});
-}
-
-void UserData::setEmojiStatus(const MTPEmojiStatus &status) {
-	const auto parsed = Data::ParseEmojiStatus(status);
-	setEmojiStatus(parsed.id, parsed.until);
-}
-
-void UserData::setEmojiStatus(DocumentId emojiStatusId, TimeId until) {
-	if (_emojiStatusId != emojiStatusId) {
-		_emojiStatusId = emojiStatusId;
-		session().changes().peerUpdated(this, UpdateFlag::EmojiStatus);
-	}
-	owner().emojiStatuses().registerAutomaticClear(this, until);
-}
-
-DocumentId UserData::emojiStatusId() const {
-	return _emojiStatusId;
 }
 
 auto UserData::unavailableReasons() const

Telegram/SourceFiles/data/data_user.h
@@ -76,7 +76,6 @@ public:
 
 	UserData(not_null<Data::Session*> owner, PeerId id);
 	void setPhoto(const MTPUserProfilePhoto &photo);
-	void setEmojiStatus(const MTPEmojiStatus &status);
 
 	void setName(
 		const QString &newFirstName,
@@ -84,9 +83,6 @@ public:
 		const QString &newPhoneName,
 		const QString &newUsername);
 	void setUsernames(const Data::Usernames &newUsernames);
-
-	void setEmojiStatus(DocumentId emojiStatusId, TimeId until = 0);
-	[[nodiscard]] DocumentId emojiStatusId() const;
 
 	void setUsername(const QString &username);
 	void setPhone(const QString &newPhone);
@@ -198,8 +194,6 @@ private:
 	uint64 _accessHash = 0;
 	static constexpr auto kInaccessibleAccessHashOld
 		= 0xFFFFFFFFFFFFFFFFULL;
-
-	DocumentId _emojiStatusId = 0;
 
 };
 

commit 0ea453de44c69703db60c8e8f1b2f89f27a1036d

Fix emoji status display for users.
Regression was introduced in bafee06806.

Telegram/SourceFiles/info/profile/info_profile_badge.cpp
@@ -30,7 +30,7 @@ namespace {
 		BadgeValue(peer),
 		EmojiStatusIdValue(peer)
 	) | rpl::map([=](BadgeType badge, DocumentId emojiStatusId) {
-		if (statusOnlyForPremium) {
+		if (statusOnlyForPremium && badge != BadgeType::Premium) {
 			emojiStatusId = 0;
 		} else if (emojiStatusId && badge == BadgeType::None) {
 			badge = BadgeType::Premium;
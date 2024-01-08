fractal-l/AyuGramDesktop

commit 22a203bf9a5bb919f6809aa85f05f846b8238fa9

Instantiate QRegularExpression instances statically

Telegram/SourceFiles/payments/ui/payments_field.cpp
@@ -36,7 +36,8 @@ struct SimpleFieldState {
 }
 
 [[nodiscard]] QString RemoveNonNumbers(QString value) {
-	return value.replace(QRegularExpression("[^0-9]"), QString());
+	static const auto RegExp = QRegularExpression("[^0-9]");
+	return value.replace(RegExp, QString());
 }
 
 [[nodiscard]] SimpleFieldState CleanMoneyState(
@@ -216,6 +217,7 @@ struct SimpleFieldState {
 		const FieldConfig &config,
 		const QString &parsed,
 		const QString &countryIso2) {
+	static const auto RegExp = QRegularExpression("[^0-9]\\.");
 	if (config.type == FieldType::Country) {
 		return countryIso2;
 	} else if (config.type == FieldType::Money) {
@@ -227,16 +229,14 @@ struct SimpleFieldState {
 			QChar(','),
 			QChar('.')
 		).replace(
-			QRegularExpression("[^0-9\\.]"),
+			RegExp,
 			QString()
 		).toDouble();
 		return QString::number(
 			int64(base::SafeRound(real * std::pow(10., rule.exponent))));
 	} else if (config.type == FieldType::CardNumber
 		|| config.type == FieldType::CardCVC) {
-		return QString(parsed).replace(
-			QRegularExpression("[^0-9\\.]"),
-			QString());
+		return QString(parsed).replace(RegExp, QString());
 	}
 	return parsed;
 }

Telegram/SourceFiles/boxes/connection_box.cpp
@@ -113,7 +113,8 @@ Base64UrlInput::Base64UrlInput(
 	rpl::producer<QString> placeholder,
 	const QString &val)
 : MaskedInputField(parent, st, std::move(placeholder), val) {
-	if (!QRegularExpression("^[a-zA-Z0-9_\\-]+$").match(val).hasMatch()) {
+	static const auto RegExp = QRegularExpression("^[a-zA-Z0-9_\\-]+$");
+	if (!RegExp.match(val).hasMatch()) {
 		setText(QString());
 	}
 }
@@ -831,8 +832,9 @@ void ProxyBox::prepare() {
 	connect(_host.data(), &HostInput::changed, [=] {
 		Ui::PostponeCall(_host, [=] {
 			const auto host = _host->getLastText().trimmed();
-			static const auto mask = u"^\\d+\\.\\d+\\.\\d+\\.\\d+:(\\d*)$"_q;
-			const auto match = QRegularExpression(mask).match(host);
+			static const auto mask = QRegularExpression(
+				u"^\\d+\\.\\d+\\.\\d+\\.\\d+:(\\d*)$"_q);
+			const auto match = mask.match(host);
 			if (_host->cursorPosition() == host.size()
 				&& match.hasMatch()) {
 				const auto port = match.captured(1);
@@ -1107,6 +1109,10 @@ void ProxiesBoxController::ShowApplyConfirmation(
 		proxy.password = fields.value(u"secret"_q);
 	}
 	if (proxy) {
+		static const auto UrlStartRegExp = QRegularExpression(
+			"^https://",
+			QRegularExpression::CaseInsensitiveOption);
+		static const auto UrlEndRegExp = QRegularExpression("/$");
 		const auto displayed = "https://" + server + "/";
 		const auto parsed = QUrl::fromUserInput(displayed);
 		const auto displayUrl = !UrlClickHandler::IsSuspicious(displayed)
@@ -1117,11 +1123,9 @@ void ProxiesBoxController::ShowApplyConfirmation(
 		const auto displayServer = QString(
 			displayUrl
 		).replace(
-			QRegularExpression(
-				"^https://",
-				QRegularExpression::CaseInsensitiveOption),
+			UrlStartRegExp,
 			QString()
-		).replace(QRegularExpression("/$"), QString());
+		).replace(UrlEndRegExp, QString());
 		const auto text = tr::lng_sure_enable_socks(
 			tr::now,
 			lt_server,

Telegram/SourceFiles/core/launcher.cpp
@@ -542,9 +542,13 @@ void Launcher::processArguments() {
 		}
 	}
 
+	static const auto RegExp = QRegularExpression("[^a-z0-9\\-_]");
 	gDebugMode = parseResult.contains("-debug");
-	gKeyFile = parseResult.value("-key", {}).join(QString()).toLower();
-	gKeyFile = gKeyFile.replace(QRegularExpression("[^a-z0-9\\-_]"), {});
+	gKeyFile = parseResult
+		.value("-key", {})
+		.join(QString())
+		.toLower()
+		.replace(RegExp, {});
 	gLaunchMode = parseResult.contains("-autostart") ? LaunchModeAutoStart
 		: parseResult.contains("-fixprevious") ? LaunchModeFixPrevious
 		: parseResult.contains("-cleanup") ? LaunchModeCleanup

Telegram/SourceFiles/core/update_checker.cpp
@@ -241,7 +241,7 @@ QString FindUpdateFile() {
 	}
 	const auto list = updates.entryInfoList(QDir::Files);
 	for (const auto &info : list) {
-		if (QRegularExpression(
+		static const auto RegExp = QRegularExpression(
 			"^("
 			"tupdate|"
 			"tx64upd|"
@@ -250,7 +250,8 @@ QString FindUpdateFile() {
 			"tlinuxupd|"
 			")\\d+(_[a-z\\d]+)?$",
 			QRegularExpression::CaseInsensitiveOption
-		).match(info.fileName()).hasMatch()) {
+		);
+		if (RegExp.match(info.fileName()).hasMatch()) {
 			return info.absoluteFilePath();
 		}
 	}

Telegram/SourceFiles/intro/intro_phone.cpp
@@ -169,7 +169,8 @@ void PhoneWidget::submit() {
 
 	// Check if such account is authorized already.
 	const auto digitsOnly = [](QString value) {
-		return value.replace(QRegularExpression("[^0-9]"), QString());
+		static const auto RegExp = QRegularExpression("[^0-9]");
+		return value.replace(RegExp, QString());
 	};
 	const auto phoneDigits = digitsOnly(phone);
 	for (const auto &[index, existing] : Core::App().domain().accounts()) {

Telegram/SourceFiles/mtproto/mtp_instance.cpp
@@ -1354,8 +1354,11 @@ bool Instance::Private::onErrorDefault(
 	const auto &type = error.type();
 	const auto code = error.code();
 	auto badGuestDc = (code == 400) && (type == u"FILE_ID_INVALID"_q);
+	static const auto MigrateRegExp = QRegularExpression("^(FILE|PHONE|NETWORK|USER)_MIGRATE_(\\d+)$");
+	static const auto FloodWaitRegExp = QRegularExpression("^FLOOD_WAIT_(\\d+)$");
+	static const auto SlowmodeWaitRegExp = QRegularExpression("^SLOWMODE_WAIT_(\\d+)$");
 	QRegularExpressionMatch m1, m2;
-	if ((m1 = QRegularExpression("^(FILE|PHONE|NETWORK|USER)_MIGRATE_(\\d+)$").match(type)).hasMatch()) {
+	if ((m1 = MigrateRegExp.match(type)).hasMatch()) {
 		if (!requestId) return false;
 
 		auto dcWithShift = ShiftedDcId(0);
@@ -1458,8 +1461,8 @@ bool Instance::Private::onErrorDefault(
 		return true;
 	} else if (code < 0
 		|| code >= 500
-		|| (m1 = QRegularExpression("^FLOOD_WAIT_(\\d+)$").match(type)).hasMatch()
-		|| ((m2 = QRegularExpression("^SLOWMODE_WAIT_(\\d+)$").match(type)).hasMatch()
+		|| (m1 = FloodWaitRegExp.match(type)).hasMatch()
+		|| ((m2 = SlowmodeWaitRegExp.match(type)).hasMatch()
 			&& m2.captured(1).toInt() < 3)) {
 		if (!requestId) return false;
 

Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -757,8 +757,9 @@ bool DcOptions::loadFromFile(const QString &path) {
 	stream.setCodec("UTF-8");
 #endif // Qt < 6.0.0
 	while (!stream.atEnd()) {
+		static const auto RegExp = QRegularExpression(R"(\s)");
 		auto line = stream.readLine();
-		auto components = line.split(QRegularExpression(R"(\s)"), Qt::SkipEmptyParts);
+		auto components = line.split(RegExp, Qt::SkipEmptyParts);
 		if (components.isEmpty() || components[0].startsWith('#')) {
 			continue;
 		}

Telegram/SourceFiles/mtproto/mtproto_proxy_data.cpp
@@ -157,11 +157,12 @@ bool ProxyData::supportsCalls() const {
 }
 
 bool ProxyData::tryCustomResolve() const {
+	static const auto RegExp = QRegularExpression(
+		QStringLiteral("^\\d+\\.\\d+\\.\\d+\\.\\d+$")
+	);
 	return (type == Type::Socks5 || type == Type::Mtproto)
 		&& !qthelp::is_ipv6(host)
-		&& !QRegularExpression(
-			QStringLiteral("^\\d+\\.\\d+\\.\\d+\\.\\d+$")
-		).match(host).hasMatch();
+		&& !RegExp.match(host).hasMatch();
 }
 
 bytes::vector ProxyData::secretFromMtprotoPassword() const {

commit f442c4987a27d5b86d3bb6317ec15add50212e0c

Fix payment field values formatting.
Regression was introduced in 22a203bf9a.

Fixes #27318.

Telegram/SourceFiles/payments/ui/payments_field.cpp
@@ -217,10 +217,10 @@ struct SimpleFieldState {
 		const FieldConfig &config,
 		const QString &parsed,
 		const QString &countryIso2) {
-	static const auto RegExp = QRegularExpression("[^0-9]\\.");
 	if (config.type == FieldType::Country) {
 		return countryIso2;
 	} else if (config.type == FieldType::Money) {
+		static const auto RegExp = QRegularExpression("[^0-9\\.]");
 		const auto rule = LookupCurrencyRule(config.currency);
 		const auto real = QString(parsed).replace(
 			QChar(rule.decimal),
@@ -236,6 +236,7 @@ struct SimpleFieldState {
 			int64(base::SafeRound(real * std::pow(10., rule.exponent))));
 	} else if (config.type == FieldType::CardNumber
 		|| config.type == FieldType::CardCVC) {
+		static const auto RegExp = QRegularExpression("[^0-9]");
 		return QString(parsed).replace(RegExp, QString());
 	}
 	return parsed;
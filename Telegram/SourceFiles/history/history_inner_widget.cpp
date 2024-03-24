fractal-l/AyuGramDesktop

commit 4d935151538bf2ed61c0c5f34136115eec324a2a

Layout chatbots editing section.

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -7,6 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "history/history_inner_widget.h"
 
+#include "chat_helpers/stickers_emoji_pack.h"
 #include "core/file_utilities.h"
 #include "core/click_handler_types.h"
 #include "history/history_item_helpers.h"
@@ -32,6 +33,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/effects/message_sending_animation_controller.h"
 #include "ui/effects/reaction_fly_animation.h"
 #include "ui/text/text_options.h"
+#include "ui/text/text_isolated_emoji.h"
 #include "ui/boxes/report_box.h"
 #include "ui/layers/generic_box.h"
 #include "ui/controls/delete_message_context_action.h"
@@ -2238,6 +2240,17 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 			}, &st::menuIconStickers);
 		}
 	};
+
+	if (const auto item = _dragStateItem) {
+		const auto emojiStickers = &session->emojiStickersPack();
+		if (const auto view = item->mainView()) {
+			if (const auto isolated = view->isolatedEmoji()) {
+				if (const auto sticker = emojiStickers->stickerForEmoji(isolated)) {
+					addDocumentActions(sticker.document, item);
+				}
+			}
+		}
+	}
 
 	const auto asGroup = !Element::Moused()
 		|| (Element::Moused() != Element::Hovered())

Telegram/CMakeLists.txt
@@ -1277,6 +1277,8 @@ PRIVATE
     profile/profile_block_widget.h
     profile/profile_cover_drop_area.cpp
     profile/profile_cover_drop_area.h
+    settings/business/settings_chatbots.cpp
+    settings/business/settings_chatbots.h
     settings/cloud_password/settings_cloud_password_common.cpp
     settings/cloud_password/settings_cloud_password_common.h
     settings/cloud_password/settings_cloud_password_email.cpp

Telegram/Resources/langs/lang.strings
@@ -2172,6 +2172,24 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 "lng_business_subtitle_chatbots" = "Chatbots";
 "lng_business_about_chatbots" = "Add any third party chatbots that will process customer interactions.";
 
+"lng_chatbots_title" = "Chatbots";
+"lng_chatbots_about" = "Add a bot to your account to help you automatically process and respond to the messages you receive. {link}";
+"lng_chatbots_about_link" = "Learn more...";
+"lng_chatbots_placeholder" = "Enter bot URL or username";
+"lng_chatbots_add_about" = "Enter the link to the Telegram bot that you want to automatically process your chats.";
+"lng_chatbots_access_title" = "Chats accessible for the bot";
+"lng_chatbots_all_except" = "All 1-to-1 Chats Except...";
+"lng_chatbots_selected" = "Only Selected Chats";
+"lng_chatbots_excluded_title" = "Excluded chats";
+"lng_chatbots_exclude_button" = "Exclude Chats";
+"lng_chatbots_included_title" = "Included chats";
+"lng_chatbots_include_button" = "Select Chats";
+"lng_chatbots_exclude_about" = "Select chats or entire chat categories which the bot will not have access to.";
+"lng_chatbots_permissions_title" = "Bot permissions";
+"lng_chatbots_reply" = "Reply to Messages";
+"lng_chatbots_reply_about" = "The bot will be able to view all new incoming messages, but not the messages that had been sent before you added the bot.";
+"lng_chatbots_remove" = "Remove Bot";
+
 "lng_boost_channel_button" = "Boost Channel";
 "lng_boost_group_button" = "Boost Group";
 "lng_boost_again_button" = "Boost Again";

Telegram/Resources/qrc/telegram/animations.qrc
@@ -14,5 +14,6 @@
     <file alias="voice_ttl_idle.tgs">../../animations/voice_ttl_idle.tgs</file>
     <file alias="voice_ttl_start.tgs">../../animations/voice_ttl_start.tgs</file>
     <file alias="palette.tgs">../../animations/palette.tgs</file>
+    <file alias="robot.tgs">../../animations/robot.tgs</file>
   </qresource>
 </RCC>

Telegram/SourceFiles/boxes/peers/edit_linked_chat_box.cpp
@@ -260,11 +260,11 @@ void Controller::choose(not_null<ChatData*> chat) {
 
 	const auto init = [=](not_null<ListBox*> box) {
 		auto above = object_ptr<Ui::VerticalLayout>(box);
-		Settings::AddDividerTextWithLottie(
-			above,
-			box->showFinishes(),
-			About(channel, chat),
-			u"discussion"_q);
+		Settings::AddDividerTextWithLottie(above, {
+			.lottie = u"discussion"_q,
+			.showFinished = box->showFinishes(),
+			.about = About(channel, chat),
+		});
 		if (!chat) {
 			Assert(channel->isBroadcast());
 

Telegram/SourceFiles/boxes/peers/edit_peer_color_box.cpp
@@ -1223,35 +1223,15 @@ void EditPeerColorBox(
 	state->index = peer->colorIndex();
 	state->emojiId = peer->backgroundEmojiId();
 	state->statusId = peer->emojiStatusId();
-
 	if (group) {
-		const auto divider = Ui::CreateChild<Ui::BoxContentDivider>(
-			box.get());
-		const auto verticalLayout = box->verticalLayout()->add(
-			object_ptr<Ui::VerticalLayout>(box.get()));
-
-		auto icon = CreateLottieIcon(
-			verticalLayout,
-			{
-				.name = u"palette"_q,
-				.sizeOverride = Size(st::settingsCloudPasswordIconSize),
-			},
-			st::peerAppearanceIconPadding);
-		box->setShowFinishedCallback([animate = std::move(icon.animate)] {
-			animate(anim::repeat::once);
+		Settings::AddDividerTextWithLottie(box->verticalLayout(), {
+			.lottie = u"palette"_q,
+			.lottieSize = st::settingsCloudPasswordIconSize,
+			.lottieMargins = st::peerAppearanceIconPadding,
+			.showFinished = box->showFinishes(),
+			.about = tr::lng_boost_group_about(Ui::Text::WithEntities),
+			.aboutMargins = st::peerAppearanceCoverLabelMargin,
 		});
-		verticalLayout->add(std::move(icon.widget));
-		verticalLayout->add(
-			object_ptr<Ui::FlatLabel>(
-				verticalLayout,
-				tr::lng_boost_group_about(),
-				st::peerAppearanceCoverLabel),
-		st::peerAppearanceCoverLabelMargin);
-
-		verticalLayout->geometryValue(
-		) | rpl::start_with_next([=](const QRect &r) {
-			divider->setGeometry(r);
-		}, divider->lifetime());
 	} else {
 		box->addRow(object_ptr<PreviewWrap>(
 			box,

Telegram/SourceFiles/core/local_url_handlers.cpp
@@ -669,6 +669,17 @@ bool ShowSearchTagsPromo(
 	return true;
 }
 
+bool ShowAboutBusinessChatbots(
+		Window::SessionController *controller,
+		const Match &match,
+		const QVariant &context) {
+	if (!controller) {
+		return false;
+	}
+	controller->showToast(u"Cool feature, yeah.."_q); AssertIsDebug();
+	return true;
+}
+
 void ExportTestChatTheme(
 		not_null<Window::SessionController*> controller,
 		not_null<const Data::CloudTheme*> theme) {
@@ -1036,7 +1047,11 @@ const std::vector<LocalUrlHandler> &InternalUrlHandlers() {
 		{
 			u"about_tags"_q,
 			ShowSearchTagsPromo
-		}
+		},
+		{
+			u"about_business_chatbots"_q,
+			ShowAboutBusinessChatbots
+		},
 	};
 	return Result;
 }

commit 41d3531acf3e63d6cc330ab56c67a48467558aa1

Remove for-development-only save document action.
Regression was introduced in 4d93515153.

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -7,7 +7,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "history/history_inner_widget.h"
 
-#include "chat_helpers/stickers_emoji_pack.h"
 #include "core/file_utilities.h"
 #include "core/click_handler_types.h"
 #include "history/history_item_helpers.h"
@@ -33,7 +32,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/effects/message_sending_animation_controller.h"
 #include "ui/effects/reaction_fly_animation.h"
 #include "ui/text/text_options.h"
-#include "ui/text/text_isolated_emoji.h"
 #include "ui/boxes/report_box.h"
 #include "ui/layers/generic_box.h"
 #include "ui/controls/delete_message_context_action.h"
@@ -2250,17 +2248,6 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 			}, &st::menuIconStickers);
 		}
 	};
-
-	if (const auto item = _dragStateItem) {
-		const auto emojiStickers = &session->emojiStickersPack();
-		if (const auto view = item->mainView()) {
-			if (const auto isolated = view->isolatedEmoji()) {
-				if (const auto sticker = emojiStickers->stickerForEmoji(isolated)) {
-					addDocumentActions(sticker.document, item);
-				}
-			}
-		}
-	}
 
 	const auto asGroup = !Element::Moused()
 		|| (Element::Moused() != Element::Hovered())
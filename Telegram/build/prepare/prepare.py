c618dd367584407bd4d7251c46e91e354703281b
fractal <fractal@nebula-nook.ru>
2025-05-05 18:45:40 +0300

    fix: make it build
    Thanks to tdesktop team for publishing broken sources ^^

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -473,7 +473,14 @@ win:
     del msys64.exe
 
     bash -c "pacman-key --init; pacman-key --populate; pacman -Syu --noconfirm"
-    pacman -Syu --noconfirm mingw-w64-x86_64-perl mingw-w64-x86_64-nasm mingw-w64-x86_64-yasm mingw-w64-x86_64-ninja msys/make diffutils pkg-config
+    pacman -Syu --noconfirm ^
+        make ^
+        mingw-w64-x86_64-diffutils ^
+        mingw-w64-x86_64-gperf ^
+        mingw-w64-x86_64-nasm ^
+        mingw-w64-x86_64-perl ^
+        mingw-w64-x86_64-pkgconf ^
+        mingw-w64-x86_64-yasm
 
     SET PATH=%PATH_BACKUP_%
 """, 'ThirdParty')
@@ -1941,7 +1948,6 @@ win:
         -DOPENSSL_FOUND=1 ^
         -DOPENSSL_INCLUDE_DIR=%OPENSSL_DIR%\\include ^
         -DOPENSSL_CRYPTO_LIBRARY="%OPENSSL_LIBS_DIR%.dbg\\libcrypto.lib" ^
-        -DGPERF_EXECUTABLE=%ROOT_DIR%\\ThirdParty\\gperf\\bin\\gperf ^
         -DZLIB_FOUND=1 ^
         -DZLIB_INCLUDE_DIR=%ZLIB_LIBS_DIR% ^
         -DZLIB_LIBRARIES="%ZLIB_LIBS_DIR%\\Debug\\zlibstaticd.lib" ^
@@ -1960,7 +1966,6 @@ release:
         -DOPENSSL_FOUND=1 ^
         -DOPENSSL_INCLUDE_DIR=%OPENSSL_DIR%\\include ^
         -DOPENSSL_CRYPTO_LIBRARY="%OPENSSL_LIBS_DIR%\\libcrypto.lib" ^
-        -DGPERF_EXECUTABLE=%ROOT_DIR%\\ThirdParty\\gperf\\bin\\gperf ^
         -DZLIB_FOUND=1 ^
         -DZLIB_INCLUDE_DIR=%ZLIB_LIBS_DIR% ^
         -DZLIB_LIBRARIES="%ZLIB_LIBS_DIR%\\Release\\zlibstatic.lib" ^

fractal-l/AyuGramDesktop

commit 2005e5692225bef7d0538e63ae1f46a446b35b02

Cherry-pick mozjpeg cmake 4 compatibility

Telegram/build/prepare/prepare.py
@@ -577,6 +577,7 @@ mac:
 stage('mozjpeg', """
     git clone -b v4.1.5 https://github.com/mozilla/mozjpeg.git
     cd mozjpeg
+    git cherry-pick 1644bdb
 win:
     cmake . ^
         -A %WIN32X64% ^

commit af4af4d71d7424af204c1c25a5fa4afb1dc5beb0

Set CMAKE_MSVC_RUNTIME_LIBRARY for mozjpeg
The cherry-pick made in 2005e5692225bef7d0538e63ae1f46a446b35b02 broke mozjpeg's own WITH_CRT_DLL variable so we have to use the cmake one

Telegram/build/prepare/prepare.py
@@ -586,6 +586,7 @@ stage('mozjpeg', """
 win:
     cmake . ^
         -A %WIN32X64% ^
+        -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>" ^
         -DWITH_JPEG8=ON ^
         -DPNG_SUPPORTED=OFF
     cmake --build . --config Debug --parallel
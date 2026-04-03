fractal-l/AyuGramDesktop

commit e7d96aa571b638fe51233c031a100cc316b176b7

Fixed initial tray icon color on macOS dark theme.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,7 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = status.button.effectiveAppearance;
+	const auto appearance = [NSApp effectiveAppearance];
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 

commit 19bc061e757af36d4ab0e63b7b6f7eef4d17fb0a

Fixed crash on macOS 10.13.
Regression was introduced in e7d96aa571.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,12 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = [NSApp effectiveAppearance];
+	const auto appearance = [&] {
+		if (@available(macOS 10.14, *)) {
+			return [NSApp effectiveAppearance];
+		}
+		return status.button.effectiveAppearance;
+	}();
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 